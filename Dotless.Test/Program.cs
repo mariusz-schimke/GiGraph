2dc262e2a2954c066ef9f6a0605ace0f202e3568
Mariusz Schimke <mariusz.schimke@iteo.com>
2020-04-26 09:26:25 +0200

    Add initial project

---
+++ Dotless.Test/Program.cs
@@ -0,0 +1,26 @@
+﻿using Dotless.Attributes;
+using Dotless.Graphs;
+using System;
+
+namespace Dotless
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            DotHtmlLabel htmlLabel = "HTML label";
+            DotLabel label = "Text \" \\label";
+
+            var dotGraph = new DotGraph("test graph", isDirected: true, isStrict: true);
+
+            Console.WriteLine($"{label}");
+            Console.WriteLine($"{htmlLabel}");
+            Console.WriteLine($"{label == htmlLabel}");
+
+            Console.WriteLine();
+            Console.WriteLine(dotGraph.ToString(true));
+
+            Console.ReadLine();
+        }
+    }
+}
