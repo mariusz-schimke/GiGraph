417207164eef1c0481fb325aab8d388b9481f7d9
Mariusz Schimke <mariusz.schimke@gmail.com>
2020-04-26 09:55:31 +0200

    Basic attributes added

---
+++ Dotless/Attributes/DotHtmlLabel.cs
@@ -0,0 +1,20 @@
+﻿namespace Dotless.Attributes
+{
+    public class DotHtmlLabel : DotLabel
+    {
+        public DotHtmlLabel(string value)
+            : base(value)
+        {
+        }
+
+        protected override string? QuoteValue(string? value)
+        {
+            return $@"<{value}>";
+        }
+
+        public static implicit operator DotHtmlLabel(string value)
+        {
+            return new DotHtmlLabel(value);
+        }
+    }
+}
