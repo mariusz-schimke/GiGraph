1d07baeaa7ccbfa86419aefb4dcb79778ae61d42
Mariusz Schimke <mariusz.schimke@gmail.com>
2020-05-03 20:31:32 +0200

    WIP: DotAttribute class cleanup

--- Dotless/Attributes/DotAttribute.cs
+++ Dotless/Attributes/DotAttribute.cs
@@ -1,26 +1,42 @@
 ﻿namespace Dotless.Attributes
 {
-    public abstract class DotAttribute<T> : IDotAttribute
+    public abstract class DotAttribute : IDotAttribute
     {
         protected readonly string _key;
-        protected readonly T _value;
 
-        internal string Key => _key;
-        internal T Value => _value;
+        protected abstract bool HasValue { get; }
 
         string IDotAttribute.Key => _key;
+        bool IDotAttribute.HasValue => HasValue;
 
-        public bool HasValue => _value is { };
-
-        public DotAttribute(string key, T value)
+        protected DotAttribute(string key)
         {
             _key = key;
+        }
+    }
+
+    public abstract class DotAttribute<T> : DotAttribute, IDotAttribute<T>
+    {
+        protected readonly T _value;
+
+        protected override bool HasValue => _value is { };
+
+        T IDotAttribute<T>.Value => _value;
+
+        protected DotAttribute(string key, T value)
+            : base(key)
+        {
             _value = value;
         }
 
         public override string? ToString()
         {
             return _value?.ToString();
+        }
+
+        public static implicit operator T(DotAttribute<T> value)
+        {
+            return value._value;
         }
     }
 }
--- Dotless/Attributes/IDotAttribute.cs
+++ Dotless/Attributes/IDotAttribute.cs
@@ -2,10 +2,14 @@
 
 namespace Dotless.Attributes
 {
-    // TODO: czy ten interfejs jest rzeczywiście użyteczny?
     public interface IDotAttribute : IDotEntity
     {
         string Key { get; }
         bool HasValue { get; }
+    }
+
+    public interface IDotAttribute<T> : IDotAttribute
+    {
+        T Value { get; }
     }
 }
