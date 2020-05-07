e375427237f0e591eb40bb09122b0c09e20f60a2
Mariusz Schimke <mariusz.schimke@gmail.com>
2020-05-07 18:58:45 +0200

    Single line output fixes

--- Dotless/EntityGenerators/GraphGenerators/DotGraphGeneratorFactory.cs
+++ Dotless/EntityGenerators/GraphGenerators/DotGraphGeneratorFactory.cs
@@ -3,32 +3,36 @@ using Dotless.Core;
 using Dotless.EntityGenerators.AttributeGenerators;
 using Dotless.EntityGenerators.NodeGenerators;
 using Dotless.EntityGenerators.Options;
+using System;
 
 namespace Dotless.EntityGenerators.GraphGenerators
 {
     // TODO: dokończyć tę klasę (jakieś interfejsy, możliwość modyfikacji/dostarczenia providerów)
     public static class DotGraphGeneratorFactory
     {
-        public static DotGraphGenerator CreateDefault()
+        public static DotGraphGenerator CreateDefault(Action<DotGenerationOptions>? setOptions = null, Action<DotSyntaxRules>? setSyntaxRules = null)
         {
             var syntaxRules = new DotSyntaxRules();
-            var dotWriterOptions = new DotGenerationOptions();
+            var options = new DotGenerationOptions();
             var converters = new DotEntityGeneratorsProvider();
 
-            converters.Register(new DotGraphGenerator(syntaxRules, dotWriterOptions, converters));
-            converters.Register(new DotGraphBodyGenerator(syntaxRules, dotWriterOptions, converters));
+            setOptions?.Invoke(options);
+            setSyntaxRules?.Invoke(syntaxRules);
 
-            converters.Register(new DotSubgraphGenerator(syntaxRules, dotWriterOptions, converters));
+            converters.Register(new DotGraphGenerator(syntaxRules, options, converters));
+            converters.Register(new DotGraphBodyGenerator(syntaxRules, options, converters));
 
-            converters.Register(new DotAttributeCollectionGenerator(syntaxRules, dotWriterOptions, converters));
+            converters.Register(new DotSubgraphGenerator(syntaxRules, options, converters));
 
-            converters.Register(new DotStringAttributeGenerator<DotTextLabel>(syntaxRules, dotWriterOptions, converters));
-            converters.Register(new DotHtmlLabelAttributeGenerator(syntaxRules, dotWriterOptions, converters));
+            converters.Register(new DotAttributeCollectionGenerator(syntaxRules, options, converters));
 
-            converters.Register(new DotNodeGenerator(syntaxRules, dotWriterOptions, converters));
-            converters.Register(new DotNodeCollectionGenerator(syntaxRules, dotWriterOptions, converters));
+            converters.Register(new DotStringAttributeGenerator<DotTextLabel>(syntaxRules, options, converters));
+            converters.Register(new DotHtmlLabelAttributeGenerator(syntaxRules, options, converters));
 
-            return new DotGraphGenerator(syntaxRules, dotWriterOptions, converters);
+            converters.Register(new DotNodeGenerator(syntaxRules, options, converters));
+            converters.Register(new DotNodeCollectionGenerator(syntaxRules, options, converters));
+
+            return new DotGraphGenerator(syntaxRules, options, converters);
         }
     }
 }
