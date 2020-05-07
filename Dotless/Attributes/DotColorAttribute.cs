fa8ad69e510a1c9105b3d84af0803cfa39a3bf6c
Mariusz Schimke <mariusz.schimke@gmail.com>
2020-05-07 20:29:25 +0200

    Color and background color attribute added

---
+++ Dotless/Attributes/DotColorAttribute.cs
@@ -0,0 +1,22 @@
+﻿using System.Drawing;
+
+namespace Dotless.Attributes
+{
+    public class DotColorAttribute : DotAttribute<Color>
+    {
+        protected DotColorAttribute(string key, Color color)
+            : base(key, color)
+        {
+        }
+
+        public DotColorAttribute(Color value)
+            : this("color", value)
+        {
+        }
+
+        protected override string GetValueAsString()
+        {
+            return $"#{_value.R:x2}{_value.G:x2}{_value.B:x2}{_value.A:x2}";
+        }
+    }
+}
