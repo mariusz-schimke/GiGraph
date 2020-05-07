34dce4ca720dadfe1e5f19e5416e4a118cc939dc
Mariusz Schimke <mariusz.schimke@gmail.com>
2020-05-07 20:41:55 +0200

    Added DotAttributedEntity with common attributes

---
+++ Dotless/DotAttributedEntity.cs
@@ -0,0 +1,26 @@
+﻿using Dotless.Attributes;
+using Dotless.Core;
+
+namespace Dotless
+{
+    public abstract class DotAttributedEntity : IDotEntity
+    {
+        public DotAttributeCollection Attributes { get; } = new DotAttributeCollection();
+
+        public virtual DotLabel? Label
+        {
+            get => (DotLabel?)Attributes.TryGet<DotLabel>(DotLabel.AttributeKey);
+            set
+            {
+                if (value is null)
+                {
+                    Attributes.Remove(DotLabel.AttributeKey);
+                }
+                else
+                {
+                    Attributes.SetAttribute(value);
+                }
+            }
+        }
+    }
+}
--- Dotless/Graphs/DotGraphBody.cs
+++ Dotless/Graphs/DotGraphBody.cs
@@ -1,28 +1,9 @@
-﻿using Dotless.Attributes;
-using Dotless.Core;
-using Dotless.Nodes;
+﻿using Dotless.Nodes;
 
 namespace Dotless.Graphs
 {
-    public abstract class DotGraphBody : IDotEntity
+    public abstract class DotGraphBody : DotAttributedEntity
     {
         public DotNodeCollection Nodes { get; } = new DotNodeCollection();
-        public DotAttributeCollection Attributes { get; } = new DotAttributeCollection();
-
-        public virtual DotLabel? Label
-        {
-            get => (DotLabel?)Attributes.TryGet<DotLabel>(DotLabel.AttributeKey);
-            set
-            {
-                if (value is null)
-                {
-                    Attributes.Remove(DotLabel.AttributeKey);
-                }
-                else
-                {
-                    Attributes.SetAttribute(value);
-                }
-            }
-        }
     }
 }
