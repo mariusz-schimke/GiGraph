365ff4c84db8fabc5216007cc51afd82611d27c5
Mariusz Schimke <mariusz.schimke@gmail.com>
2020-05-10 14:49:00 +0200

    Subgraph collection generator and writer added

---
+++ Gigraph.Dot.Writers/SubgraphWriters/DotSubgraphCollectionWriter.cs
@@ -0,0 +1,23 @@
+﻿using Gigraph.Dot.Writers.Contexts;
+using Gigraph.Dot.Writers.SubgraphWriters;
+
+namespace Gigraph.Dot.Writers.NodeWriters
+{
+    public class DotSubgraphCollectionWriter : DotStatementWriter, IDotSubgraphCollectionWriter
+    {
+        public DotSubgraphCollectionWriter(DotTokenWriter tokenWriter, DotEntityWriterContext context, bool useStatementDelimiter)
+            : base(tokenWriter, context, useStatementDelimiter)
+        {
+        }
+
+        public virtual IDotSubgraphWriter BeginSubgraph()
+        {
+            return new DotSubgraphWriter(_tokenWriter, _context);
+        }
+
+        public virtual void EndSubgraph()
+        {
+            EndStatement();
+        }
+    }
+}
