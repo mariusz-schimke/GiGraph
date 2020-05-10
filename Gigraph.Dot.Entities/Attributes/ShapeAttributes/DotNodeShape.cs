b3a1334666a68832b8bef68fa2164eceaee3ff02
Mariusz Schimke <mariusz.schimke@gmail.com>
2020-05-10 16:58:34 +0200

    Node shape attribute added

---
+++ Gigraph.Dot.Entities/Attributes/ShapeAttributes/DotNodeShape.cs
@@ -0,0 +1,85 @@
+﻿namespace Gigraph.Dot.Entities.Attributes.ShapeAttributes
+{
+    /// <summary>
+    /// The node shape. To see how each of the shapes are rendered, please go to <seealso cref="https://www.graphviz.org/doc/info/shapes.html"/>.
+    /// </summary>
+    public enum DotNodeShape
+    {
+        None,
+
+        Box,
+        Polygon,
+
+        Ellipse,
+        Oval,
+        Circle,
+
+        Point,
+        Egg,
+
+        Triangle,
+
+        PlainText,
+        Plain,
+
+        Diamond,
+        Trapezium,
+        Parallelogram,
+
+        House,
+        Pentagon,
+        Hexagon,
+        Septagon,
+        Octagon,
+
+        DoubleCircle,
+        DoubleOctagon,
+        TripleOctagon,
+
+        InvTriangle,
+        InvTrapezium,
+        InvHouse,
+
+        MDiamond,
+        MSquare,
+        MCircle,
+
+        Rect,
+        Rectangle,
+        Square,
+
+        Star,
+        Underline,
+        Cylinder,
+
+        Note,
+        Tab,
+        Folder,
+        Box3D,
+        Component,
+
+        Promoter,
+        Cds,
+        Terminator,
+        Utr,
+        PrimerSite,
+        RestrictionSite,
+        FivePOverhang,
+        ThreePOverhang,
+        NOverhang,
+        Assembly,
+        Signature,
+        Insulator,
+        Ribosite,
+        RnaStab,
+        Proteasesite,
+        ProteinStab,
+        RPromoter,
+        RArrow,
+        LArrow,
+        LPromoter,
+
+        Record,
+        MRecord
+    }
+}
