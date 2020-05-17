ccc69757be7c06f35ce4ac46842f4e6eb0bd75da
Mariusz Schimke <mariusz.schimke@gmail.com>
2020-05-17 16:34:00 +0200

    Added a double value attribute support

--- GiGraph.Dot.Entities/Attributes/Collections/IDotAttributeCollection.cs
+++ GiGraph.Dot.Entities/Attributes/Collections/IDotAttributeCollection.cs
@@ -23,6 +23,13 @@ namespace GiGraph.Dot.Entities.Attributes.Collections
         void Set(string key, string value);
 
         /// <summary>
+        /// Adds or replaces the specified double value attribute in the collection.
+        /// </summary>
+        /// <param name="key">The key of the attribute to include in the collection.</param>
+        /// <param name="value">The value of the attribute to include in the collection.</param>
+        void Set(string key, double value);
+
+        /// <summary>
         /// Adds or replaces the specified custom attribute in the collection.
         /// <para>
         ///     The value provided will be rendered in the generated graph exactly the way it is provided here,
