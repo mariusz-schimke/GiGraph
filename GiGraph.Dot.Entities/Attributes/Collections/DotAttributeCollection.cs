75663f5bc0f08180c097ac29e0d158333e0c674e
Mariusz Schimke <mariusz.schimke@gmail.com>
2020-05-17 16:34:00 +0200

    Added a double value attribute support

--- GiGraph.Dot.Entities/Attributes/Collections/DotAttributeCollection.cs
+++ GiGraph.Dot.Entities/Attributes/Collections/DotAttributeCollection.cs
@@ -22,6 +22,11 @@ namespace GiGraph.Dot.Entities.Attributes.Collections
             _attributes[key] = new DotStringAttribute(key, value);
         }
 
+        public virtual void Set(string key, double value)
+        {
+            _attributes[key] = new DotDoubleAttribute(key, value);
+        }
+
         public virtual void SetCustom(string key, string value)
         {
             _attributes[key] = new DotCustomAttribute(key, value);
