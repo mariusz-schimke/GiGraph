944251526e4326d88a13be1c026a2d7d06e7e4e2
Mariusz Schimke <mariusz.schimke@gmail.com>
2020-05-17 16:36:12 +0200

    Added a boolean value attribute support

--- GiGraph.Dot.Entities/Attributes/Collections/IDotAttributeCollection.cs
+++ GiGraph.Dot.Entities/Attributes/Collections/IDotAttributeCollection.cs
@@ -30,6 +30,13 @@ namespace GiGraph.Dot.Entities.Attributes.Collections
         void Set(string key, double value);
 
         /// <summary>
+        /// Adds or replaces the specified boolean value attribute in the collection.
+        /// </summary>
+        /// <param name="key">The key of the attribute to include in the collection.</param>
+        /// <param name="value">The value of the attribute to include in the collection.</param>
+        void Set(string key, bool value);
+
+        /// <summary>
         /// Adds or replaces the specified custom attribute in the collection.
         /// <para>
         ///     The value provided will be rendered in the generated graph exactly the way it is provided here,
---
+++ GiGraph.Dot.Entities/Attributes/DotBoolAttribute.cs
@@ -0,0 +1,23 @@
+﻿namespace GiGraph.Dot.Entities.Attributes
+{
+    /// <summary>
+    /// A boolean value attribute.
+    /// </summary>
+    public class DotBoolAttribute : DotAttribute<bool>
+    {
+        /// <summary>
+        /// Creates a new instance of the attribute.
+        /// </summary>
+        /// <param name="key">The key of the attribute.</param>
+        /// <param name="value">The value of the attribute.</param>
+        public DotBoolAttribute(string key, bool value)
+            : base(key, value)
+        {
+        }
+
+        protected override string GetDotEncodedValue()
+        {
+            return Value ? "true" : "false";
+        }
+    }
+}
