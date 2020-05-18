2a85eb6222f2f26af87636e2289138cc0426dae4
Mariusz Schimke <mariusz.schimke@gmail.com>
2020-05-18 17:17:29 +0200

    DotStyle descriptions improved

--- GiGraph.Dot.Entities/Attributes/Enums/DotStyle.cs
+++ GiGraph.Dot.Entities/Attributes/Enums/DotStyle.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using GiGraph.Dot.Entities.Graphs;
+using System;
 
 namespace GiGraph.Dot.Entities.Attributes.Enums
 {
@@ -9,44 +10,45 @@ namespace GiGraph.Dot.Entities.Attributes.Enums
     public enum DotStyle
     {
         /// <summary>
-        /// Used internally to indicate that a normal default style should be assigned to the element.
-        /// Can be used in order to restore the style of an element to a default value when a default style
-        /// has been set for this element type on a graph or a subgraph level.
+        /// Indicates that the normal default style should be assigned to the current element.
+        /// This setting can be used in order to restore the style of the element to the normal default value
+        /// when a custom default style has been set for this type of element on a graph or a subgraph level,
+        /// using <see cref="DotCommonGraph.EdgeDefaults"/> or <see cref="DotCommonGraph.NodeDefaults"/>.
         /// </summary>
         Default = 0,
 
         /// <summary>
-        /// Applicable to nodes and edges only.
+        /// Applicable to nodes, edges, and clusters.
         /// </summary>
         Solid = 1 << 0,
 
         /// <summary>
-        /// Applicable to nodes and edges only.
+        /// Applicable to nodes, edges, and clusters.
         /// </summary>
         Dashed = 1 << 1,
 
         /// <summary>
-        /// Applicable to nodes and edges only.
+        /// Applicable to nodes, edges, and clusters.
         /// </summary>
         Dotted = 1 << 2,
 
         /// <summary>
-        /// Applicable to nodes, edges, and clusters only.
+        /// Applicable to nodes, edges, and clusters.
         /// </summary>
         Bold = 1 << 3,
 
         /// <summary>
-        /// Applicable to nodes and clusters only.
+        /// Applicable to nodes and clusters.
         /// </summary>
         Rounded = 1 << 4,
 
         /// <summary>
-        /// Applicable to nodes only.
+        /// Applicable to nodes.
         /// </summary>
         Diagonals = 1 << 5,
 
         /// <summary>
-        /// Applicable to nodes and clusters only.
+        /// Applicable to nodes and clusters.
         /// </summary>
         Filled = 1 << 6,
 
@@ -60,7 +62,7 @@ namespace GiGraph.Dot.Entities.Attributes.Enums
         Striped = 1 << 7,
 
         /// <summary>
-        /// Applicable to nodes only. Allowed only for elliptically-shaped nodes.
+        /// Applicable to elliptically-shaped nodes.
         /// Causes the fill to be done as a set of wedges. The colors are specified via a color list, 
         /// with the colors drawn counter-clockwise starting at angle 0.
         /// Optional color weights can be specified to indicate the proportional widths of the bars.
@@ -69,17 +71,18 @@ namespace GiGraph.Dot.Entities.Attributes.Enums
         Wedged = 1 << 8,
 
         /// <summary>
-        /// Applicable to nodes, clusters and graphs. Indicates a radial-style gradient fill if applicable.
+        /// Applicable to nodes, clusters and graphs. Indicates a radial-style gradient fill
+        /// when colors are specified via a color list.
         /// </summary>
         Radial = 1 << 9,
 
         /// <summary>
-        /// Applicable to edges only.
+        /// Applicable to edges.
         /// </summary>
         Tapered = 1 << 10,
 
         /// <summary>
-        /// Applicable to nodes and edges only.
+        /// Applicable to nodes and edges.
         /// </summary>
         Invisible = 1 << 11
     }
