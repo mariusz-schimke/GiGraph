b17a60f0983cffb5bf45aea424fdf33a7fd1c41c
Mariusz Schimke <mariusz.schimke@gmail.com>
2020-05-22 22:12:24 +0200

    DotShape naming cleanup

--- GiGraph.Dot.Entities/Attributes/Enums/DotShape.cs
+++ GiGraph.Dot.Entities/Attributes/Enums/DotShape.cs
@@ -1,86 +1,217 @@
 ﻿namespace GiGraph.Dot.Entities.Attributes.Enums
 {
     /// <summary>
-    /// The node shape. To see how each shape is rendered,
-    /// please go to <see cref="https://www.graphviz.org/doc/info/shapes.html"/>.
+    /// The node shape.
+    /// <see href="https://www.graphviz.org/doc/info/shapes.html">View how individual shapes are visualized</see>.
     /// </summary>
     public enum DotShape
     {
+        /// <summary>
+        /// No shape, just label. Synonymous to <see cref="PlainText"/>.
+        /// </summary>
         None,
 
-        Box,
-        Polygon,
+        /// <summary>
+        /// No shape, just label. Similar to <see cref="PlainText"/> and <see cref="None"/>, except that it also enforces
+        /// width = 0, height = 0, and margin = 0, which guarantees that the actual size of the node
+        /// is entirely determined by the label. This is useful, for example, when using HTML-like labels.
+        /// </summary>
+        Plain,
+
+        /// <summary>
+        /// No shape, just label. Synonymous to <see cref="None"/>.
+        /// </summary>
+        PlainText,
+
+        Underline,
+
+        /// <summary>
+        /// The point shape. The way it is visualized is only affected by the peripheries, width, and height attributes.
+        /// </summary>
+        Point,
+
+        Circle,
+        MCircle,
+        DoubleCircle,
 
         Ellipse,
         Oval,
-        Circle,
-
-        Point,
         Egg,
 
-        Triangle,
+        /// <summary>
+        /// A rectangular shape. Synonymous to <see cref="Rect"/> and <see cref="Rectangle"/>.
+        /// </summary>
+        Box,
 
-        PlainText,
-        Plain,
+        /// <summary>
+        /// A cuboid.
+        /// </summary>
+        Box3D,
+
+        /// <summary>
+        /// A rectangular shape. Synonymous to <see cref="Rectangle"/> and <see cref="Box"/>.
+        /// </summary>
+        Rect,
+
+        /// <summary>
+        /// A rectangular shape. Synonymous to <see cref="Rect"/> and <see cref="Box"/>.
+        /// </summary>
+        Rectangle,
+
+        Cylinder,
+
+        Square,
+        MSquare,
+
+        Triangle,
+        InvertedTriangle,
 
         Diamond,
-        Trapezium,
+        MDiamond,
+
         Parallelogram,
 
-        House,
+        /// <summary>
+        /// A polygon shape. When set, the attributes sides, skew, and distortion are also used.
+        /// </summary>
+        Polygon,
+
         Pentagon,
         Hexagon,
         Septagon,
-        Octagon,
 
-        DoubleCircle,
+        Octagon,
         DoubleOctagon,
         TripleOctagon,
 
-        InvTriangle,
-        InvTrapezium,
-        InvHouse,
+        Trapezium,
+        InvertedTrapezium,
 
-        MDiamond,
-        MSquare,
-        MCircle,
-
-        Rect,
-        Rectangle,
-        Square,
+        House,
+        InvertedHouse,
 
         Star,
-        Underline,
-        Cylinder,
 
         Note,
-        Tab,
         Folder,
-        Box3D,
         Component,
+        Tab,
 
-        Promoter,
-        Cds,
-        Terminator,
-        Utr,
-        PrimerSite,
-        RestrictionSite,
-        FivePOverhang,
-        ThreePOverhang,
-        NOverhang,
-        Assembly,
-        Signature,
-        Insulator,
-        Ribosite,
-        RnaStab,
-        Proteasesite,
-        ProteinStab,
-        RPromoter,
-        RArrow,
-        LArrow,
-        LPromoter,
-
+        /// <summary>
+        /// Visually, a record is a box, with fields represented by alternating rows of horizontal or vertical subboxes.
+        /// Use an HTML label to define its structure
+        /// (<see href="https://www.graphviz.org/doc/info/shapes.html#record">learn more</see>).
+        /// </summary>
         Record,
-        MRecord
+
+        /// <summary>
+        /// Visually, this type of record shape is a box with rounded corers, and fields represented by alternating rows
+        /// of horizontal or vertical subboxes. Use an HTML label to define its structure
+        /// (<see href="https://www.graphviz.org/doc/info/shapes.html#record">learn more</see>).
+        /// </summary>
+        MRecord,
+
+
+        // --- SYNTHETIC BIOLOGY SHAPES ---
+
+        /// <summary>
+        /// Synthetic biology: coding sequence (CDS).
+        /// </summary>
+        Cds,
+
+        /// <summary>
+        /// Synthetic biology: signature.
+        /// </summary>
+        Signature,
+
+        /// <summary>
+        /// Synthetic biology: left arrow.
+        /// </summary>
+        LeftArrow,
+
+        /// <summary>
+        /// Synthetic biology: right arrow.
+        /// </summary>
+        RightArrow,
+
+        /// <summary>
+        /// Synthetic biology: left promoter.
+        /// </summary>
+        LeftPromoter,
+
+        /// <summary>
+        /// Synthetic biology: right promoter.
+        /// </summary>
+        RightPromoter,
+
+        /// <summary>
+        /// Synthetic biology: promoter.
+        /// </summary>
+        Promoter,
+
+        /// <summary>
+        /// Synthetic biology: untranslated region (UTR).
+        /// </summary>
+        Utr,
+
+        /// <summary>
+        /// Synthetic biology: terminator.
+        /// </summary>
+        Terminator,
+
+        /// <summary>
+        /// Synthetic biology: primer site.
+        /// </summary>
+        PrimerSite,
+
+        /// <summary>
+        /// Synthetic biology: restriction site.
+        /// </summary>
+        RestrictionSite,
+
+        /// <summary>
+        /// Synthetic biology: 3' overhang.
+        /// </summary>
+        ThreePrimeOverhang,
+
+        /// <summary>
+        /// Synthetic biology: 5' overhang.
+        /// </summary>
+        FivePrimeOverhang,
+
+        /// <summary>
+        /// Synthetic biology: n overhang.
+        /// </summary>
+        NOverhang,
+
+        /// <summary>
+        /// Synthetic biology: assembly.
+        /// </summary>
+        Assembly,
+
+        /// <summary>
+        /// Synthetic biology: insulator.
+        /// </summary>
+        Insulator,
+
+        /// <summary>
+        /// Synthetic biology: ribo site.
+        /// </summary>
+        RiboSite,
+
+        /// <summary>
+        /// Synthetic biology: protease site.
+        /// </summary>
+        ProteaseSite,
+
+        /// <summary>
+        /// Synthetic biology: RNA stab.
+        /// </summary>
+        RnaStab,
+
+        /// <summary>
+        /// Synthetic biology: protein stab.
+        /// </summary>
+        ProteinStab
     }
 }
