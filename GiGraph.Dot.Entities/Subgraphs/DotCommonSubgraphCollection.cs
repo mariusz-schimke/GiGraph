1452a9540bcb7468e3caf3a3e6c0f6d970cb1210
Mariusz Schimke <mariusz.schimke@gmail.com>
2020-05-23 15:07:55 +0200

    Subgraph collection extended

--- GiGraph.Dot.Entities/Subgraphs/DotCommonSubgraphCollection.cs
+++ GiGraph.Dot.Entities/Subgraphs/DotCommonSubgraphCollection.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using GiGraph.Dot.Entities.Attributes.Enums;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 
@@ -12,9 +13,68 @@ namespace GiGraph.Dot.Entities.Subgraphs
         /// Adds a subgraph to the collection.
         /// </summary>
         /// <param name="subgraph">The subgraph to add.</param>
-        public virtual DotCommonSubgraph Add(DotCommonSubgraph subgraph)
+        public virtual T Add<T>(T subgraph)
+            where T : DotCommonSubgraph
         {
             _subgraphs.Add(subgraph);
+            return subgraph;
+        }
+
+        /// <summary>
+        /// Adds a new cluster subgraph to the collection.
+        /// </summary>
+        /// <param name="init">An optional initialization delegate.</param>
+        public virtual DotCluster AddCluster(Action<DotCluster> init = null)
+        {
+            return AddCluster(id: null, init);
+        }
+
+        /// <summary>
+        /// Adds a new cluster subgraph to the collection.
+        /// </summary>
+        /// <param name="id">The unique identifier of the subgraph. Pass null if no identifier should be used.</param>
+        /// <param name="init">An optional initialization delegate.</param>
+        public virtual DotCluster AddCluster(string id, Action<DotCluster> init = null)
+        {
+            var cluster = Add(new DotCluster(id));
+            init?.Invoke(cluster);
+            return cluster;
+        }
+
+        /// <summary>
+        /// Adds a new subgraph to the collection.
+        /// </summary>
+        /// <param name="init">An optional initialization delegate.</param>
+        public virtual DotSubgraph AddSubgraph(Action<DotSubgraph> init = null)
+        {
+            return DoAddSubgraph(id: null, rank: null, init);
+        }
+
+        /// <summary>
+        /// Adds a new subgraph to the collection.
+        /// </summary>
+        /// <param name="rank">The rank attribute to assign to the subgraph.</param>
+        /// <param name="init">An optional initialization delegate.</param>
+        public virtual DotSubgraph AddSubgraph(DotRank rank, Action<DotSubgraph> init = null)
+        {
+            return DoAddSubgraph(id: null, rank, init);
+        }
+
+        /// <summary>
+        /// Adds a new subgraph to the collection.
+        /// </summary>
+        /// <param name="id">The unique identifier of the subgraph. Pass null if no identifier should be used.</param>
+        /// <param name="rank">The rank attribute to assign to the subgraph.</param>
+        /// <param name="init">An optional initialization delegate.</param>
+        public virtual DotSubgraph AddSubgraph(string id, DotRank rank, Action<DotSubgraph> init = null)
+        {
+            return DoAddSubgraph(id, rank, init);
+        }
+
+        protected virtual DotSubgraph DoAddSubgraph(string id, DotRank? rank, Action<DotSubgraph> init)
+        {
+            var subgraph = Add(new DotSubgraph(id, rank));
+            init?.Invoke(subgraph);
             return subgraph;
         }
 
