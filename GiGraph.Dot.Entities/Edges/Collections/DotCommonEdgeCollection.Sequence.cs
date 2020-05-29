822e1a98d69bb7a53ffc303ba8e27ac2b96d5d93
Mariusz Schimke <mariusz.schimke@gmail.com>
2020-05-29 23:56:59 +0200

    New methods added to edge collection (AddSequence)

--- GiGraph.Dot.Entities/Edges/Collections/DotCommonEdgeCollection.Sequence.cs
+++ GiGraph.Dot.Entities/Edges/Collections/DotCommonEdgeCollection.Sequence.cs
@@ -1,4 +1,5 @@
 ﻿using GiGraph.Dot.Entities.Attributes.Collections;
+using GiGraph.Dot.Entities.Edges.Endpoints;
 using System;
 using System.Collections.Generic;
 
@@ -9,7 +10,7 @@ namespace GiGraph.Dot.Entities.Edges.Collections
         /// <summary>
         /// Adds a sequence of edges that join consecutive nodes with the specified identifiers.
         /// </summary>
-        /// <param name="nodeIds">The identifiers of consecutive nodes to connect with edges.</param>
+        /// <param name="nodeIds">The identifiers of consecutive nodes to connect with edges (at least a pair is required).</param>
         public virtual DotEdgeSequence AddSequence(params string[] nodeIds)
         {
             return AddSequence(nodeIds, initEdge: null);
@@ -19,7 +20,7 @@ namespace GiGraph.Dot.Entities.Edges.Collections
         /// Adds a sequence of edges that connect consecutive nodes with the specified identifiers.
         /// </summary>
         /// <param name="initEdge">An optional initializer delegate to call for the attributes of the created sequence.</param>
-        /// <param name="nodeIds">The identifiers of consecutive nodes to connect with edges.</param>
+        /// <param name="nodeIds">The identifiers of consecutive nodes to connect with edges (at least a pair is required).</param>
         public virtual DotEdgeSequence AddSequence(Action<IDotEdgeAttributes> initEdge, params string[] nodeIds)
         {
             return AddSequence(nodeIds, initEdge);
@@ -28,11 +29,43 @@ namespace GiGraph.Dot.Entities.Edges.Collections
         /// <summary>
         /// Adds a sequence of edges that connect consecutive nodes with the specified identifiers.
         /// </summary>
-        /// <param name="nodeIds">The identifiers of consecutive nodes to connect with edges.</param>
+        /// <param name="nodeIds">The identifiers of consecutive nodes to connect with edges (at least a pair is required).</param>
         /// <param name="initEdge">An optional initializer delegate to call for the attributes of the created sequence.</param>
         public virtual DotEdgeSequence AddSequence(IEnumerable<string> nodeIds, Action<IDotEdgeAttributes> initEdge = null)
         {
             return Add(DotEdgeSequence.FromNodes(nodeIds), initEdge);
+        }
+
+        /// <summary>
+        /// Adds a sequence of edges that connect the specified endpoints consecutively.
+        /// At least a pair of endpoints has to be provided.
+        /// </summary>
+        /// <param name="endpoints">The endpoints to initialize the instance with.</param>
+        public virtual DotEdgeSequence AddSequence(params DotCommonEndpoint[] endpoints)
+        {
+            return AddSequence(endpoints, initEdge: null);
+        }
+
+        /// <summary>
+        /// Adds a sequence of edges that connect the specified endpoints consecutively.
+        /// At least a pair of endpoints has to be provided.
+        /// </summary>
+        /// <param name="initEdge">An optional initializer delegate to call for the attributes of the created sequence.</param>
+        /// <param name="endpoints">The endpoints to initialize the instance with.</param>
+        public virtual DotEdgeSequence AddSequence(Action<IDotEdgeAttributes> initEdge, params DotCommonEndpoint[] endpoints)
+        {
+            return AddSequence(endpoints, initEdge);
+        }
+
+        /// <summary>
+        /// Adds a sequence of edges that connect the specified endpoints consecutively.
+        /// At least a pair of endpoints has to be provided.
+        /// </summary>
+        /// <param name="endpoints">The endpoints to initialize the instance with.</param>
+        /// <param name="initEdge">An optional initializer delegate to call for the attributes of the created sequence.</param>
+        public virtual DotEdgeSequence AddSequence(IEnumerable<DotCommonEndpoint> endpoints, Action<IDotEdgeAttributes> initEdge = null)
+        {
+            return Add(new DotEdgeSequence(endpoints), initEdge);
         }
     }
 }
