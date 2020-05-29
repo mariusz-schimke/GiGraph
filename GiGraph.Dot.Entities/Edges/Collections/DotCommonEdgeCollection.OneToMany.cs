ba6f9f3dea14430750668c43526e6ddba36b0708
Mariusz Schimke <mariusz.schimke@gmail.com>
2020-05-29 23:46:16 +0200

    New methods added to edge collection (AddOneToMany)

--- GiGraph.Dot.Entities/Edges/Collections/DotCommonEdgeCollection.OneToMany.cs
+++ GiGraph.Dot.Entities/Edges/Collections/DotCommonEdgeCollection.OneToMany.cs
@@ -1,4 +1,6 @@
 ﻿using GiGraph.Dot.Entities.Attributes.Collections;
+using GiGraph.Dot.Entities.Edges.Endpoints;
+using GiGraph.Dot.Entities.Subgraphs;
 using System;
 using System.Collections.Generic;
 
@@ -39,6 +41,28 @@ namespace GiGraph.Dot.Entities.Edges.Collections
         public virtual DotOneToManyEdgeGroup AddOneToMany(string tailNodeId, IEnumerable<string> headNodeIds, Action<IDotEdgeAttributes> initEdge = null)
         {
             return Add(new DotOneToManyEdgeGroup(tailNodeId, headNodeIds), initEdge);
+        }
+
+        /// <summary>
+        /// Adds a group of edges where the tail endpoint is connected to all nodes in the specified head endpoint group.
+        /// </summary>
+        /// <param name="tail">The tail (source, left) node.</param>
+        /// <param name="head">The group whose nodes (as the head endpoints) the <paramref name="tail"/> node should be connected to.</param>
+        /// <param name="initEdge">An optional initializer delegate to call for the attributes of the created edge group.</param>
+        public virtual DotOneToManyEdgeGroup AddOneToMany(DotEndpoint tail, DotEndpointGroup head, Action<IDotEdgeAttributes> initEdge = null)
+        {
+            return Add(new DotOneToManyEdgeGroup(tail, head), initEdge);
+        }
+
+        /// <summary>
+        /// Adds a group of edges where the tail endpoint is connected to all nodes in the specified head subgraph.
+        /// </summary>
+        /// <param name="tailNodeId">The identifier of the tail (source, left) node.</param>
+        /// <param name="head">The subgraph whose nodes (as the head endpoints) the <paramref name="tailNodeId"/> node should be connected to.</param>
+        /// <param name="initEdge">An optional initializer delegate to call for the attributes of the created edge group.</param>
+        public virtual DotOneToManyEdgeGroup AddOneToMany(string tailNodeId, DotSubgraph head, Action<IDotEdgeAttributes> initEdge = null)
+        {
+            return Add(new DotOneToManyEdgeGroup(tailNodeId, head), initEdge);
         }
     }
 }
