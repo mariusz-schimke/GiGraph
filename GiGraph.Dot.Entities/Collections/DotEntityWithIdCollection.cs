d9c45d9a81b9fe1d7a539cda793c1626b794e76c
Mariusz Schimke <mariusz.schimke@gmail.com>
2020-05-30 12:13:40 +0200

    Added a common entity collection to clean up redundant code

---
+++ GiGraph.Dot.Entities/Collections/DotEntityWithIdCollection.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+
+namespace GiGraph.Dot.Entities.Collections
+{
+    public abstract class DotEntityWithIdCollection<T> : List<T>
+        where T : IDotEntity
+    {
+        /// <summary>
+        /// Searches for an item with the specified identifier, and returns the zero-based index of the first occurrence
+        /// within the collection.
+        /// </summary>
+        /// <param name="id">The identifier of the items to search.</param>
+        public abstract int IndexOf(string id);
+
+        /// <summary>
+        /// Determines whether the specified items is in the collection.
+        /// </summary>
+        /// <param name="id">The identifier of the items to locate in the collection.</param>
+        public virtual bool Contains(string id)
+        {
+            return IndexOf(id) >= 0;
+        }
+
+        /// <summary>
+        /// Removes the first occurrence of an item with the specified identifier from the collection.
+        /// </summary>
+        /// <param name="id">The identifier of the item to remove.</param>
+        public virtual bool Remove(string id)
+        {
+            var index = IndexOf(id);
+
+            if (index >= 0)
+            {
+                RemoveAt(index);
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
