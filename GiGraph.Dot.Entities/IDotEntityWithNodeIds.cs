5ba7bb7c9680b53746167b7ef8f9d295bebeb014
Mariusz Schimke <mariusz.schimke@gmail.com>
2020-05-30 18:38:01 +0200

    Redundant interface removed

--- GiGraph.Dot.Entities/IDotEntityWithNodeIds.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-
-namespace GiGraph.Dot.Entities
-{
-    public interface IDotEntityWithNodeIds : IDotEntity
-    {
-        IEnumerable<string> NodeIds { get; }
-    }
-}
--- GiGraph.Dot.Entities/Nodes/DotNode.cs
+++ GiGraph.Dot.Entities/Nodes/DotNode.cs
@@ -1,13 +1,12 @@
 ﻿using GiGraph.Dot.Entities.Attributes.Collections;
 using System;
-using System.Collections.Generic;
 
 namespace GiGraph.Dot.Entities.Nodes
 {
     /// <summary>
     /// Represents a graph node.
     /// </summary>
-    public class DotNode : DotCommonNode, IDotEntityWithNodeIds
+    public class DotNode : DotCommonNode
     {
         protected string _id;
 
@@ -19,8 +18,6 @@ namespace GiGraph.Dot.Entities.Nodes
             get => _id;
             set => _id = value ?? throw new ArgumentNullException(nameof(Id), "Node identifier cannot be null.");
         }
-
-        IEnumerable<string> IDotEntityWithNodeIds.NodeIds => new[] { _id };
 
         /// <summary>
         /// The attributes of the node.
