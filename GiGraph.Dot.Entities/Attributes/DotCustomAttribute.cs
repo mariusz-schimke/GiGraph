002595f3df1281f0b61e05c36343e025c38e01e7
mariusz.schimke <mariusz.schimke@gmail.com>
2020-06-06 21:35:47 +0200

    Escaping added to DotLogicalEndpointAttribute

--- GiGraph.Dot.Entities/Attributes/DotCustomAttribute.cs
+++ GiGraph.Dot.Entities/Attributes/DotCustomAttribute.cs
@@ -1,7 +1,8 @@
 ﻿namespace GiGraph.Dot.Entities.Attributes
 {
     /// <summary>
-    /// An attribute with any key and any value that are supported by a destination DOT graph renderer.
+    /// An attribute with any key and any value that are directly supported by a destination DOT graph renderer.
+    /// The value specified for the attribute will be rendered as is in the output script (without escaping).
     /// </summary>
     public class DotCustomAttribute : DotCommonAttribute<string>
     {
@@ -9,7 +10,8 @@
         /// Creates a new custom attribute.
         /// </summary>
         /// <param name="key">The key of the attribute.</param>
-        /// <param name="value">The value of the attribute in a format understood by a destination DOT graph renderer.</param>
+        /// <param name="value">The value of the attribute in a format understood by a destination DOT graph renderer.
+        /// The value will be rendered as is in the output script (without escaping).</param>
         public DotCustomAttribute(string key, string value)
             : base(key, value)
         {
