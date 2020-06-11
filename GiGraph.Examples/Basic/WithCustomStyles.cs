59481f38e990adaae9747d029c12e18daa47a8d8
mariusz.schimke <mariusz.schimke@gmail.com>
2020-06-11 19:44:40 +0200

    Styles example updated

--- GiGraph.Examples/Basic/WithCustomStyles.cs
+++ GiGraph.Examples/Basic/WithCustomStyles.cs
@@ -14,7 +14,6 @@ namespace GiGraph.Examples.Basic
             // set left to right layout direction of the graph using graph attributes
             graph.Attributes.LayoutDirection = DotRankDirection.LeftToRight;
 
-
             // set the defaults for all nodes of the graph
             graph.NodeDefaults.Shape = DotShape.Rectangle;
             graph.NodeDefaults.Style = DotStyle.Filled;
@@ -31,14 +30,19 @@ namespace GiGraph.Examples.Basic
 
             // -- add nodes --
 
+            // add nodes with a two-color fill, and fill proportions specified by the weight parameter
             graph.Nodes.Add("a").Attributes.FillColor = DotColorDefinition.From(Color.RoyalBlue, Color.Turquoise, weight2: 0.25);
             graph.Nodes.Add("b").Attributes.FillColor = DotColorDefinition.From(Color.Navy, Color.RoyalBlue, weight1: 0.25);
 
+            // a rectangular node with a striped fill
             graph.Nodes.Add("striped", attrs =>
             {
+                // set style to striped
                 attrs.Style = DotStyle.Filled | DotStyle.Striped;
 
                 attrs.Color = Color.Transparent;
+
+                // set the colors of individual stripes and their proportions
                 attrs.FillColor = DotColorDefinition.From(
                     new DotWeightedColor(Color.Navy, 0.1),
                     Color.RoyalBlue,
@@ -46,12 +50,17 @@ namespace GiGraph.Examples.Basic
                     Color.Orange);
             });
 
+            // a circular node with a wedged fill
             graph.Nodes.Add("wedged", attrs =>
             {
                 attrs.Shape = DotShape.Circle;
+
+                // set wedged style
                 attrs.Style = DotStyle.Filled | DotStyle.Wedged;
 
                 attrs.Color = Color.Transparent;
+
+                // set the colors of individual wedges and their proportions
                 attrs.FillColor = DotColorDefinition.From(
                     Color.Orange,
                     Color.RoyalBlue,
@@ -59,11 +68,12 @@ namespace GiGraph.Examples.Basic
                     Color.Turquoise);
             });
 
-            // use a subgraph to override the default attributes for a group of nodes and/or edges
+
+            // a subgraph example – to override the default attributes for a group of nodes and/or edges
             graph.Subgraphs.Add(sg =>
             {
                 sg.NodeDefaults.Color = Color.RoyalBlue;
-                sg.NodeDefaults.FillColor = Color.White;
+                sg.NodeDefaults.FillColor = Color.Orange;
                 sg.NodeDefaults.Shape = DotShape.Circle;
 
                 sg.EdgeDefaults.Color = Color.RoyalBlue;
@@ -74,24 +84,28 @@ namespace GiGraph.Examples.Basic
 
             // -- add edges --
 
-            graph.Edges.Add("c", "d", attrs =>
-            {
-                attrs.Label = "parallel splines";
-                attrs.ArrowDirection = DotArrowDirection.Both;
-                attrs.Color = DotColorDefinition.From(Color.Turquoise, Color.RoyalBlue);
-            });
-
             graph.Edges.Add("a", "b", attrs =>
             {
                 attrs.Label = "multicolor series";
                 attrs.ArrowDirection = DotArrowDirection.Both;
 
+                // this will render a multicolor edge, where each color may optionally have a proportion specified by the weight parameter
                 attrs.Color = DotColorDefinition.From(
                     new DotWeightedColor(Color.Turquoise, 0.33),
                     new DotWeightedColor(Color.Gray, 0.33),
                     Color.Navy);
             });
+            
+            graph.Edges.Add("c", "d", attrs =>
+            {
+                attrs.Label = "parallel splines";
+                attrs.ArrowDirection = DotArrowDirection.Both;
 
+                // this will render two parallel splines (but more of them can be added by adding further colors)
+                attrs.Color = DotColorDefinition.From(Color.Turquoise, Color.RoyalBlue);
+            });
+
+            // a dotted edge
             graph.Edges.Add("e", "f", attrs =>
             {
                 attrs.Label = "dotted";
