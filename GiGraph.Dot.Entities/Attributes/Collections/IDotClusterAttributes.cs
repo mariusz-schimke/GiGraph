548864b15f1777b186c7c9013f5b902fbf12eb6d
mariusz.schimke <mariusz.schimke@gmail.com>
2020-06-12 23:11:29 +0200

    FontName and FontPath attributes support added

--- GiGraph.Dot.Entities/Attributes/Collections/IDotClusterAttributes.cs
+++ GiGraph.Dot.Entities/Attributes/Collections/IDotClusterAttributes.cs
@@ -12,7 +12,9 @@ namespace GiGraph.Dot.Entities.Attributes.Collections
         string Label { get; set; }
 
         /// <summary>
-        /// Gets or sets the label in HTML format to display on the cluster.
+        /// <para>
+        ///     Gets or sets the label in HTML format to display on the cluster.
+        /// </para>
         /// <para>
         ///     <see cref="Label"/> and <see cref="LabelHtml"/> are actually the same attribute in a different format,
         ///     so when one is set, the other is replaced.
@@ -33,31 +35,35 @@ namespace GiGraph.Dot.Entities.Attributes.Collections
         DotColorDefinition Color { get; set; }
 
         /// <summary>
-        /// Gets or sets the background color of the cluster (default: none).
-        /// Used as the initial background for the cluster. If the <see cref="Style"/> attribute of the cluster
-        /// contains the <see cref="DotStyle.Filled"/> style, the cluster's <see cref="FillColor"/> will overlay the background color.
         /// <para>
-        /// When <see cref="DotColorList"/> is used, a gradient fill is generated. By default, this is a linear fill;
-        /// setting <see cref="Style"/> to <see cref="DotStyle.Radial"/> will cause a radial fill.
-        /// At present, only two colors are used. If the second color is <see cref="System.Drawing.Color.Empty"/>,
-        /// the default color is used for it. See also the <see cref="GradientAngle"/> attribute for setting the gradient angle.
+        ///     Gets or sets the background color of the cluster (default: none).
+        ///     Used as the initial background for the cluster. If the <see cref="Style"/> attribute of the cluster
+        ///     contains the <see cref="DotStyle.Filled"/> style, the cluster's <see cref="FillColor"/> will overlay the background color.
+        /// </para>
+        /// <para>
+        ///     When <see cref="DotColorList"/> is used, a gradient fill is generated. By default, this is a linear fill;
+        ///     setting <see cref="Style"/> to <see cref="DotStyle.Radial"/> will cause a radial fill.
+        ///     At present, only two colors are used. If the second color is <see cref="System.Drawing.Color.Empty"/>,
+        ///     the default color is used for it. See also the <see cref="GradientAngle"/> attribute for setting the gradient angle.
         /// </para>
         /// </summary>
         DotColorDefinition BackgroundColor { get; set; }
 
         /// <summary>
-        /// Gets or sets the color used to fill the background of the cluster, assuming that <see cref="Style"/> is <see cref="DotStyle.Filled"/>.
-        /// If <see cref="FillColor"/> is not defined, <see cref="Color"/> is used. 
-        /// If <see cref="Color"/> is not defined, <see cref="BackgroundColor"/> is used.
-        /// If it is not defined too, the default is used, except when the output format is MIF, which use black by default.
         /// <para>
-        /// When <see cref="DotColorList"/> is used, a gradient fill is generated. By default, this is a linear fill; 
-        /// setting <see cref="Style"/> to <see cref="DotStyle.Radial"/> will cause a radial fill.
-        /// At present, only two colors are used. If the second color is missing, the default color is used for it.
-        /// See also the <see cref="GradientAngle"/> attribute for setting the gradient angle.
-        /// Note that a cluster inherits the root graph's attributes if defined. 
-        /// Thus, if the root graph has defined a fill color (<see cref="IDotGraphAttributes.FillColor"/>), this will override a 
-        /// <see cref="Color"/> or <see cref="BackgroundColor"/> attribute set for the cluster.
+        ///     Gets or sets the color used to fill the background of the cluster, assuming that <see cref="Style"/> is <see cref="DotStyle.Filled"/>.
+        ///     If <see cref="FillColor"/> is not defined, <see cref="Color"/> is used. 
+        ///     If <see cref="Color"/> is not defined, <see cref="BackgroundColor"/> is used.
+        ///     If it is not defined too, the default is used, except when the output format is MIF, which use black by default.
+        /// </para>
+        /// <para>
+        ///     When <see cref="DotColorList"/> is used, a gradient fill is generated. By default, this is a linear fill; 
+        ///     setting <see cref="Style"/> to <see cref="DotStyle.Radial"/> will cause a radial fill.
+        ///     At present, only two colors are used. If the second color is missing, the default color is used for it.
+        ///     See also the <see cref="GradientAngle"/> attribute for setting the gradient angle.
+        ///     Note that a cluster inherits the root graph's attributes if defined. 
+        ///     Thus, if the root graph has defined a fill color (<see cref="IDotGraphAttributes.FillColor"/>), this will override a 
+        ///     <see cref="Color"/> or <see cref="BackgroundColor"/> attribute set for the cluster.
         /// </para>
         /// </summary>
         DotColorDefinition FillColor { get; set; }
@@ -87,8 +93,10 @@ namespace GiGraph.Dot.Entities.Attributes.Collections
         Color? PenColor { get; set; }
 
         /// <summary>
-        /// Sets the style of the cluster (default: null). See the descriptions of individual <see cref="DotStyle"/> values
-        /// to learn which styles are applicable to this element type.
+        /// <para>
+        ///     Sets the style of the cluster (default: null). See the descriptions of individual <see cref="DotStyle"/> values
+        ///     to learn which styles are applicable to this element type.
+        /// </para>
         /// <para>
         ///     Multiple styles can be used at once, for example:
         ///     <code>
@@ -102,5 +110,34 @@ namespace GiGraph.Dot.Entities.Attributes.Collections
         /// Gets or sets the color used for text (default: <see cref="System.Drawing.Color.Black"/>).
         /// </summary>
         Color? FontColor { get; set; }
+
+        /// <summary>
+        /// <para>
+        ///     Gets or sets the font used for text (default: "Times-Roman"). This very much depends on the output format and,
+        ///     for non-bitmap output such as PostScript or SVG, the availability of the font when the graph is displayed or printed.
+        ///     As such, it is best to rely on font faces that are generally available, such as Times-Roman, Helvetica or Courier.
+        /// </para>
+        /// <para>
+        ///     How font names are resolved also depends on the underlying library that handles font name resolution.
+        ///     If Graphviz was built using the fontconfig library, the latter library will be used to search for the font.
+        ///     See the commands fc-list, fc-match and the other fontconfig commands for how names are resolved and which fonts are available.
+        ///     Other systems may provide their own font package, such as Quartz for OS X.
+        /// </para>
+        /// <para>
+        ///     Note that various font attributes, such as weight and slant, can be built into the font name.
+        ///     Unfortunately, the syntax varies depending on which font system is dominant.
+        ///     Thus, using <see cref="FontName"/> = "times bold italic" will produce a bold, slanted Times font using Pango,
+        ///     the usual main font library. Alternatively, <see cref="FontName"/> = "times:italic" will produce a slanted Times font
+        ///     from fontconfig, while <see cref="FontName"/> = "times-bold" will resolve to a bold Times using Quartz.
+        ///     You will need to ascertain which package is used by your Graphviz system and refer to the relevant documentation.
+        /// </para>
+        /// <para>
+        ///     If Graphviz is not built with a high-level font library, <see cref="FontName"/> will be considered the name
+        ///     of a Type 1 or True Type font file. If you specify <see cref="FontName"/> = "schlbk", the tool will look
+        ///     for a file named schlbk.ttf or schlbk.pfa or schlbk.pfb in one of the directories specified
+        ///     by the <see cref="IDotGraphAttributes.FontPath"/> attribute. The lookup does support various aliases for the common fonts.
+        /// </para>
+        /// </summary>
+        string FontName { get; set; }
     }
 }
