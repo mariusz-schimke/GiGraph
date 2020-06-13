d6e8f47bac2df0f1aecec41354bfff74169bfe2d
mariusz.schimke <mariusz.schimke@gmail.com>
2020-06-13 18:46:25 +0200

    DotEdgePort constructors optimization

--- GiGraph.Dot.Entities/Types/Edges/DotEdgePort.cs
+++ GiGraph.Dot.Entities/Types/Edges/DotEdgePort.cs
@@ -19,7 +19,7 @@ namespace GiGraph.Dot.Entities.Types.Edges
 
         /// <summary>
         /// <para>
-        ///     Gets a value that modifies the edge placement to aim for the specified compass point on the <see cref="PortName"/>
+        ///     Gets a value that modifies the edge placement to aim for the specified compass point on the <see cref="PortName"/> port
         ///     if specified, or on the node itself otherwise.
         /// </para>
         /// <para>
@@ -29,38 +29,43 @@ namespace GiGraph.Dot.Entities.Types.Edges
         public virtual DotCompassPoint? CompassPoint { get; }
 
         /// <summary>
-        /// Creates a new instance of the port.
+        /// Creates a new instance of the port, initialized with a port name.
         /// </summary>
         /// <param name="portName">
-        /// Determines the edge placement to aim for the specified port. If specified, the corresponding node
+        /// Determines the edge placement to aim for the specified port. The corresponding node
         /// must either have record shape (<see cref="DotShape.Record"/>, <see cref="DotShape.MRecord"/>)
         /// with one of its fields having the given port name, or have an HTML-like label, one of whose components
         /// has a PORT attribute set to the specified port name.
         /// </param>
-        /// <param name="compassPoint">
-        /// Determines the edge placement to aim for the specified compass point on the <paramref name="portName"/> if specified,
-        /// or on the node itself otherwise. If no compass point is specified explicitly, the default value is <see cref="DotCompassPoint.Center"/>.
-        /// </param>
-        public DotEdgePort(string portName, DotCompassPoint? compassPoint = null)
+        public DotEdgePort(string portName)
         {
-            if (portName is null && compassPoint is null)
-            {
-                throw new ArgumentNullException(
-                    $"{nameof(portName)} / {nameof(compassPoint)}",
-                    "At least a port name or a compass point has to be specified for an edge port.");
-            }
+            PortName = portName ?? throw new ArgumentNullException(nameof(portName), "Port name cannot be null.");
+        }
 
-            PortName = portName;
+        /// <summary>
+        /// Creates a new instance of the port, initialized with a compass point.
+        /// </summary>
+        /// <param name="compassPoint">Determines the edge placement to aim for the specified compass point on a node.</param>
+        public DotEdgePort(DotCompassPoint compassPoint)
+        {
             CompassPoint = compassPoint;
         }
 
         /// <summary>
-        /// Creates a new instance of the port initialized with a compass point.
+        /// Creates a new instance of the port, initialized with a port name and a compass point.
         /// </summary>
-        /// <param name="compassPoint">Determines the edge placement to aim for the specified compass point on a node.</param>
-        public DotEdgePort(DotCompassPoint compassPoint)
-            : this(portName: null, compassPoint)
+        /// <param name="portName">
+        /// Determines the edge placement to aim for the specified port. The corresponding node
+        /// must either have record shape (<see cref="DotShape.Record"/>, <see cref="DotShape.MRecord"/>)
+        /// with one of its fields having the given port name, or have an HTML-like label, one of whose components
+        /// has a PORT attribute set to the specified port name.
+        /// </param>
+        /// <param name="compassPoint">Determines the edge placement to aim for the specified compass point
+        /// on the <paramref name="portName"/> port.</param>
+        public DotEdgePort(string portName, DotCompassPoint compassPoint)
+            : this(portName)
         {
+            CompassPoint = compassPoint;
         }
 
         public static implicit operator DotEdgePort(string portName)
