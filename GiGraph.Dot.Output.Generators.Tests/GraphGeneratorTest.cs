fc3e5e700fe0c8393caac8919998ade5017b6f11
mariusz.schimke <mariusz.schimke@gmail.com>
2020-06-21 17:06:52 +0200

    Graph generator test extended

--- GiGraph.Dot.Output.Generators.Tests/GraphGeneratorTest.cs
+++ GiGraph.Dot.Output.Generators.Tests/GraphGeneratorTest.cs
@@ -1,13 +1,9 @@
 using System.Drawing;
 using System.Text;
-using GiGraph.Dot.Entities;
-using GiGraph.Dot.Entities.Attributes;
 using GiGraph.Dot.Entities.Attributes.Enums;
 using GiGraph.Dot.Entities.Graphs;
-using GiGraph.Dot.Entities.Types.Records;
+using GiGraph.Dot.Entities.Subgraphs;
 using GiGraph.Dot.Extensions;
-using GiGraph.Dot.Output.Generators.GraphGenerators;
-using GiGraph.Dot.Output.Generators.Providers;
 using GiGraph.Dot.Output.Options;
 using Xunit;
 
@@ -34,7 +30,23 @@ namespace GiGraph.Dot.Tests
         }
 
         [Fact]
-        public void a()
+        public void renders_empty_graph_in_expected_single_line_format()
+        {
+            var graph = new DotGraph();
+
+            var sb = new StringBuilder();
+            sb.Append("digraph { }");
+
+            var options = new DotFormattingOptions()
+            {
+                SingleLineOutput = true
+            };
+
+            Assert.Equal(sb.ToString(), graph.Build(options, _generationOptions, _syntaxRules));
+        }
+
+        [Fact]
+        public void graph_with_all_possible_elements_is_rendered_according_to_rules_and_options()
         {
             var graph = GetCompleteGraph(directed: true);
 
@@ -57,6 +69,7 @@ namespace GiGraph.Dot.Tests
 
             sb.AppendLine("    node1 -> node2 [ color = gold, style = dotted ]");
             sb.AppendLine("    node1 -> node2 -> node3 [ color = beige, style = invis ]");
+            sb.AppendLine("    node1 -> { snode1 snode2 } -> node2 [ constraint = true, style = solid ]");
 
             sb.Append("}");
 
@@ -102,6 +115,12 @@ namespace GiGraph.Dot.Tests
                 edge.Attributes.Color = Color.Beige;
                 edge.Attributes.Style = DotStyle.Invisible;
             }, "node1", "node2", "node3");
+            
+            graph.Edges.AddSequence(edge =>
+            {
+                edge.Attributes.Constraint = true;
+                edge.Attributes.Style = DotStyle.Solid;
+            }, "node1", DotSubgraph.FromNodes("snode1", "snode2"), "node2");
 
             return graph;
         }
