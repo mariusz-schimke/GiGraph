ea8381fe3b5b91ba7918f7bb6ef0bf4a065c5ca7
mariusz.schimke <mariusz.schimke@gmail.com>
2020-06-21 15:03:27 +0200

    Colors test added

---
+++ GiGraph.Dot.Tests/ColorsTest.cs
@@ -0,0 +1,64 @@
+using System.Drawing;
+using GiGraph.Dot.Entities;
+using GiGraph.Dot.Entities.Types.Colors;
+using GiGraph.Dot.Output.Options;
+using Xunit;
+
+namespace GiGraph.Dot.Tests
+{
+    public class ColorsTest
+    {
+        private DotGenerationOptions _generationOptions = new DotGenerationOptions();
+        private DotSyntaxRules _syntaxRules = new DotSyntaxRules();
+
+        [Fact]
+        public void color_definition_returns_color_name_as_dot_encoded_value_when_specified()
+        {
+            DotColorDefinition def = Color.Red;
+
+            Assert.Equal("red", ((IDotEncodableValue) def).GetDotEncodedValue(_generationOptions, _syntaxRules));
+        }
+
+        [Fact]
+        public void color_definition_returns_hex_color_as_dot_encoded_value_when_name_is_not_specified()
+        {
+            DotColorDefinition def = Color.FromArgb(0xff, Color.Red);
+
+            Assert.Equal("#ff0000", ((IDotEncodableValue) def).GetDotEncodedValue(_generationOptions, _syntaxRules));
+        }
+
+        [Fact]
+        public void color_definition_includes_alpha_in_dot_encoded_value_when_less_than_ff()
+        {
+            DotColorDefinition def = Color.FromArgb(0x0f, Color.Red);
+
+            Assert.Equal("#ff00000f", ((IDotEncodableValue) def).GetDotEncodedValue(_generationOptions, _syntaxRules));
+        }
+
+        [Fact]
+        public void color_definition_returns_empty_string_as_dot_encoded_value_when_color_empty_is_used()
+        {
+            DotColorDefinition def = Color.Empty;
+
+            Assert.Equal(string.Empty, ((IDotEncodableValue) def).GetDotEncodedValue(_generationOptions, _syntaxRules));
+        }
+
+        [Fact]
+        public void weighted_color_definition_includes_weight_in_dot_encoded_value()
+        {
+            DotColorDefinition def = new DotWeightedColor(Color.Blue, 0.8);
+
+            Assert.Equal("blue;0.8", ((IDotEncodableValue) def).GetDotEncodedValue(_generationOptions, _syntaxRules));
+        }
+        
+        [Fact]
+        public void color_list_includes_colon_separated_colors_in_dot_encoded_value()
+        {
+            DotColorDefinition def = DotColorDefinition.Multi(
+                new DotWeightedColor(Color.Blue, 0.8),
+                Color.Green);
+
+            Assert.Equal("blue;0.8:green", ((IDotEncodableValue) def).GetDotEncodedValue(_generationOptions, _syntaxRules));
+        }
+    }
+}
