52e31d79354d6ed336eae8e1d67370da8c8d55c4
mariusz.schimke <mariusz.schimke@gmail.com>
2020-06-21 17:06:36 +0200

    Implicit conversion added to endpoint definitions

--- GiGraph.Dot.Entities/Edges/Endpoints/DotEndpointDefinition.cs
+++ GiGraph.Dot.Entities/Edges/Endpoints/DotEndpointDefinition.cs
@@ -1,4 +1,6 @@
-﻿namespace GiGraph.Dot.Entities.Edges.Endpoints
+﻿using GiGraph.Dot.Entities.Subgraphs;
+
+namespace GiGraph.Dot.Entities.Edges.Endpoints
 {
     /// <summary>
     /// Represents an endpoint of an edge.
@@ -7,5 +9,20 @@
     {
         string IDotOrderableEntity.OrderingKey => GetOrderingKey();
         protected abstract string GetOrderingKey();
+
+        public static implicit operator DotEndpointDefinition(string nodeId)
+        {
+            return (DotEndpoint) nodeId;
+        }
+
+        public static implicit operator DotEndpointDefinition(string[] nodeIds)
+        {
+            return (DotEndpointGroup) nodeIds;
+        }
+
+        public static implicit operator DotEndpointDefinition(DotSubgraph subgraph)
+        {
+            return (DotEndpointGroup) subgraph;
+        }
     }
 }
