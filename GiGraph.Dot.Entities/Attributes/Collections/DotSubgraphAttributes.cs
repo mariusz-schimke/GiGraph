9b6b064a64c2ea9b46d398c6711deaaf36678576
mariusz.schimke <mariusz.schimke@gmail.com>
2020-06-27 18:37:30 +0200

    Entity-specific attributes moved to specialized attribute collections

--- GiGraph.Dot.Entities/Attributes/Collections/DotSubgraphAttributes.cs
+++ GiGraph.Dot.Entities/Attributes/Collections/DotSubgraphAttributes.cs
@@ -1,9 +1,16 @@
+using GiGraph.Dot.Entities.Attributes.Enums;
 using GiGraph.Dot.Entities.Types.Colors;
 
 namespace GiGraph.Dot.Entities.Attributes.Collections
 {
     public class DotSubgraphAttributes : DotEntityAttributes, IDotSubgraphAttributes
     {
+        public virtual DotRank? Rank
+        {
+            get => TryGetValueAs<DotRank>("rank", out var result) ? result : (DotRank?) null;
+            set => AddOrRemove("rank", value, v => new DotRankAttribute("rank", v.Value));
+        }
+
         public override void SetFilled(DotColorDefinition value)
         {
             // subgraphs do not supported a fill color
