95206ba98276a2b8a5c8207a49c446173ce54df7
mariusz.schimke <mariusz.schimke@gmail.com>
2020-06-27 18:37:30 +0200

    Entity-specific attributes moved to specialized attribute collections

--- GiGraph.Dot.Entities/Attributes/Collections/DotNodeAttributes.cs
+++ GiGraph.Dot.Entities/Attributes/Collections/DotNodeAttributes.cs
@@ -1,3 +1,4 @@
+using System;
 using GiGraph.Dot.Entities.Attributes.Enums;
 using GiGraph.Dot.Entities.Types.Colors;
 
@@ -5,6 +6,44 @@ namespace GiGraph.Dot.Entities.Attributes.Collections
 {
     public class DotNodeAttributes : DotEntityAttributes, IDotNodeAttributes
     {
+        public virtual DotShape? Shape
+        {
+            get => TryGetValueAs<DotShape>("shape", out var result) ? result : (DotShape?) null;
+            set => AddOrRemove("shape", value, v => new DotShapeAttribute("shape", v.Value));
+        }
+
+        public virtual int? Sides
+        {
+            get => TryGetValueAs<int>("sides", out var result) ? result : (int?) null;
+            set => AddOrRemove("sides", value, v => v.Value < 0
+                ? throw new ArgumentOutOfRangeException(nameof(Sides), v.Value, "The number of sides must be greater than or equal to 0.")
+                : new DotIntAttribute("sides", v.Value));
+        }
+
+        public virtual bool? Regular
+        {
+            get => TryGetValueAs<bool>("regular", out var result) ? result : (bool?) null;
+            set => AddOrRemove("regular", value, v => new DotBoolAttribute("regular", v.Value));
+        }
+
+        public virtual double? Skew
+        {
+            get => TryGetValueAs<double>("skew", out var result) ? result : (double?) null;
+            set => AddOrRemove("skew", value, v => new DotDoubleAttribute("skew", v.Value));
+        }
+
+        public virtual double? Distortion
+        {
+            get => TryGetValueAs<double>("distortion", out var result) ? result : (double?) null;
+            set => AddOrRemove("distortion", value, v => new DotDoubleAttribute("distortion", v.Value));
+        }
+
+        public virtual double? Orientation
+        {
+            get => TryGetValueAs<double>("orientation", out var result) ? result : (double?) null;
+            set => AddOrRemove("orientation", value, v => new DotDoubleAttribute("orientation", v.Value));
+        }
+
         public override void SetFilled(DotColorDefinition value)
         {
             Style = Style.GetValueOrDefault(DotStyle.Filled) | DotStyle.Filled;
