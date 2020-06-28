89b6761504e9e4af8f0a5caa03e231c1cce7b64d
mariusz.schimke <mariusz.schimke@gmail.com>
2020-06-28 20:31:11 +0200

    Record nodes example added and updated in readme

---
+++ GiGraph.Dot.Examples/Basic/RecordNodes.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using GiGraph.Dot.Entities.Edges.Enums;
+using GiGraph.Dot.Entities.Graphs;
+using GiGraph.Dot.Entities.Types.Records;
+using GiGraph.Dot.Extensions;
+
+namespace GiGraph.Dot.Examples.Basic
+{
+    public static class RecordNodes
+    {
+        public static DotGraph Generate()
+        {
+            var graph = new DotGraph(isDirected: true);
+
+            graph.Nodes.Add("Bar").ToRecord
+            (
+                $"Foo{Environment.NewLine}Bar",
+                new DotRecordField[]
+                {
+                    "Baz",
+                    new DotRecord
+                    (
+                        "Garply",
+                        "Waldo",
+                        new DotRecordTextField("Fred", portName: "port1")
+                    ),
+                    "Plugh",
+                },
+                "Qux",
+                "Quux");
+
+            graph.Edges.Add("Foo", "Bar", edge =>
+            {
+                edge.Head.Port.Name = "port1";
+                edge.Head.Port.CompassPoint = DotCompassPoint.NorthEast;
+            });
+
+            return graph;
+        }
+    }
+}
