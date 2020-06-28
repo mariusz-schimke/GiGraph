25b73bf0c6f634563e9a02ed6af6036b5dbe790c
mariusz.schimke <mariusz.schimke@gmail.com>
2020-06-28 19:19:01 +0200

    Added an HtmlNodes example file

---
+++ GiGraph.Dot.Examples/Basic/HtmlNodes.cs
@@ -0,0 +1,41 @@
+﻿using GiGraph.Dot.Entities.Edges.Enums;
+using GiGraph.Dot.Entities.Graphs;
+using GiGraph.Dot.Entities.Types.Edges;
+using GiGraph.Dot.Extensions;
+
+namespace GiGraph.Dot.Examples.Basic
+{
+    public static class HtmlNodes
+    {
+        public static DotGraph Generate()
+        {
+            var graph = new DotGraph(isDirected: true);
+
+            graph.Nodes.Add("Foo");
+
+            graph.Nodes.Add("Bar").ToHtml
+            (
+                @"<TABLE BORDER=""0"" CELLBORDER=""1"" CELLSPACING=""0"" CELLPADDING=""4"">
+                    <TR>
+                        <TD ROWSPAN=""3"">Foo<BR/>Bar</TD>
+                        <TD COLSPAN=""3"">Baz</TD>
+                        <TD ROWSPAN=""3"">Qux</TD>
+                        <TD ROWSPAN=""3"">Quux</TD>
+                    </TR>
+                    <TR>
+                        <TD>Garply</TD>
+                        <TD>Waldo</TD>
+                        <TD PORT=""port1"">Fred</TD>
+                    </TR>
+                    <TR>
+                        <TD COLSPAN=""3"">Plugh</TD>
+                    </TR>
+                </TABLE>"
+            );
+
+            graph.Edges.Add("Foo", "Bar").Attributes.HeadPort = new DotEndpointPort("port1", DotCompassPoint.NorthEast);
+
+            return graph;
+        }
+    }
+}
