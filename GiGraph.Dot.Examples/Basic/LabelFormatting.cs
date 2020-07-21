cf1ef341682669e3b5a0415672b7d9be26d9ab1f
mariusz.schimke <mariusz.schimke@gmail.com>
2020-07-21 23:18:47 +0200

    Label formatting example added

---
+++ GiGraph.Dot.Examples/Basic/LabelFormatting.cs
@@ -0,0 +1,35 @@
+﻿using GiGraph.Dot.Entities.Graphs;
+using GiGraph.Dot.Entities.Types.Strings;
+
+namespace GiGraph.Dot.Examples.Basic
+{
+    public static class LabelFormatting
+    {
+        public static DotGraph Generate()
+        {
+            var graph = new DotGraph("Label formatting example", isDirected: true);
+
+            graph.Attributes.Label = new DotEscapeStringBuilder()
+                                    .AppendGraphId()
+                                    .ToEscapeString();
+
+            graph.Nodes.Add("Foo", attrs =>
+            {
+                attrs.Label = new DotEscapeStringBuilder("Node ")
+                             .AppendNodeId()
+                             .ToEscapeString();
+            });
+
+            graph.Edges.Add("Foo", "Bar", edge =>
+            {
+                edge.Attributes.Label = new DotEscapeStringBuilder("From ")
+                                       .AppendEdgeTailNodeId()
+                                       .Append(" to ")
+                                       .AppendEdgeHeadNodeId()
+                                       .ToEscapeString();
+            });
+
+            return graph;
+        }
+    }
+}
