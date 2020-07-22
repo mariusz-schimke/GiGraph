70b4fbc1fd2acaaf7d069a9dd868b440f2d15389
mariusz.schimke <mariusz.schimke@gmail.com>
2020-07-22 19:29:57 +0200

    Label formatting example updated

--- GiGraph.Dot.Examples/Basic/LabelFormatting.cs
+++ GiGraph.Dot.Examples/Basic/LabelFormatting.cs
@@ -7,19 +7,30 @@ namespace GiGraph.Dot.Examples.Basic
     {
         public static DotGraph Generate()
         {
-            var graph = new DotGraph("Label formatting example", isDirected: true);
+            var graph = new DotGraph("Label formatting");
 
-            graph.Attributes.Label = new DotEscapeStringBuilder()
+            // graph label with the ID of the graph
+            graph.Attributes.Label = new DotEscapeStringBuilder("Graph label: ")
                                     .AppendGraphId()
                                     .ToEscapeString();
 
+            // or an alternative way
+            graph.Attributes.Label = "Graph label: " + DotEscapeString.GraphId;
+
+
+            // node label with the ID of the node
             graph.Nodes.Add("Foo", attrs =>
             {
                 attrs.Label = new DotEscapeStringBuilder("Node ")
                              .AppendNodeId()
                              .ToEscapeString();
+
+                // or an alternative way
+                attrs.Label = "Node " + DotEscapeString.NodeId;
             });
 
+
+            // edge label with the IDs of the head and the tail nodes
             graph.Edges.Add("Foo", "Bar", edge =>
             {
                 edge.Attributes.Label = new DotEscapeStringBuilder("From ")
@@ -27,6 +38,10 @@ namespace GiGraph.Dot.Examples.Basic
                                        .Append(" to ")
                                        .AppendEdgeHeadNodeId()
                                        .ToEscapeString();
+
+                // or an alternative way
+                edge.Attributes.Label = "From " + DotEscapeString.EdgeTailNodeId +
+                                        " to " + DotEscapeString.EdgeHeadNodeId;
             });
 
             return graph;
