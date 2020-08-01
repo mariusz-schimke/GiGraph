67e881cbde3cf42605b0b7b99dcc2418799b5692
mariusz.schimke <mariusz.schimke@gmail.com>
2020-08-01 23:13:49 +0100

    Graph attribute keys are obtained from property attributes now

--- GiGraph.Dot.Entities/Attributes/Collections/DotGraphAttributeCollection.cs
+++ GiGraph.Dot.Entities/Attributes/Collections/DotGraphAttributeCollection.cs
@@ -1,7 +1,9 @@
 using System;
+using System.Reflection;
 using GiGraph.Dot.Entities.Attributes.Entities;
 using GiGraph.Dot.Entities.Attributes.Enums;
 using GiGraph.Dot.Entities.Types.AspectRatio;
+using GiGraph.Dot.Entities.Types.Attributes;
 using GiGraph.Dot.Entities.Types.Points;
 using GiGraph.Dot.Entities.Types.Ranks;
 
@@ -9,67 +11,77 @@ namespace GiGraph.Dot.Entities.Attributes.Collections
 {
     public class DotGraphAttributeCollection : DotEntityAttributeCollection<IDotGraphAttributes>, IDotGraphAttributeCollection
     {
+        [DotAttributeKey("rankdir")]
         public virtual DotRankDirection? LayoutDirection
         {
-            get => TryGetValueAs<DotRankDirection>("rankdir", out var result) ? result : (DotRankDirection?) null;
-            set => AddOrRemove("rankdir", value, (k, v) => new DotRankDirectionAttribute(k, v.Value));
+            get => TryGetValueAs<DotRankDirection>(MethodBase.GetCurrentMethod(), out var result) ? result : (DotRankDirection?) null;
+            set => AddOrRemove(MethodBase.GetCurrentMethod(), value, (k, v) => new DotRankDirectionAttribute(k, v.Value));
         }
 
+        [DotAttributeKey("splines")]
         public virtual DotEdgeShape? EdgeShape
         {
-            get => TryGetValueAs<DotEdgeShape>("splines", out var result) ? result : (DotEdgeShape?) null;
-            set => AddOrRemove("splines", value, (k, v) => new DotEdgeShapeAttribute(k, v.Value));
+            get => TryGetValueAs<DotEdgeShape>(MethodBase.GetCurrentMethod(), out var result) ? result : (DotEdgeShape?) null;
+            set => AddOrRemove(MethodBase.GetCurrentMethod(), value, (k, v) => new DotEdgeShapeAttribute(k, v.Value));
         }
 
+        [DotAttributeKey("clusterrank")]
         public virtual DotClusterMode? ClusterRank
         {
-            get => TryGetValueAs<DotClusterMode>("clusterrank", out var result) ? result : (DotClusterMode?) null;
-            set => AddOrRemove("clusterrank", value, (k, v) => new DotClusterModeAttribute(k, v.Value));
+            get => TryGetValueAs<DotClusterMode>(MethodBase.GetCurrentMethod(), out var result) ? result : (DotClusterMode?) null;
+            set => AddOrRemove(MethodBase.GetCurrentMethod(), value, (k, v) => new DotClusterModeAttribute(k, v.Value));
         }
 
+        [DotAttributeKey("concentrate")]
         public virtual bool? ConcentrateEdges
         {
-            get => TryGetValueAs<bool>("concentrate", out var result) ? result : (bool?) null;
-            set => AddOrRemove("concentrate", value, (k, v) => new DotBoolAttribute(k, v.Value));
+            get => TryGetValueAs<bool>(MethodBase.GetCurrentMethod(), out var result) ? result : (bool?) null;
+            set => AddOrRemove(MethodBase.GetCurrentMethod(), value, (k, v) => new DotBoolAttribute(k, v.Value));
         }
 
+        [DotAttributeKey("compound")]
         public virtual bool? Compound
         {
-            get => TryGetValueAs<bool>("compound", out var result) ? result : (bool?) null;
-            set => AddOrRemove("compound", value, (k, v) => new DotBoolAttribute(k, v.Value));
+            get => TryGetValueAs<bool>(MethodBase.GetCurrentMethod(), out var result) ? result : (bool?) null;
+            set => AddOrRemove(MethodBase.GetCurrentMethod(), value, (k, v) => new DotBoolAttribute(k, v.Value));
         }
 
+        [DotAttributeKey("forcelabels")]
         public virtual bool? ForceExternalLabels
         {
-            get => TryGetValueAs<bool>("forcelabels", out var result) ? result : (bool?) null;
-            set => AddOrRemove("forcelabels", value, (k, v) => new DotBoolAttribute(k, v.Value));
+            get => TryGetValueAs<bool>(MethodBase.GetCurrentMethod(), out var result) ? result : (bool?) null;
+            set => AddOrRemove(MethodBase.GetCurrentMethod(), value, (k, v) => new DotBoolAttribute(k, v.Value));
         }
 
+        [DotAttributeKey("fontpath")]
         public virtual string FontDirectories
         {
-            get => TryGetValueAs<string>("fontpath", out var result) ? result : null;
-            set => AddOrRemove("fontpath", value, (k, v) => new DotStringAttribute(k, v));
+            get => TryGetValueAs<string>(MethodBase.GetCurrentMethod(), out var result) ? result : null;
+            set => AddOrRemove(MethodBase.GetCurrentMethod(), value, (k, v) => new DotStringAttribute(k, v));
         }
 
+        [DotAttributeKey("charset")]
         public virtual string Charset
         {
-            get => TryGetValueAs<string>("charset", out var result) ? result : null;
-            set => AddOrRemove("charset", value, (k, v) => new DotStringAttribute(k, v));
+            get => TryGetValueAs<string>(MethodBase.GetCurrentMethod(), out var result) ? result : null;
+            set => AddOrRemove(MethodBase.GetCurrentMethod(), value, (k, v) => new DotStringAttribute(k, v));
         }
 
+        [DotAttributeKey("nodesep")]
         public virtual double? NodeSeparation
         {
-            get => TryGetValueAs<double>("nodesep", out var result) ? result : (double?) null;
-            set => AddOrRemove("nodesep", value, (k, v) => v.Value < 0.0
+            get => TryGetValueAs<double>(MethodBase.GetCurrentMethod(), out var result) ? result : (double?) null;
+            set => AddOrRemove(MethodBase.GetCurrentMethod(), value, (k, v) => v.Value < 0.0
                 ? throw new ArgumentOutOfRangeException(nameof(NodeSeparation), v.Value, "Node separation must be greater than or equal to 0.")
                 : new DotDoubleAttribute(k, v.Value));
         }
 
+        [DotAttributeKey("ranksep")]
         public virtual DotRankSeparationDefinition RankSeparation
         {
             get
             {
-                const string key = "ranksep";
+                var key = GetAttributeKey(MethodBase.GetCurrentMethod());
 
                 if (TryGetValueAs<DotRankSeparationDefinition>(key, out var definition))
                 {
@@ -83,38 +95,43 @@ namespace GiGraph.Dot.Entities.Attributes.Collections
 
                 return TryGetValueAs<double[]>(key, out var array) ? array : null;
             }
-            set => AddOrRemove("ranksep", value, (k, v) => new DotRankSeparationAttribute(k, v));
+            set => AddOrRemove(MethodBase.GetCurrentMethod(), value, (k, v) => new DotRankSeparationAttribute(k, v));
         }
 
+        [DotAttributeKey("rotate")]
         public virtual int? RotateBy
         {
-            get => TryGetValueAs<int>("rotate", out var result) ? result : (int?) null;
-            set => AddOrRemove("rotate", value, (k, v) => new DotIntAttribute(k, v.Value));
+            get => TryGetValueAs<int>(MethodBase.GetCurrentMethod(), out var result) ? result : (int?) null;
+            set => AddOrRemove(MethodBase.GetCurrentMethod(), value, (k, v) => new DotIntAttribute(k, v.Value));
         }
 
+        [DotAttributeKey("imagepath")]
         public virtual string ImageDirectories
         {
-            get => TryGetValueAs<string>("imagepath", out var result) ? result : null;
-            set => AddOrRemove("imagepath", value, (k, v) => new DotStringAttribute(k, v));
+            get => TryGetValueAs<string>(MethodBase.GetCurrentMethod(), out var result) ? result : null;
+            set => AddOrRemove(MethodBase.GetCurrentMethod(), value, (k, v) => new DotStringAttribute(k, v));
         }
 
+        [DotAttributeKey("pad")]
         public virtual DotPoint Padding
         {
-            get => TryGetValueAs<DotPoint>("pad", out var result) ? result : null;
-            set => AddOrRemove("pad", value, (k, v) => new DotPointAttribute(k, v));
+            get => TryGetValueAs<DotPoint>(MethodBase.GetCurrentMethod(), out var result) ? result : null;
+            set => AddOrRemove(MethodBase.GetCurrentMethod(), value, (k, v) => new DotPointAttribute(k, v));
         }
 
+        [DotAttributeKey("size")]
         public virtual DotPoint Size
         {
-            get => TryGetValueAs<DotPoint>("size", out var result) ? result : null;
-            set => AddOrRemove("size", value, (k, v) => new DotPointAttribute(k, v));
+            get => TryGetValueAs<DotPoint>(MethodBase.GetCurrentMethod(), out var result) ? result : null;
+            set => AddOrRemove(MethodBase.GetCurrentMethod(), value, (k, v) => new DotPointAttribute(k, v));
         }
 
+        [DotAttributeKey("ratio")]
         public virtual DotAspectRatioDefinition AspectRatio
         {
             get
             {
-                const string key = "ratio";
+                var key = GetAttributeKey(MethodBase.GetCurrentMethod());
 
                 if (TryGetValueAs<DotAspectRatioDefinition>(key, out var definition))
                 {
@@ -123,7 +140,7 @@ namespace GiGraph.Dot.Entities.Attributes.Collections
 
                 return TryGetValueAs<double>(key, out var number) ? number : (double?) null;
             }
-            set => AddOrRemove("ratio", value, (k, v) => new DotAspectRatioDefinitionAttribute(k, v));
+            set => AddOrRemove(MethodBase.GetCurrentMethod(), value, (k, v) => new DotAspectRatioDefinitionAttribute(k, v));
         }
     }
 }
