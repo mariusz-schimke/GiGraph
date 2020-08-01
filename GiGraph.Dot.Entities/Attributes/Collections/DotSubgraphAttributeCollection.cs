ec75955095dd65f46199be14e8766f13cd7beefc
mariusz.schimke <mariusz.schimke@gmail.com>
2020-08-01 23:05:37 +0100

    Subgraph attribute keys are obtained from property attributes now

--- GiGraph.Dot.Entities/Attributes/Collections/DotSubgraphAttributeCollection.cs
+++ GiGraph.Dot.Entities/Attributes/Collections/DotSubgraphAttributeCollection.cs
@@ -1,15 +1,18 @@
+using System.Reflection;
 using GiGraph.Dot.Entities.Attributes.Entities;
 using GiGraph.Dot.Entities.Attributes.Enums;
+using GiGraph.Dot.Entities.Types.Attributes;
 using GiGraph.Dot.Entities.Types.Colors;
 
 namespace GiGraph.Dot.Entities.Attributes.Collections
 {
     public class DotSubgraphAttributeCollection : DotEntityAttributeCollection<IDotSubgraphAttributes>, IDotSubgraphAttributeCollection
     {
+        [DotAttributeKey("rank")]
         public virtual DotRank? Rank
         {
-            get => TryGetValueAs<DotRank>("rank", out var result) ? result : (DotRank?) null;
-            set => AddOrRemove("rank", value, (k, v) => new DotRankAttribute(k, v.Value));
+            get => TryGetValueAs<DotRank>(MethodBase.GetCurrentMethod(), out var result) ? result : (DotRank?) null;
+            set => AddOrRemove(MethodBase.GetCurrentMethod(), value, (k, v) => new DotRankAttribute(k, v.Value));
         }
 
         public override void SetFilled(DotColorDefinition value)
