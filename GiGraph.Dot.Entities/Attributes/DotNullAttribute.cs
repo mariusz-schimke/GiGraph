bf32984d788efc454e4decb91a83d11feb6cd0ea
mariusz.schimke <mariusz.schimke@gmail.com>
2020-08-03 14:45:29 +0100

    DotNullAttribute type added

---
+++ GiGraph.Dot.Entities/Attributes/DotNullAttribute.cs
@@ -0,0 +1,31 @@
+using GiGraph.Dot.Output.Options;
+
+namespace GiGraph.Dot.Entities.Attributes
+{
+    /// <summary>
+    ///     An attribute with no value.
+    /// </summary>
+    public class DotNullAttribute : DotAttribute
+    {
+        /// <summary>
+        ///     Creates a new instance of the attribute.
+        /// </summary>
+        /// <param name="key">
+        ///     The key of the attribute.
+        /// </param>
+        public DotNullAttribute(string key)
+            : base(key)
+        {
+        }
+
+        protected internal override object GetValue()
+        {
+            return null;
+        }
+
+        protected internal override string GetDotEncodedValue(DotGenerationOptions options, DotSyntaxRules syntaxRules)
+        {
+            return null;
+        }
+    }
+}
