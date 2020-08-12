a51ff8c846ded8d40fafa07041d3d033bdd57774
mariusz.schimke <mariusz.schimke@gmail.com>
2020-08-12 20:35:55 +0200

    Cluster mode attribute value is retrieved through an enum attribute now

--- GiGraph.Dot.Entities/Attributes/DotClusterModeAttribute.cs
+++ GiGraph.Dot.Entities/Attributes/DotClusterModeAttribute.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using GiGraph.Dot.Entities.Attributes.Enums;
+using GiGraph.Dot.Entities.Types.Attributes;
 using GiGraph.Dot.Output.Options;
 
 namespace GiGraph.Dot.Entities.Attributes
@@ -25,13 +26,9 @@ namespace GiGraph.Dot.Entities.Attributes
 
         protected internal override string GetDotEncodedValue(DotGenerationOptions options, DotSyntaxRules syntaxRules)
         {
-            return Value switch
-            {
-                DotClusterMode.None => "none",
-                DotClusterMode.Global => "global",
-                DotClusterMode.Local => "local",
-                _ => throw new ArgumentOutOfRangeException(nameof(Value), $"The specified cluster mode '{Value}' is invalid.")
-            };
+            return DotAttributeValueAttribute.TryGetValue(Value, out var result)
+                ? result
+                : throw new ArgumentOutOfRangeException(nameof(Value), $"The specified cluster mode '{Value}' is invalid.");
         }
     }
 }
