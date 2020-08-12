ee0d191dfb7bb9d55adaf749bae2f413ea3391e7
mariusz.schimke <mariusz.schimke@gmail.com>
2020-08-12 20:55:57 +0200

    Horizontal alignment attribute value is retrieved through an enum attribute now

--- GiGraph.Dot.Entities/Attributes/Enums/DotHorizontalAlignment.cs
+++ GiGraph.Dot.Entities/Attributes/Enums/DotHorizontalAlignment.cs
@@ -1,12 +1,19 @@
-﻿namespace GiGraph.Dot.Entities.Attributes.Enums
+﻿using GiGraph.Dot.Entities.Types.Attributes;
+
+namespace GiGraph.Dot.Entities.Attributes.Enums
 {
     /// <summary>
     ///     The justification options for labels.
     /// </summary>
     public enum DotHorizontalAlignment
     {
+        [DotAttributeValue("l")]
         Left,
+        
+        [DotAttributeValue("c")]
         Center,
+        
+        [DotAttributeValue("r")]
         Right
     }
 }
