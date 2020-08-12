6a0297620bdfd3a7d719a6a70c413a7c31abb2ea
mariusz.schimke <mariusz.schimke@gmail.com>
2020-08-12 21:11:22 +0200

    Node shape attribute value is retrieved through an enum attribute now

--- GiGraph.Dot.Entities/Attributes/DotNodeShapeAttribute.cs
+++ GiGraph.Dot.Entities/Attributes/DotNodeShapeAttribute.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using GiGraph.Dot.Entities.Attributes.Enums;
+using GiGraph.Dot.Entities.Types.Attributes;
 using GiGraph.Dot.Output.Options;
 
 namespace GiGraph.Dot.Entities.Attributes
@@ -29,71 +30,9 @@ namespace GiGraph.Dot.Entities.Attributes
 
         protected internal override string GetDotEncodedValue(DotGenerationOptions options, DotSyntaxRules syntaxRules)
         {
-            return Value switch
-            {
-                DotNodeShape.None => "none",
-                DotNodeShape.Box => "box",
-                DotNodeShape.Polygon => "polygon",
-                DotNodeShape.Ellipse => "ellipse",
-                DotNodeShape.Oval => "oval",
-                DotNodeShape.Circle => "circle",
-                DotNodeShape.Point => "point",
-                DotNodeShape.Egg => "egg",
-                DotNodeShape.Triangle => "triangle",
-                DotNodeShape.PlainText => "plaintext",
-                DotNodeShape.Plain => "plain",
-                DotNodeShape.Diamond => "diamond",
-                DotNodeShape.Trapezium => "trapezium",
-                DotNodeShape.Parallelogram => "parallelogram",
-                DotNodeShape.House => "house",
-                DotNodeShape.Pentagon => "pentagon",
-                DotNodeShape.Hexagon => "hexagon",
-                DotNodeShape.Septagon => "septagon",
-                DotNodeShape.Octagon => "octagon",
-                DotNodeShape.DoubleCircle => "doublecircle",
-                DotNodeShape.DoubleOctagon => "doubleoctagon",
-                DotNodeShape.TripleOctagon => "tripleoctagon",
-                DotNodeShape.InvertedTriangle => "invtriangle",
-                DotNodeShape.InvertedTrapezium => "invtrapezium",
-                DotNodeShape.InvertedHouse => "invhouse",
-                DotNodeShape.ClippedDiamond => "Mdiamond",
-                DotNodeShape.ClippedSquare => "Msquare",
-                DotNodeShape.ClippedCircle => "Mcircle",
-                DotNodeShape.Rect => "rect",
-                DotNodeShape.Rectangle => "rectangle",
-                DotNodeShape.Square => "square",
-                DotNodeShape.Star => "star",
-                DotNodeShape.Underline => "underline",
-                DotNodeShape.Cylinder => "cylinder",
-                DotNodeShape.Note => "note",
-                DotNodeShape.Tab => "tab",
-                DotNodeShape.Folder => "folder",
-                DotNodeShape.Box3D => "box3d",
-                DotNodeShape.Component => "component",
-                DotNodeShape.Promoter => "promoter",
-                DotNodeShape.Cds => "cds",
-                DotNodeShape.Terminator => "terminator",
-                DotNodeShape.Utr => "utr",
-                DotNodeShape.PrimerSite => "primersite",
-                DotNodeShape.RestrictionSite => "restrictionsite",
-                DotNodeShape.FivePrimeOverhang => "fivepoverhang",
-                DotNodeShape.ThreePrimeOverhang => "threepoverhang",
-                DotNodeShape.NOverhang => "noverhang",
-                DotNodeShape.Assembly => "assembly",
-                DotNodeShape.Signature => "signature",
-                DotNodeShape.Insulator => "insulator",
-                DotNodeShape.RiboSite => "ribosite",
-                DotNodeShape.RnaStab => "rnastab",
-                DotNodeShape.ProteaseSite => "proteasesite",
-                DotNodeShape.ProteinStab => "proteinstab",
-                DotNodeShape.RightPromoter => "rpromoter",
-                DotNodeShape.RightArrow => "rarrow",
-                DotNodeShape.LeftArrow => "larrow",
-                DotNodeShape.LeftPromoter => "lpromoter",
-                DotNodeShape.Record => "record",
-                DotNodeShape.RoundedRecord => "Mrecord",
-                _ => throw new ArgumentOutOfRangeException(nameof(Value), $"The specified node shape '{Value}' is invalid.")
-            };
+            return DotAttributeValueAttribute.TryGetValue(Value, out var result)
+                ? result
+                : throw new ArgumentOutOfRangeException(nameof(Value), $"The specified node shape '{Value}' is invalid.");
         }
     }
 }
--- GiGraph.Dot.Entities/Attributes/Enums/DotNodeShape.cs
+++ GiGraph.Dot.Entities/Attributes/Enums/DotNodeShape.cs
@@ -1,4 +1,5 @@
-﻿using GiGraph.Dot.Entities.Types.Labels;
+﻿using GiGraph.Dot.Entities.Types.Attributes;
+using GiGraph.Dot.Entities.Types.Labels;
 
 namespace GiGraph.Dot.Entities.Attributes.Enums
 {
@@ -14,6 +15,7 @@ namespace GiGraph.Dot.Entities.Attributes.Enums
         /// <summary>
         ///     No shape, just label. Synonymous to <see cref="PlainText" />.
         /// </summary>
+        [DotAttributeValue("none")]
         None,
 
         /// <summary>
@@ -21,85 +23,151 @@ namespace GiGraph.Dot.Entities.Attributes.Enums
         ///     height = 0, and margin = 0, which guarantees that the actual size of the node is entirely determined by the label. This is
         ///     useful, for example, when using HTML-like labels.
         /// </summary>
+        [DotAttributeValue("plain")]
         Plain,
 
         /// <summary>
         ///     No shape, just label. Synonymous to <see cref="None" />.
         /// </summary>
+        [DotAttributeValue("plaintext")]
         PlainText,
 
+        [DotAttributeValue("underline")]
         Underline,
 
         /// <summary>
         ///     The point shape. The way it is visualized is only affected by the peripheries, width, and height attributes.
         /// </summary>
+        [DotAttributeValue("point")]
         Point,
 
+        [DotAttributeValue("circle")]
         Circle,
+
+        [DotAttributeValue("Mcircle")]
         ClippedCircle,
+
+        [DotAttributeValue("doublecircle")]
         DoubleCircle,
 
+
+        [DotAttributeValue("ellipse")]
         Ellipse,
+
+        [DotAttributeValue("oval")]
         Oval,
+
+        [DotAttributeValue("egg")]
         Egg,
 
         /// <summary>
         ///     A rectangular shape. Synonymous to <see cref="Rect" /> and <see cref="Rectangle" />.
         /// </summary>
+        [DotAttributeValue("box")]
         Box,
 
         /// <summary>
         ///     A cuboid.
         /// </summary>
+        [DotAttributeValue("box3d")]
         Box3D,
 
         /// <summary>
         ///     A rectangular shape. Synonymous to <see cref="Rectangle" /> and <see cref="Box" />.
         /// </summary>
+        [DotAttributeValue("rect")]
         Rect,
 
         /// <summary>
         ///     A rectangular shape. Synonymous to <see cref="Rect" /> and <see cref="Box" />.
         /// </summary>
+        [DotAttributeValue("rectangle")]
         Rectangle,
 
+
+        [DotAttributeValue("cylinder")]
         Cylinder,
 
+
+        [DotAttributeValue("square")]
         Square,
+
+        [DotAttributeValue("Msquare")]
         ClippedSquare,
 
+
+        [DotAttributeValue("triangle")]
         Triangle,
+
+        [DotAttributeValue("invtriangle")]
         InvertedTriangle,
 
+
+        [DotAttributeValue("diamond")]
         Diamond,
+
+        [DotAttributeValue("Mdiamond")]
         ClippedDiamond,
 
+
+        [DotAttributeValue("parallelogram")]
         Parallelogram,
 
         /// <summary>
         ///     A polygon shape. When set, the attributes sides, skew, and distortion are also used.
         /// </summary>
+        [DotAttributeValue("polygon")]
         Polygon,
 
+
+        [DotAttributeValue("pentagon")]
         Pentagon,
+
+        [DotAttributeValue("hexagon")]
         Hexagon,
+
+        [DotAttributeValue("septagon")]
         Septagon,
 
+
+        [DotAttributeValue("octagon")]
         Octagon,
+
+        [DotAttributeValue("doubleoctagon")]
         DoubleOctagon,
+
+        [DotAttributeValue("tripleoctagon")]
         TripleOctagon,
 
+
+        [DotAttributeValue("trapezium")]
         Trapezium,
+
+        [DotAttributeValue("invtrapezium")]
         InvertedTrapezium,
 
+
+        [DotAttributeValue("house")]
         House,
+
+        [DotAttributeValue("invhouse")]
         InvertedHouse,
 
+
+        [DotAttributeValue("star")]
         Star,
 
+
+        [DotAttributeValue("note")]
         Note,
+
+        [DotAttributeValue("folder")]
         Folder,
+
+        [DotAttributeValue("component")]
         Component,
+
+        [DotAttributeValue("tab")]
         Tab,
 
         /// <summary>
@@ -110,6 +178,7 @@ namespace GiGraph.Dot.Entities.Attributes.Enums
         ///     </see>
         ///     ).
         /// </summary>
+        [DotAttributeValue("record")]
         Record,
 
         /// <summary>
@@ -120,6 +189,7 @@ namespace GiGraph.Dot.Entities.Attributes.Enums
         ///     </see>
         ///     ).
         /// </summary>
+        [DotAttributeValue("Mrecord")]
         RoundedRecord,
 
 
@@ -128,101 +198,121 @@ namespace GiGraph.Dot.Entities.Attributes.Enums
         /// <summary>
         ///     Synthetic biology: coding sequence (CDS).
         /// </summary>
+        [DotAttributeValue("cds")]
         Cds,
 
         /// <summary>
         ///     Synthetic biology: signature.
         /// </summary>
+        [DotAttributeValue("signature")]
         Signature,
 
         /// <summary>
         ///     Synthetic biology: left arrow.
         /// </summary>
+        [DotAttributeValue("larrow")]
         LeftArrow,
 
         /// <summary>
         ///     Synthetic biology: right arrow.
         /// </summary>
+        [DotAttributeValue("rarrow")]
         RightArrow,
 
         /// <summary>
         ///     Synthetic biology: left promoter.
         /// </summary>
+        [DotAttributeValue("lpromoter")]
         LeftPromoter,
 
         /// <summary>
         ///     Synthetic biology: right promoter.
         /// </summary>
+        [DotAttributeValue("rpromoter")]
         RightPromoter,
 
         /// <summary>
         ///     Synthetic biology: promoter.
         /// </summary>
+        [DotAttributeValue("promoter")]
         Promoter,
 
         /// <summary>
         ///     Synthetic biology: untranslated region (UTR).
         /// </summary>
+        [DotAttributeValue("utr")]
         Utr,
 
         /// <summary>
         ///     Synthetic biology: terminator.
         /// </summary>
+        [DotAttributeValue("terminator")]
         Terminator,
 
         /// <summary>
         ///     Synthetic biology: primer site.
         /// </summary>
+        [DotAttributeValue("primersite")]
         PrimerSite,
 
         /// <summary>
         ///     Synthetic biology: restriction site.
         /// </summary>
+        [DotAttributeValue("restrictionsite")]
         RestrictionSite,
 
         /// <summary>
         ///     Synthetic biology: 3' overhang.
         /// </summary>
+        [DotAttributeValue("threepoverhang")]
         ThreePrimeOverhang,
 
         /// <summary>
         ///     Synthetic biology: 5' overhang.
         /// </summary>
+        [DotAttributeValue("fivepoverhang")]
         FivePrimeOverhang,
 
         /// <summary>
         ///     Synthetic biology: n overhang.
         /// </summary>
+        [DotAttributeValue("noverhang")]
         NOverhang,
 
         /// <summary>
         ///     Synthetic biology: assembly.
         /// </summary>
+        [DotAttributeValue("assembly")]
         Assembly,
 
         /// <summary>
         ///     Synthetic biology: insulator.
         /// </summary>
+        [DotAttributeValue("insulator")]
         Insulator,
 
         /// <summary>
         ///     Synthetic biology: ribo site.
         /// </summary>
+        [DotAttributeValue("ribosite")]
         RiboSite,
 
         /// <summary>
         ///     Synthetic biology: protease site.
         /// </summary>
+        [DotAttributeValue("proteasesite")]
         ProteaseSite,
 
         /// <summary>
         ///     Synthetic biology: RNA stab.
         /// </summary>
+        [DotAttributeValue("rnastab")]
         RnaStab,
 
         /// <summary>
         ///     Synthetic biology: protein stab.
         /// </summary>
+        [DotAttributeValue("proteinstab")]
         ProteinStab
     }
 }
