077ce20fba7e2a132a7a17ba20e5bc9a42cc5e55
mariusz.schimke <mariusz.schimke@gmail.com>
2020-09-17 23:07:21 +0200

    Added a unit test for checking interfaces coherence

---
+++ GiGraph.Dot.Entities.Tests/Attributes/AttributeGroupsCoherenceTest.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Linq;
+using GiGraph.Dot.Entities.Attributes.Collections.Edge;
+using Xunit;
+
+namespace GiGraph.Dot.Entities.Tests.Attributes
+{
+    public class AttributeGroupsCoherenceTest : AttributesTestBase
+    {
+        [Theory]
+        [InlineData(typeof(IDotEdgeHeadAttributes), typeof(IDotEdgeTailAttributes))]
+        public void interfaces_contain_equivalent_members(Type interface1, Type interface2)
+        {
+            var interface1PropMethodPairs = GetInterfacePropertyMethodPairs(interface1);
+            var interface2PropMethodPairs = GetInterfacePropertyMethodPairs(interface2);
+
+            Assert.Equal(interface1PropMethodPairs.Length, interface2PropMethodPairs.Length);
+
+            foreach (var interface1PropMethodPair in interface1PropMethodPairs)
+            {
+                Assert.NotNull(interface1PropMethodPair.Method);
+                Assert.Contains(interface2PropMethodPairs, x =>
+                    interface1PropMethodPair.Property.Name.Equals(x.Property.Name)&&
+                    interface1PropMethodPair.Property.PropertyType == x.Property.PropertyType);
+            }
+        }
+    }
+}
