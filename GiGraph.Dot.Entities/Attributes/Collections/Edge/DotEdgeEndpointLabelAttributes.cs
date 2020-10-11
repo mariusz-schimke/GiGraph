4006a839fb8275acb06e5d481a3d83afac77e89b
mariusz.schimke <mariusz.schimke@gmail.com>
2020-10-11 16:17:58 +0200

    Endpoint label attributes class extracted

---
+++ GiGraph.Dot.Entities/Attributes/Collections/Edge/DotEdgeEndpointLabelAttributes.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Reflection;
+using GiGraph.Dot.Entities.Attributes.Collections.KeyLookup;
+using GiGraph.Dot.Entities.Types.Attributes;
+
+namespace GiGraph.Dot.Entities.Attributes.Collections.Edge
+{
+    public class DotEdgeEndpointLabelAttributes : DotEntityAttributes<IDotEdgeEndpointLabelAttributes>, IDotEdgeEndpointLabelAttributes
+    {
+        protected static readonly DotMemberAttributeKeyLookup MemberAttributeKeyLookup = CreateMemberAttributeKeyLookupFor(typeof(DotEdgeEndpointLabelAttributes));
+
+        protected DotEdgeEndpointLabelAttributes(DotAttributeCollection attributes, DotMemberAttributeKeyLookup propertyAttributeKeyLookup, DotEdgeEndpointLabelFontAttributes fontAttributes)
+            : base(attributes, propertyAttributeKeyLookup)
+        {
+            Font = fontAttributes;
+        }
+
+        public DotEdgeEndpointLabelAttributes(DotAttributeCollection attributes)
+            : this(attributes, MemberAttributeKeyLookup, new DotEdgeEndpointLabelFontAttributes(attributes))
+        {
+        }
+
+        // todo: adjust 'see' references in comment
+        /// <summary>
+        ///     Font properties used for the head <see cref="IDotEdgeHeadAttributes.Label" /> and the tail
+        ///     <see cref="IDotEdgeTailAttributes.Label" /> of the edge. If not set, defaults to the edge's font properties (
+        ///     <see cref="IDotEdgeAttributes.Font" />).
+        /// </summary>
+        public virtual DotEdgeEndpointLabelFontAttributes Font { get; }
+
+        [DotAttributeKey("labeldistance")]
+        public virtual double? Distance
+        {
+            get => GetValueAsDouble(MethodBase.GetCurrentMethod());
+            set => AddOrRemove(MethodBase.GetCurrentMethod(), value, (k, v) => v.Value < 0.0
+                ? throw new ArgumentOutOfRangeException(nameof(IDotEdgeEndpointLabelAttributes.Distance), v.Value, "Endpoint label distance must be greater than or equal to 0.")
+                : new DotDoubleAttribute(k, v.Value));
+        }
+
+        [DotAttributeKey("labelangle")]
+        public virtual double? Angle
+        {
+            get => GetValueAsDouble(MethodBase.GetCurrentMethod());
+            set => AddOrRemove(MethodBase.GetCurrentMethod(), value, (k, v) => new DotDoubleAttribute(k, v.Value));
+        }
+    }
+}
---
+++ GiGraph.Dot.Entities/Attributes/Collections/Edge/DotEdgeEndpointLabelFontAttributes.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Reflection;
+using GiGraph.Dot.Entities.Attributes.Collections.KeyLookup;
+using GiGraph.Dot.Entities.Types.Attributes;
+using GiGraph.Dot.Entities.Types.Colors;
+
+namespace GiGraph.Dot.Entities.Attributes.Collections.Edge
+{
+    public class DotEdgeEndpointLabelFontAttributes : DotEntityAttributes<IDotEdgeEndpointLabelFontAttributes>, IDotEdgeEndpointLabelFontAttributes
+    {
+        protected static readonly DotMemberAttributeKeyLookup MemberAttributeKeyLookup = CreateMemberAttributeKeyLookupFor(typeof(DotEdgeEndpointLabelFontAttributes));
+
+        public DotEdgeEndpointLabelFontAttributes(DotAttributeCollection attributes)
+            : base(attributes, MemberAttributeKeyLookup)
+        {
+        }
+
+        /// <summary>
+        ///     Font used for the head <see cref="IDotEdgeHeadAttributes.Label" /> and the tail <see cref="IDotEdgeTailAttributes.Label" />
+        ///     of the edge. If not set, defaults to the edge's font name (<see cref="IDotEntityFontAttributes.Name" />).
+        /// </summary>
+        [DotAttributeKey("labelfontname")]
+        public virtual string Name
+        {
+            get => GetValueAsString(MethodBase.GetCurrentMethod());
+            set => AddOrRemove(MethodBase.GetCurrentMethod(), value, (k, v) => new DotStringAttribute(k, v));
+        }
+
+        /// <summary>
+        ///     Color used for the head <see cref="IDotEdgeHeadAttributes.Label" /> and the tail <see cref="IDotEdgeTailAttributes.Label" />
+        ///     of the edge. If not set, defaults to the edge's font color (<see cref="IDotEntityFontAttributes.Color" />).
+        /// </summary>
+        [DotAttributeKey("labelfontcolor")]
+        public virtual DotColor Color
+        {
+            get => GetValueAsColor(MethodBase.GetCurrentMethod());
+            set => AddOrRemove(MethodBase.GetCurrentMethod(), value, (k, v) => new DotColorDefinitionAttribute(k, v));
+        }
+
+        /// <summary>
+        ///     Font size, in points, used for the head <see cref="IDotEdgeHeadAttributes.Label" /> and the tail
+        ///     <see cref="IDotEdgeTailAttributes.Label" /> of the edge. If not set, defaults to the edge's font size (
+        ///     <see cref="IDotEntityFontAttributes.Size" />).
+        /// </summary>
+        [DotAttributeKey("labelfontsize")]
+        public virtual double? Size
+        {
+            get => GetValueAsDouble(MethodBase.GetCurrentMethod());
+            set => AddOrRemove(MethodBase.GetCurrentMethod(), value, (k, v) => v.Value < 0.0
+                ? throw new ArgumentOutOfRangeException(nameof(IDotEdgeEndpointLabelFontAttributes.Size), v.Value, "Endpoint label font size must be greater than or equal to 0.")
+                : new DotDoubleAttribute(k, v.Value));
+        }
+    }
+}
