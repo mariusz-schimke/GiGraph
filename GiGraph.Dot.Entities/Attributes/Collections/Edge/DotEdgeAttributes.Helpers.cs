cafce0855ab17c502386f2c8713792a653941fee
mariusz.schimke <mariusz.schimke@gmail.com>
2020-11-01 15:32:32 +0100

    SetSegments and SetMultiline moved from extension methods to edge attributes

--- GiGraph.Dot.Entities/Attributes/Collections/Edge/DotEdgeAttributes.Helpers.cs
+++ GiGraph.Dot.Entities/Attributes/Collections/Edge/DotEdgeAttributes.Helpers.cs
@@ -1,4 +1,7 @@
+using System;
+using System.Linq;
 using GiGraph.Dot.Entities.Attributes.Enums;
+using GiGraph.Dot.Entities.Types.Colors;
 using GiGraph.Dot.Entities.Types.Styles;
 
 namespace GiGraph.Dot.Entities.Attributes.Collections.Edge
@@ -34,6 +37,88 @@ namespace GiGraph.Dot.Entities.Attributes.Collections.Edge
         {
             Width = startWidth;
             ApplyStyleOption(DotStyles.Tapered);
+        }
+
+        /// <summary>
+        ///     Converts the current edge to a segmented line with the specified colors. At least one of the colors has to have a weight
+        ///     specified (use <see cref="DotWeightedColor" />).
+        /// </summary>
+        /// <param name="segments">
+        ///     The colors to assign to consecutive segments of the edge. At least one of the colors has to have a weight specified (use
+        ///     <see cref="DotWeightedColor" />), that determines the proportion of the area covered with the specified color. If only
+        ///     weighted colors are provided, the weights must sum to at most 1. If both colors with and without weights are provided, the
+        ///     sum of the weighted ones should be below 1, as otherwise those without weights will be ignored by the visualization tool.
+        /// </param>
+        public virtual void SetSegmented(params DotColor[] segments)
+        {
+            SetSegmented(new DotMultiColor(segments));
+        }
+
+        /// <summary>
+        ///     Converts the current edge to a segmented line with the specified colors. At least one of the colors has to have a weight
+        ///     specified (use <see cref="DotWeightedColor" />).
+        /// </summary>
+        /// <param name="segments">
+        ///     The colors to assign to consecutive segments of the edge. At least one of the colors has to have a weight specified (use
+        ///     <see cref="DotWeightedColor" />), that determines the proportion of the area covered with the specified color. If only
+        ///     weighted colors are provided, the weights must sum to at most 1. If both colors with and without weights are provided, the
+        ///     sum of the weighted ones should be below 1, as otherwise those without weights will be ignored by the visualization tool.
+        /// </param>
+        public virtual void SetSegmented(DotMultiColor segments)
+        {
+            if (!segments.Colors.Any(item => item is DotWeightedColor))
+            {
+                throw new ArgumentException("At least one color has to have a weight specified.", nameof(segments));
+            }
+
+            Color = segments;
+        }
+
+        /// <summary>
+        ///     Converts the current edge to multiple parallel splines with the specified colors.
+        /// </summary>
+        /// <param name="splineCount">
+        ///     The number of parallel splines to compose the edge of.
+        /// </param>
+        /// <param name="color">
+        ///     The color to assign to the splines the edge will be composed of. If not specified, <see cref="System.Drawing.Color.Black" /> is used.
+        /// </param>
+        public virtual void SetMultiline(int splineCount, DotColor color = null)
+        {
+            color ??= System.Drawing.Color.Black;
+            var colors = Enumerable.Range(0, splineCount)
+               .Select(i => new DotColor(color.Color, color.Scheme));
+
+            SetMultiline(colors.ToArray());
+        }
+
+        /// <summary>
+        ///     Converts the current edge to multiple parallel splines with the specified colors.
+        /// </summary>
+        /// <param name="splines">
+        ///     The colors to assign to individual splines the edge will be composed of. Note that weighted colors (
+        ///     <see cref="DotWeightedColor" />) should not be used among the colors.
+        /// </param>
+        public virtual void SetMultiline(params DotColor[] splines)
+        {
+            SetMultiline(new DotMultiColor(splines));
+        }
+
+        /// <summary>
+        ///     Converts the current edge to multiple parallel splines with the specified colors.
+        /// </summary>
+        /// <param name="splines">
+        ///     The colors to assign to individual splines the edge will be composed of. Note that weighted colors (
+        ///     <see cref="DotWeightedColor" />) should not be used among the colors.
+        /// </param>
+        public virtual void SetMultiline(DotMultiColor splines)
+        {
+            if (splines.Colors.Any(color => color is DotWeightedColor))
+            {
+                throw new ArgumentException("Weighted colors cannot be applied to parallel edge splines.", nameof(splines));
+            }
+
+            Color = splines;
         }
     }
 }
