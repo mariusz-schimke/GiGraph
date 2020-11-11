139b9b27124c142f846297b0fbecf95f2d4b699c
mariusz.schimke <mariusz.schimke@gmail.com>
2020-11-11 22:55:55 +0100

    Unit tests cleanup

--- GiGraph.Dot.Entities.Tests/Attributes/DotAttributeSupportTest.cs
+++ GiGraph.Dot.Entities.Tests/Attributes/DotAttributeSupportTest.cs
@@ -13,64 +13,30 @@ namespace GiGraph.Dot.Entities.Tests.Attributes
 {
     public class DotAttributeSupportTest
     {
-        [Fact]
-        public void graph_supports_correct_attribute_keys()
+        public static IEnumerable<object[]> EntityAttributeKeyMappings =>
+            new List<object[]>
+            {
+                new object[] { DotEntityTypes.Graph, new DotGraph().Attributes.GetKeyMapping() },
+                new object[] { DotEntityTypes.Graph, new DotGraph().Clusters.Attributes.GetKeyMapping() },
+                new object[] { DotEntityTypes.Cluster, new DotCluster("").Attributes.GetKeyMapping() },
+                new object[] { DotEntityTypes.Subgraph, new DotSubgraph().Attributes.GetKeyMapping() },
+                new object[] { DotEntityTypes.Node, new DotNode("").Attributes.GetKeyMapping() },
+                new object[] { DotEntityTypes.Edge, new DotEdge("").Attributes.GetKeyMapping() }
+            };
+
+        [Theory]
+        [MemberData(nameof(EntityAttributeKeyMappings))]
+        public void entity_supports_correct_attribute_keys(DotEntityTypes entityType, Dictionary<string, string> entityKeyMapping)
         {
-            var keys = GetSupportedKeysFor(DotEntityTypes.Graph);
+            var validKeys = GetSupportedKeysFor(entityType);
 
-            var graph = new DotGraph();
-            var graphKeyMapping = graph.Attributes.GetKeyMapping();
-            var graphClusterKeyMapping = graph.Clusters.Attributes.GetKeyMapping();
-
-            AssertSupportsValidAttributes(graphKeyMapping, keys);
-            AssertSupportsValidAttributes(graphClusterKeyMapping, keys);
-        }
-
-        [Fact]
-        public void cluster_supports_correct_attribute_keys()
-        {
-            var keys = GetSupportedKeysFor(DotEntityTypes.Cluster);
-            var mapping = new DotCluster("").Attributes.GetKeyMapping();
-
-            AssertSupportsValidAttributes(mapping, keys);
-        }
-
-        [Fact]
-        public void subgraph_supports_correct_attribute_keys()
-        {
-            var keys = GetSupportedKeysFor(DotEntityTypes.Subgraph);
-            var mapping = new DotSubgraph().Attributes.GetKeyMapping();
-
-            AssertSupportsValidAttributes(mapping, keys);
-        }
-        
-        [Fact]
-        public void node_supports_correct_attribute_keys()
-        {
-            var keys = GetSupportedKeysFor(DotEntityTypes.Node);
-            var mapping = new DotNode("").Attributes.GetKeyMapping();
-
-            AssertSupportsValidAttributes(mapping, keys);
-        }
-        
-        [Fact]
-        public void edge_supports_correct_attribute_keys()
-        {
-            var keys = GetSupportedKeysFor(DotEntityTypes.Edge);
-            var mapping = new DotEdge("").Attributes.GetKeyMapping();
-
-            AssertSupportsValidAttributes(mapping, keys);
-        }
-
-        private void AssertSupportsValidAttributes(Dictionary<string, string> entityKeyMapping, string[] validKeys)
-        {
             var result = entityKeyMapping.Keys
                .Except(validKeys.Intersect(entityKeyMapping.Keys))
                .ToArray();
 
             if (result.Any())
             {
-                throw new Exception($"Incorrect attributes for entity: {string.Join(", ", result)}");
+                throw new Exception($"Incorrect attributes supported by {entityType}: {string.Join(", ", result)}");
             }
         }
 
