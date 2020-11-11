4e829de3a628b061a6b39f5da336f15830f0d2e1
mariusz.schimke <mariusz.schimke@gmail.com>
2020-11-11 19:10:40 +0100

    Snapshooter added for DOT rendering tests

--- GiGraph.Dot.Output.Generators.Tests/GraphGeneratorTest.cs
+++ GiGraph.Dot.Output.Generators.Tests/GraphGeneratorTest.cs
@@ -1,5 +1,4 @@
 using System.Drawing;
-using System.Text;
 using GiGraph.Dot.Entities.Attributes.Enums;
 using GiGraph.Dot.Entities.Edges.Enums;
 using GiGraph.Dot.Entities.Graphs;
@@ -7,6 +6,7 @@ using GiGraph.Dot.Entities.Subgraphs;
 using GiGraph.Dot.Entities.Types.Styles;
 using GiGraph.Dot.Extensions;
 using GiGraph.Dot.Output.Options;
+using Snapshooter.Xunit;
 using Xunit;
 
 namespace GiGraph.Dot.Output.Generators.Tests
@@ -119,51 +119,9 @@ namespace GiGraph.Dot.Output.Generators.Tests
         public void graph_with_all_possible_elements_is_rendered_according_to_default_rules_and_options()
         {
             var graph = GetCompleteGraph(directed: true);
+            var dot = graph.Build(syntaxOptions: _syntaxOptions, syntaxRules: _syntaxRules);
 
-            var sb = new StringBuilder();
-            sb.AppendLine("digraph graph1");
-            sb.AppendLine("{");
-
-            sb.AppendLine("    comment = graph_comment");
-            sb.AppendLine("    compound = true");
-            sb.AppendLine("    fillcolor = brown");
-            sb.AppendLine();
-
-            sb.AppendLine("    node [ color = red, label = node_label ]");
-            sb.AppendLine("    edge [ color = blue, label = edge_label ]");
-            sb.AppendLine();
-
-            sb.AppendLine("    subgraph Subgraph2");
-            sb.AppendLine("    {");
-            sb.AppendLine("    }");
-            sb.AppendLine();
-
-            sb.AppendLine("    subgraph Subgraph1");
-            sb.AppendLine("    {");
-            sb.AppendLine("    }");
-            sb.AppendLine();
-
-            sb.AppendLine("    subgraph \"cluster Cluster2\"");
-            sb.AppendLine("    {");
-            sb.AppendLine("    }");
-            sb.AppendLine();
-
-            sb.AppendLine("    subgraph \"cluster Cluster1\"");
-            sb.AppendLine("    {");
-            sb.AppendLine("    }");
-            sb.AppendLine();
-
-            sb.AppendLine("    node3 [ shape = assembly, style = bold ]");
-            sb.AppendLine("    node1, node2 [ shape = box, style = \"dashed, bold\" ]");
-            sb.AppendLine();
-
-            sb.AppendLine("    node6:port6:e -> node7 [ color = gold, style = dotted ]");
-            sb.AppendLine("    node4 -> { snode1 snode2 } -> node5 [ constraint = true, style = solid ]");
-            sb.AppendLine("    node1 -> node2 -> node3 [ color = beige, style = invis ]");
-
-            sb.Append("}");
-
-            Assert.Equal(sb.ToString(), graph.Build(syntaxOptions: _syntaxOptions, syntaxRules: _syntaxRules));
+            Snapshot.Match(dot, "directed_graph_default_options.gv");
         }
 
         [Fact]
@@ -171,51 +129,10 @@ namespace GiGraph.Dot.Output.Generators.Tests
         {
             var graph = GetCompleteGraph(directed: true);
 
-            var sb = new StringBuilder();
-            sb.AppendLine("digraph graph1");
-            sb.AppendLine("{");
-
-            sb.AppendLine("    comment = graph_comment");
-            sb.AppendLine("    compound = true");
-            sb.AppendLine("    fillcolor = brown");
-            sb.AppendLine();
-
-            sb.AppendLine("    node [ color = red, label = node_label ]");
-            sb.AppendLine("    edge [ color = blue, label = edge_label ]");
-            sb.AppendLine();
-
-            sb.AppendLine("    subgraph Subgraph1");
-            sb.AppendLine("    {");
-            sb.AppendLine("    }");
-            sb.AppendLine();
-
-            sb.AppendLine("    subgraph Subgraph2");
-            sb.AppendLine("    {");
-            sb.AppendLine("    }");
-            sb.AppendLine();
-
-            sb.AppendLine("    subgraph \"cluster Cluster1\"");
-            sb.AppendLine("    {");
-            sb.AppendLine("    }");
-            sb.AppendLine();
-
-            sb.AppendLine("    subgraph \"cluster Cluster2\"");
-            sb.AppendLine("    {");
-            sb.AppendLine("    }");
-            sb.AppendLine();
-
-            sb.AppendLine("    node1, node2 [ shape = box, style = \"bold, dashed\" ]");
-            sb.AppendLine("    node3 [ shape = assembly, style = bold ]");
-            sb.AppendLine();
-
-            sb.AppendLine("    node1 -> node2 -> node3 [ color = beige, style = invis ]");
-            sb.AppendLine("    node4 -> { snode1 snode2 } -> node5 [ constraint = true, style = solid ]");
-            sb.AppendLine("    node6:port6:e -> node7 [ color = gold, style = dotted ]");
-
-            sb.Append("}");
-
             var options = DotSyntaxOptions.Custom(o => o.SortElements = true);
-            Assert.Equal(sb.ToString(), graph.Build(syntaxOptions: options, syntaxRules: _syntaxRules));
+            var dot = graph.Build(syntaxOptions: options, syntaxRules: _syntaxRules);
+
+            Snapshot.Match(dot, "directed_graph_sorted.gv");
         }
 
         [Fact]
@@ -223,66 +140,19 @@ namespace GiGraph.Dot.Output.Generators.Tests
         {
             var graph = GetCompleteGraph(directed: true);
 
-            var sb = new StringBuilder();
-            sb.AppendLine("digraph \"graph1\"");
-            sb.AppendLine("{");
-
-            sb.AppendLine("    comment = graph_comment");
-            sb.AppendLine("    compound = true");
-            sb.AppendLine("    fillcolor = brown");
-            sb.AppendLine();
-
-            sb.AppendLine("    node [ color = red, label = node_label ]");
-            sb.AppendLine("    edge [ color = blue, label = edge_label ]");
-            sb.AppendLine();
-
-            sb.AppendLine("    subgraph \"Subgraph2\"");
-            sb.AppendLine("    {");
-            sb.AppendLine("    }");
-            sb.AppendLine();
-
-            sb.AppendLine("    subgraph \"Subgraph1\"");
-            sb.AppendLine("    {");
-            sb.AppendLine("    }");
-            sb.AppendLine();
-
-            sb.AppendLine("    subgraph \"cluster Cluster2\"");
-            sb.AppendLine("    {");
-            sb.AppendLine("    }");
-            sb.AppendLine();
-
-            sb.AppendLine("    subgraph \"cluster Cluster1\"");
-            sb.AppendLine("    {");
-            sb.AppendLine("    }");
-            sb.AppendLine();
-
-            sb.AppendLine("    \"node3\" [ shape = assembly, style = bold ]");
-            sb.AppendLine("    \"node1\", \"node2\" [ shape = box, style = \"dashed, bold\" ]");
-            sb.AppendLine();
-
-            sb.AppendLine("    \"node6\":\"port6\":\"e\" -> \"node7\" [ color = gold, style = dotted ]");
-            sb.AppendLine("    \"node4\" -> { \"snode1\" \"snode2\" } -> \"node5\" [ constraint = true, style = solid ]");
-            sb.AppendLine("    \"node1\" -> \"node2\" -> \"node3\" [ color = beige, style = invis ]");
-
-            sb.Append("}");
-
             var options = DotSyntaxOptions.Custom(o => o.PreferQuotedIdentifiers = true);
-            Assert.Equal(sb.ToString(), graph.Build(syntaxOptions: options, syntaxRules: _syntaxRules));
+            var dot = graph.Build(syntaxOptions: options, syntaxRules: _syntaxRules);
+
+            Snapshot.Match(dot, "directed_graph_quoted_identifiers.gv");
         }
 
         [Fact]
         public void renders_empty_graph_in_expected_format()
         {
             var graph = new DotGraph();
+            var dot = graph.Build(syntaxOptions: _syntaxOptions, syntaxRules: _syntaxRules);
 
-            var sb = new StringBuilder();
-            sb.AppendLine("digraph");
-            sb.AppendLine("{");
-            sb.Append("}");
-
-            Assert.Equal(
-                sb.ToString(),
-                graph.Build(syntaxOptions: _syntaxOptions, syntaxRules: _syntaxRules));
+            Snapshot.Match(dot, "directed_empty_graph_default_options.gv");
         }
 
         [Fact]
@@ -290,15 +160,13 @@ namespace GiGraph.Dot.Output.Generators.Tests
         {
             var graph = new DotGraph();
 
-            var sb = new StringBuilder();
-            sb.Append("digraph { }");
-
             var options = new DotFormattingOptions
             {
                 SingleLine = true
             };
 
-            Assert.Equal(sb.ToString(), graph.Build(options, _syntaxOptions, _syntaxRules));
+            var dot = graph.Build(options, _syntaxOptions, _syntaxRules);
+            Snapshot.Match(dot, "directed_empty_graph_single_line.gv");
         }
 
         [Fact]
@@ -306,47 +174,8 @@ namespace GiGraph.Dot.Output.Generators.Tests
         {
             var graph = GetSectionedGraph(directed: true);
 
-            var sb = new StringBuilder();
-            sb.AppendLine("// graph comment");
-            sb.AppendLine("digraph graph1");
-            sb.AppendLine("{");
-
-            sb.AppendLine("    compound = true");
-            sb.AppendLine();
-
-            sb.AppendLine("    node [ label = \"node label\" ]");
-            sb.AppendLine("    edge [ label = \"edge label\" ]");
-            sb.AppendLine();
-
-            sb.AppendLine("    // subgraph comment");
-            sb.AppendLine("    {");
-            sb.AppendLine("        rank = min");
-            sb.AppendLine();
-            sb.AppendLine("        /* subgraph subsection comment */");
-            sb.AppendLine();
-            sb.AppendLine("        rank = max");
-            sb.AppendLine("    }");
-            sb.AppendLine();
-
-            sb.AppendLine("    // cluster comment");
-            sb.AppendLine("    subgraph \"cluster cluster1\"");
-            sb.AppendLine("    {");
-            sb.AppendLine("        color = blue");
-            sb.AppendLine();
-            sb.AppendLine("        /* cluster subsection comment */");
-            sb.AppendLine();
-            sb.AppendLine("        color = magenta");
-            sb.AppendLine("    }");
-            sb.AppendLine();
-
-            sb.AppendLine("    node1");
-            sb.AppendLine();
-
-            sb.AppendLine("    node1 -> node2");
-
-            sb.Append("}");
-
-            Assert.Equal(sb.ToString(), graph.Build());
+            var dot = graph.Build();
+            Snapshot.Match(dot, "directed_sectioned_graph_default_options.gv");
         }
     }
 }
