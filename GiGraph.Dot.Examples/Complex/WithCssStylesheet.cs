fe4e7cfa664b667656653f6f3eb0ac9a594a017e
mariusz.schimke <mariusz.schimke@gmail.com>
2020-11-26 22:45:05 +0100

    CSS stylesheet example added

---
+++ GiGraph.Dot.Examples/Complex/WithCssStylesheet.cs
@@ -0,0 +1,20 @@
+﻿using System.IO;
+using GiGraph.Dot.Entities.Graphs;
+
+namespace GiGraph.Dot.Examples.Complex
+{
+    public static class WithCssStylesheet
+    {
+        public static DotGraph Generate()
+        {
+            var graph = new DotGraph();
+
+            graph.Attributes.Collection.Set("stylesheet", Path.GetFullPath("stylesheet.css"));
+
+            graph.Nodes.Add("foo").Attributes.Collection.Set("class", "foo_node");
+            graph.Edges.Add("foo", "bar");
+
+            return graph;
+        }
+    }
+}
