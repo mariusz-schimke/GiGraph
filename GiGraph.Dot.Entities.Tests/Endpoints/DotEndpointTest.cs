d66f6019be4a31614788e8fd8d518683e1f8d3d0
mariusz.schimke <mariusz.schimke@gmail.com>
2020-12-09 22:41:51 +0100

    Unit tests updated

--- GiGraph.Dot.Entities.Tests/Endpoints/DotEndpointTest.cs
+++ GiGraph.Dot.Entities.Tests/Endpoints/DotEndpointTest.cs
@@ -10,8 +10,7 @@ namespace GiGraph.Dot.Entities.Tests.Endpoints
         {
             var id1 = new DotEndpoint("1");
             var id2 = new DotEndpoint("1");
-            Assert.Equal(id1, id2);
-            Assert.Equal(id1.GetHashCode(), id2.GetHashCode());
+            Assert.True(id1.IsSameEndpoint(id2));
         }
 
         [Fact]
@@ -19,8 +18,7 @@ namespace GiGraph.Dot.Entities.Tests.Endpoints
         {
             var id1 = new DotEndpoint("1");
             var id2 = new DotEndpoint("2");
-            Assert.NotEqual(id1, id2);
-            Assert.NotEqual(id1.GetHashCode(), id2.GetHashCode());
+            Assert.False(id1.IsSameEndpoint(id2));
         }
 
         [Fact]
@@ -28,8 +26,7 @@ namespace GiGraph.Dot.Entities.Tests.Endpoints
         {
             var id1 = new DotEndpoint("1");
             var id2 = new DotClusterEndpoint("1");
-            Assert.NotEqual(id1, id2);
-            Assert.NotEqual(id1.GetHashCode(), id2.GetHashCode());
+            Assert.False(id1.IsSameEndpoint(id2));
         }
     }
 }
