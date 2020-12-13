785d1d73771d829c0e5ce73069946f9b9e192e70
mariusz.schimke <mariusz.schimke@gmail.com>
2020-12-13 16:17:20 +0100

    Cluster collection refactoring

--- GiGraph.Dot.Entities/Clusters/Collections/DotClusterCollection.cs
+++ GiGraph.Dot.Entities/Clusters/Collections/DotClusterCollection.cs
@@ -30,11 +30,11 @@ namespace GiGraph.Dot.Entities.Clusters.Collections
         ///     will be treated as one cluster when visualized.
         /// </param>
         /// <param name="nodeIds">
-        ///     Optional node identifiers to initialize the cluster with.
+        ///     Optional node identifiers to populate the cluster with.
         /// </param>
         public virtual DotCluster Add(string id, params string[] nodeIds)
         {
-            return AddCluster(id, nodeIds, init: null);
+            return AddCluster(id, nodeIds);
         }
 
         /// <summary>
@@ -45,35 +45,14 @@ namespace GiGraph.Dot.Entities.Clusters.Collections
         ///     will be treated as one cluster when visualized.
         /// </param>
         /// <param name="nodeIds">
-        ///     Optional node identifiers to initialize the cluster with.
+        ///     A node identifier collection to populate the cluster with.
         /// </param>
-        /// <param name="init">
-        ///     An optional cluster initialization delegate.
-        /// </param>
-        public virtual DotCluster Add(string id, Action<DotCluster> init, params string[] nodeIds)
+        public virtual DotCluster Add(string id, IEnumerable<string> nodeIds)
         {
-            return AddCluster(id, nodeIds, init);
+            return AddCluster(id, nodeIds);
         }
 
-        /// <summary>
-        ///     Adds a new cluster subgraph with the specified identifier and nodes to the collection.
-        /// </summary>
-        /// <param name="id">
-        ///     The unique identifier of the cluster. If no identifier or the same identifier is specified for multiple clusters added, they
-        ///     will be treated as one cluster when visualized.
-        /// </param>
-        /// <param name="nodeIds">
-        ///     A node identifier collection to initialize the cluster with.
-        /// </param>
-        /// <param name="init">
-        ///     An optional cluster initialization delegate.
-        /// </param>
-        public virtual DotCluster Add(string id, IEnumerable<string> nodeIds, Action<DotCluster> init = null)
-        {
-            return AddCluster(id, nodeIds, init);
-        }
-
-        protected virtual DotCluster AddCluster(string id, IEnumerable<string> nodeIds, Action<DotCluster> init)
+        protected virtual DotCluster AddCluster(string id, IEnumerable<string> nodeIds, Action<DotCluster> init = null)
         {
             return Add(DotCluster.FromNodes(id, nodeIds), init);
         }
