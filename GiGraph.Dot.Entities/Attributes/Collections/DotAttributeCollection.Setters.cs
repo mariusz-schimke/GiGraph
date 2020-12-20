5059b33120a0e7686ba4b18943bb94a5dc224a7a
mariusz.schimke <mariusz.schimke@gmail.com>
2020-12-21 00:12:39 +0100

    Rectangle array support added

--- GiGraph.Dot.Entities/Attributes/Collections/DotAttributeCollection.Setters.cs
+++ GiGraph.Dot.Entities/Attributes/Collections/DotAttributeCollection.Setters.cs
@@ -568,6 +568,34 @@ namespace GiGraph.Dot.Entities.Attributes.Collections
         }
 
         /// <summary>
+        ///     Adds or replaces the specified rectangle array attribute in the collection.
+        /// </summary>
+        /// <param name="key">
+        ///     The key of the attribute to include in the collection.
+        /// </param>
+        /// <param name="value">
+        ///     The value of the attribute to include in the collection.
+        /// </param>
+        public virtual DotRectangleArrayAttribute Set(string key, DotRectangle[] value)
+        {
+            return Set(new DotRectangleArrayAttribute(key, value));
+        }
+
+        /// <summary>
+        ///     Adds or replaces the specified rectangle array attribute in the collection.
+        /// </summary>
+        /// <param name="key">
+        ///     The key of the attribute to include in the collection.
+        /// </param>
+        /// <param name="value">
+        ///     The value of the attribute to include in the collection.
+        /// </param>
+        public virtual DotRectangleArrayAttribute Set(string key, IEnumerable<DotRectangle> value)
+        {
+            return Set(new DotRectangleArrayAttribute(key, value));
+        }
+
+        /// <summary>
         ///     Adds or replaces the specified aspect ratio attribute in the collection.
         /// </summary>
         /// <param name="key">
---
+++ GiGraph.Dot.Entities/Attributes/DotRectangleArrayAttribute.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using GiGraph.Dot.Entities.Types.Geometry;
+using GiGraph.Dot.Output.Options;
+
+namespace GiGraph.Dot.Entities.Attributes
+{
+    /// <summary>
+    ///     A rectangle array value attribute.
+    /// </summary>
+    public class DotRectangleArrayAttribute : DotAttribute<DotRectangle[]>
+    {
+        /// <summary>
+        ///     Creates a new instance of the attribute.
+        /// </summary>
+        /// <param name="key">
+        ///     The key of the attribute.
+        /// </param>
+        /// <param name="value">
+        ///     The value of the attribute.
+        /// </param>
+        public DotRectangleArrayAttribute(string key, DotRectangle[] value)
+            : base(key, value)
+        {
+        }
+
+        /// <summary>
+        ///     Creates a new instance of the attribute.
+        /// </summary>
+        /// <param name="key">
+        ///     The key of the attribute.
+        /// </param>
+        /// <param name="value">
+        ///     The value of the attribute.
+        /// </param>
+        public DotRectangleArrayAttribute(string key, IEnumerable<DotRectangle> value)
+            : base(key, value?.ToArray())
+        {
+        }
+
+        protected internal override string GetDotEncodedValue(DotSyntaxOptions options, DotSyntaxRules syntaxRules)
+        {
+            return string.Join(
+                " ",
+                (Value ?? Enumerable.Empty<DotRectangle>()).Select(value => value.GetDotEncodedValue(options, syntaxRules))
+            );
+        }
+    }
+}
