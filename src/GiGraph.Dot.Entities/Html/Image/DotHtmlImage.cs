4f784378c39af73cd6ffa4ab3adb3b4c0aaab8c8
Mariusz Schimke <mariusz.schimke@gmail.com>
2021-07-30 17:48:59 +0200

    HTML image element support added

---
+++ src/GiGraph.Dot.Entities/Html/Image/DotHtmlImage.cs
@@ -0,0 +1,44 @@
+using GiGraph.Dot.Entities.Html.Image.Attributes;
+using GiGraph.Dot.Types.Images;
+
+namespace GiGraph.Dot.Entities.Html.Image
+{
+    /// <summary>
+    ///     An HTML &lt;image&gt; element.
+    /// </summary>
+    public class DotHtmlImage : DotHtmlElement, IDotHtmlImageAttributes
+    {
+        /// <summary>
+        ///     Initializes a new font element instance.
+        /// </summary>
+        public DotHtmlImage()
+            : this(new DotHtmlImageAttributes())
+        {
+        }
+
+        protected DotHtmlImage(DotHtmlImageAttributes attributes)
+            : base("image", attributes.Collection)
+        {
+            Attributes = attributes;
+        }
+
+        /// <summary>
+        ///     The attributes of the font.
+        /// </summary>
+        public new virtual DotHtmlImageAttributes Attributes { get; }
+
+        /// <inheritdoc cref="IDotHtmlImageAttributes.Source" />
+        public virtual string Source
+        {
+            get => ((IDotHtmlImageAttributes) Attributes).Source;
+            set => ((IDotHtmlImageAttributes) Attributes).Source = value;
+        }
+
+        /// <inheritdoc cref="IDotHtmlImageAttributes.Scaling" />
+        public virtual DotImageScaling? Scaling
+        {
+            get => ((IDotHtmlImageAttributes) Attributes).Scaling;
+            set => ((IDotHtmlImageAttributes) Attributes).Scaling = value;
+        }
+    }
+}
--- src/GiGraph.Dot.Types/Images/DotImageScaling.cs
+++ src/GiGraph.Dot.Types/Images/DotImageScaling.cs
@@ -1,4 +1,5 @@
 ﻿using GiGraph.Dot.Output.Metadata;
+using GiGraph.Dot.Output.Metadata.Html;
 
 namespace GiGraph.Dot.Types.Images
 {
@@ -11,31 +12,36 @@ namespace GiGraph.Dot.Types.Images
         ///     The image retains its natural size.
         /// </summary>
         [DotAttributeValue("false")]
+        [DotHtmlElementAttributeValue("FALSE")]
         None,
 
         /// <summary>
-        ///     The image is uniformly scaled (i.e., its aspect ratio is preserved) to fit inside the node. At least one dimension of the
-        ///     image will be as large as possible given the size of the node.
+        ///     The image is uniformly scaled (i.e., its aspect ratio is preserved) to fit inside the node or table cell. At least one
+        ///     dimension of the image will be as large as possible given the size of the node.
         /// </summary>
         [DotAttributeValue("true")]
+        [DotHtmlElementAttributeValue("TRUE")]
         Uniform,
 
         /// <summary>
-        ///     The width of the image is scaled to fill the node width.
+        ///     The width of the image is scaled to fill the node or table cell width.
         /// </summary>
         [DotAttributeValue("width")]
+        [DotHtmlElementAttributeValue("WIDTH")]
         FillWidth,
 
         /// <summary>
-        ///     The height of the image is scaled to fill the node height.
+        ///     The height of the image is scaled to fill the node or table cell height.
         /// </summary>
         [DotAttributeValue("height")]
+        [DotHtmlElementAttributeValue("HEIGHT")]
         FillHeight,
 
         /// <summary>
-        ///     Both the height and the width are scaled separately to fill the node.
+        ///     Both the height and the width are scaled separately to fill the node or table cell.
         /// </summary>
         [DotAttributeValue("both")]
+        [DotHtmlElementAttributeValue("BOTH")]
         FillBoth
     }
 }
