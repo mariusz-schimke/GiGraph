da92acbe07b02b12766e96fb727fdbb812955be6
Mariusz Schimke <mariusz.schimke@gmail.com>
2021-07-31 17:33:53 +0200

    HTML table row test added

---
+++ src/GiGraph.Dot.Entities.Tests/Html/Table/DotHtmlTableRowTest.cs
@@ -0,0 +1,25 @@
+using GiGraph.Dot.Entities.Html.Table;
+using GiGraph.Dot.Output;
+using GiGraph.Dot.Output.Options;
+using Snapshooter.Xunit;
+using Xunit;
+
+namespace GiGraph.Dot.Entities.Tests.Html.Table
+{
+    public class DotHtmlTableRowTest
+    {
+        private readonly DotSyntaxOptions _syntaxOptions = new();
+        private readonly DotSyntaxRules _syntaxRules = new();
+
+        [Fact]
+        public void encoded_html_table_row_is_valid_html()
+        {
+            var row = new DotHtmlTableRow();
+
+            Snapshot.Match(
+                ((IDotHtmlEncodable) row).ToHtml(_syntaxOptions, _syntaxRules),
+                "html_table_row"
+            );
+        }
+    }
+}
