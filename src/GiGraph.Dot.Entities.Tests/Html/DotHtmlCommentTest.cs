d4cc289591843d7a264f1e5046549bfe7a872bf3
Mariusz Schimke <mariusz.schimke@gmail.com>
2021-07-31 16:52:08 +0200

    HTML comment test added

---
+++ src/GiGraph.Dot.Entities.Tests/Html/DotHtmlCommentTest.cs
@@ -0,0 +1,26 @@
+using System;
+using GiGraph.Dot.Entities.Html;
+using GiGraph.Dot.Output;
+using GiGraph.Dot.Output.Options;
+using Snapshooter.Xunit;
+using Xunit;
+
+namespace GiGraph.Dot.Entities.Tests.Html
+{
+    public class DotHtmlCommentTest
+    {
+        private readonly DotSyntaxOptions _syntaxOptions = new();
+        private readonly DotSyntaxRules _syntaxRules = new();
+
+        [Fact]
+        public void encoded_html_comment_is_valid_html()
+        {
+            var tag = new DotHtmlComment($"My custom comment{Environment.NewLine} < > \" ' & \u00A0");
+
+            Snapshot.Match(
+                ((IDotHtmlEncodable) tag).ToHtml(_syntaxOptions, _syntaxRules),
+                "html_comment"
+            );
+        }
+    }
+}
