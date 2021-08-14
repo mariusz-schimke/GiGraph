f97410fd02f9deea683c61824bc2f6329270369b
Mariusz Schimke <mariusz.schimke@gmail.com>
2021-08-14 09:28:15 +0200

    Refactoring

--- src/GiGraph.Dot.Entities/Html/DotHtmlElement.IEnumerable.cs
+++ src/GiGraph.Dot.Entities/Html/DotHtmlElement.IEnumerable.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 namespace GiGraph.Dot.Entities.Html
 {
     // Implemented in order to provide the collection initializer syntax for HTML elements with content.
-    public partial class DotHtmlElement : ICollection<IDotHtmlEntity>
+    public partial class DotHtmlElement : IEnumerable<IDotHtmlEntity>
     {
         IEnumerator<IDotHtmlEntity> IEnumerable<IDotHtmlEntity>.GetEnumerator()
         {
@@ -26,29 +26,5 @@ namespace GiGraph.Dot.Entities.Html
         {
             Content.Add(item);
         }
-
-        void ICollection<IDotHtmlEntity>.Clear()
-        {
-            Content.Clear();
-        }
-
-        bool ICollection<IDotHtmlEntity>.Contains(IDotHtmlEntity item)
-        {
-            return Content.Contains(item);
-        }
-
-        void ICollection<IDotHtmlEntity>.CopyTo(IDotHtmlEntity[] array, int arrayIndex)
-        {
-            Content.CopyTo(array, arrayIndex);
-        }
-
-        bool ICollection<IDotHtmlEntity>.Remove(IDotHtmlEntity item)
-        {
-            return Content.Remove(item);
-        }
-
-        int ICollection<IDotHtmlEntity>.Count => Content.Count;
-
-        bool ICollection<IDotHtmlEntity>.IsReadOnly => ((ICollection<IDotHtmlEntity>) Content).IsReadOnly;
     }
 }
