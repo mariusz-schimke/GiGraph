95dfcb8e4ab4000526144eae2cd5e689c85bb7ae
Mariusz Schimke <mariusz.schimke@gmail.com>
2021-08-14 09:42:32 +0200

    Cleanup

--- src/GiGraph.Dot.Entities/Html/DotHtml.cs
+++ src/GiGraph.Dot.Entities/Html/DotHtml.cs
@@ -7,10 +7,6 @@ namespace GiGraph.Dot.Entities.Html
     /// </summary>
     public class DotHtml : DotHtmlEntity
     {
-        // TODO: dodać osobną klasę z kodami HTML: https://www.rapidtables.com/web/html/html-codes.html
-        // a może to mógłby być enum, gdzie wartość to kod znaku, a atrybut określałby wartość tekstową html?
-        // Wtedy konwersja do HTML-a mogłaby bazować na opcjach globalnych (np. HEX/DEC/Kod jeśli kod dostępny)
-
         protected readonly string _html;
 
         /// <summary>
