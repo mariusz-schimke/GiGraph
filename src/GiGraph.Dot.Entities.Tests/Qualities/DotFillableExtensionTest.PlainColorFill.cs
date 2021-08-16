7e98991859e9ad180cb4de416da13c5705a5cd66
Mariusz Schimke <mariusz.schimke@gmail.com>
2021-08-16 20:40:28 +0200

    DotFillableExtensionTest unit test added

---
+++ src/GiGraph.Dot.Entities.Tests/Qualities/DotFillableExtensionTest.PlainColorFill.cs
@@ -0,0 +1,37 @@
+using System.Drawing;
+using GiGraph.Dot.Entities.Graphs;
+using GiGraph.Dot.Extensions;
+using Snapshooter.Xunit;
+using Xunit;
+
+namespace GiGraph.Dot.Entities.Tests.Qualities
+{
+    public partial class DotFillableExtensionTest
+    {
+        [Fact]
+        public void sets_plain_color_fill_on_node()
+        {
+            var graph = new DotGraph();
+            graph.Nodes.Add("node").SetPlainColorFill(Color.Red);
+
+            Snapshot.Match(graph.Build(), "plain_color_fill_on_node");
+        }
+
+        [Fact]
+        public void sets_plain_color_fill_on_node_group()
+        {
+            var graph = new DotGraph();
+            graph.Nodes.AddGroup("node1", "node2").SetPlainColorFill(Color.Red);
+
+            Snapshot.Match(graph.Build(), "plain_color_fill_on_node_group");
+        }
+
+        [Fact]
+        public void sets_plain_color_fill_on_node_collection()
+        {
+            var graph = new DotGraph();
+            graph.Nodes.SetPlainColorFill(Color.Red);
+            Snapshot.Match(graph.Build(), "plain_color_fill_on_node_collection");
+        }
+    }
+}
---
+++ src/GiGraph.Dot.Entities.Tests/Qualities/DotFillableExtensionTest.RadialGradientFill.cs
@@ -0,0 +1,50 @@
+using System.Drawing;
+using GiGraph.Dot.Entities.Graphs;
+using GiGraph.Dot.Extensions;
+using GiGraph.Dot.Types.Colors;
+using Snapshooter.Xunit;
+using Xunit;
+
+namespace GiGraph.Dot.Entities.Tests.Qualities
+{
+    public partial class DotFillableExtensionTest
+    {
+        [Fact]
+        public void sets_radial_gradient_fill_on_node()
+        {
+            var graph = new DotGraph();
+            graph.Nodes.Add("node1").SetRadialGradientFill(new DotGradientColor(Color.Red, Color.Brown));
+            graph.Nodes.Add("node2").SetRadialGradientFill(new DotGradientColor(Color.Red, Color.Brown), 45);
+
+            Snapshot.Match(graph.Build(), "radial_gradient_fill_on_node");
+        }
+
+        [Fact]
+        public void sets_radial_gradient_fill_on_node_group()
+        {
+            var graph = new DotGraph();
+            graph.Nodes.AddGroup("node1", "node2").SetRadialGradientFill(new DotGradientColor(Color.Red, Color.Brown));
+            graph.Nodes.AddGroup("node3", "node4").SetRadialGradientFill(new DotGradientColor(Color.Red, Color.Brown), 45);
+
+            Snapshot.Match(graph.Build(), "radial_gradient_fill_on_node_group");
+        }
+
+        [Fact]
+        public void sets_radial_gradient_fill_on_node_collection()
+        {
+            var graph = new DotGraph();
+            graph.Nodes.SetRadialGradientFill(new DotGradientColor(Color.Red, Color.Brown));
+
+            Snapshot.Match(graph.Build(), "radial_gradient_fill_on_node_collection");
+        }
+
+        [Fact]
+        public void sets_radial_gradient_fill_with_angle_on_node_collection()
+        {
+            var graph = new DotGraph();
+            graph.Nodes.SetRadialGradientFill(new DotGradientColor(Color.Red, Color.Brown), 45);
+
+            Snapshot.Match(graph.Build(), "radial_gradient_fill_on_node_collection_with_angle");
+        }
+    }
+}
