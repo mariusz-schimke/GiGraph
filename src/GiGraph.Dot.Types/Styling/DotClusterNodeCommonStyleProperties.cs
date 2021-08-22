db59a4eed1101b0c446bf588bebfdc288852af90
Mariusz Schimke <mariusz.schimke@gmail.com>
2021-08-22 19:20:13 +0200

    Refactoring

--- src/GiGraph.Dot.Types/Styling/DotClusterNodeCommonStyleProperties.cs
+++ src/GiGraph.Dot.Types/Styling/DotClusterNodeCommonStyleProperties.cs
@@ -8,26 +8,26 @@ namespace GiGraph.Dot.Types.Styling
         /// <summary>
         ///     The fill style.
         /// </summary>
-        public virtual TFillStyle FillStyle { get; init; } = FillStyle;
+        public TFillStyle FillStyle { get; init; } = FillStyle;
 
         /// <summary>
         ///     The border style.
         /// </summary>
-        public virtual DotBorderStyle BorderStyle { get; init; } = BorderStyle;
+        public DotBorderStyle BorderStyle { get; init; } = BorderStyle;
 
         /// <summary>
         ///     The border weight.
         /// </summary>
-        public virtual DotBorderWeight BorderWeight { get; init; } = BorderWeight;
+        public DotBorderWeight BorderWeight { get; init; } = BorderWeight;
 
         /// <summary>
         ///     The corner style.
         /// </summary>
-        public virtual DotCornerStyle CornerStyle { get; init; } = CornerStyle;
+        public DotCornerStyle CornerStyle { get; init; } = CornerStyle;
 
         /// <summary>
         ///     Determines whether the element is invisible.
         /// </summary>
-        public virtual bool Invisible { get; init; } = Invisible;
+        public bool Invisible { get; init; } = Invisible;
     }
 }
