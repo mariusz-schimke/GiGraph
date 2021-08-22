781667df7d0463109ee6cd142cced08a1624d0ed
Mariusz Schimke <mariusz.schimke@gmail.com>
2021-08-22 19:32:55 +0200

    DotGraphStyleProperties is a record type now

--- src/GiGraph.Dot.Types/Graphs/DotGraphStyleProperties.cs
+++ src/GiGraph.Dot.Types/Graphs/DotGraphStyleProperties.cs
@@ -5,24 +5,9 @@ namespace GiGraph.Dot.Types.Graphs
     /// <summary>
     ///     Graph style options.
     /// </summary>
-    public class DotGraphStyleProperties
-    {
-        /// <summary>
-        ///     Creates and initializes a new instance.
-        /// </summary>
-        /// <param name="fillStyle">
-        ///     The fill style for graph and clusters. The only option applicable to the root graph is
-        ///     <see cref="DotClusterFillStyle.Radial" />.
-        /// </param>
-        public DotGraphStyleProperties(DotClusterFillStyle fillStyle = DotClusterFillStyle.None)
-        {
-            FillStyle = fillStyle;
-        }
-
-        /// <summary>
-        ///     Gets or sets a fill style for graph and clusters. The only option applicable to the root graph is
-        ///     <see cref="DotClusterFillStyle.Radial" />.
-        /// </summary>
-        public virtual DotClusterFillStyle FillStyle { get; set; }
-    }
+    /// <param name="FillStyle">
+    ///     The fill style for graph and clusters. The only option applicable to the root graph is
+    ///     <see cref="DotClusterFillStyle.Radial" />.
+    /// </param>
+    public record DotGraphStyleProperties(DotClusterFillStyle FillStyle = DotClusterFillStyle.None);
 }
