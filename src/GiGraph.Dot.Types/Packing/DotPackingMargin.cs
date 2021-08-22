9330fa8bfb6bd0df081cc5c379aca135e5bcd450
Mariusz Schimke <mariusz.schimke@gmail.com>
2021-08-22 19:03:06 +0200

    DotPackingDefinition is a record type now

--- src/GiGraph.Dot.Types/Packing/DotPackingMargin.cs
+++ src/GiGraph.Dot.Types/Packing/DotPackingMargin.cs
@@ -9,32 +9,15 @@ namespace GiGraph.Dot.Types.Packing
     ///     </see>
     ///     , of a margin around each graph part being packed.
     /// </summary>
-    public class DotPackingMargin : DotPackingDefinition
+    /// <param name="Size">
+    ///     The size, in
+    ///     <see href="http://www.graphviz.org/doc/info/attrs.html#points">
+    ///         points
+    ///     </see>
+    ///     , of a margin around each graph part being packed.
+    /// </param>
+    public record DotPackingMargin(int Size) : DotPackingDefinition
     {
-        /// <summary>
-        ///     Creates a new instance initialized with margin size.
-        /// </summary>
-        /// <param name="size">
-        ///     The size, in
-        ///     <see href="http://www.graphviz.org/doc/info/attrs.html#points">
-        ///         points
-        ///     </see>
-        ///     , of a margin around each graph part being packed.
-        /// </param>
-        public DotPackingMargin(int size)
-        {
-            Size = size;
-        }
-
-        /// <summary>
-        ///     Gets or sets the size, in
-        ///     <see href="http://www.graphviz.org/doc/info/attrs.html#points">
-        ///         points
-        ///     </see>
-        ///     , of a margin around each graph part being packed.
-        /// </summary>
-        public virtual int Size { get; set; }
-
         protected internal override string GetDotEncodedValue(DotSyntaxOptions options, DotSyntaxRules syntaxRules)
         {
             return Size.ToString();
