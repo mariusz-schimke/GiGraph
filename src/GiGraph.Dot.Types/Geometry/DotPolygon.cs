7b3978d42a63ec034bbd950be7c037452e768af1
Mariusz Schimke <mariusz.schimke@gmail.com>
2021-08-22 11:43:32 +0200

    DotPolygon is a record type now

--- src/GiGraph.Dot.Types/Geometry/DotPolygon.cs
+++ src/GiGraph.Dot.Types/Geometry/DotPolygon.cs
@@ -3,73 +3,61 @@ namespace GiGraph.Dot.Types.Geometry
     /// <summary>
     ///     Geometry properties of a polygonal shape.
     /// </summary>
-    public class DotPolygon
+    /// <param name="Sides">
+    ///     Number of sides.
+    /// </param>
+    /// <param name="Regular">
+    ///     Determines whether the shape is regular.
+    /// </param>
+    /// <param name="Peripheries">
+    ///     Number of peripheries.
+    /// </param>
+    /// <param name="Rotation">
+    ///     Rotation angle.
+    /// </param>
+    /// <param name="Skew">
+    ///     Skew factor.
+    /// </param>
+    /// <param name="Distortion">
+    ///     Distortion factor.
+    /// </param>
+    public record DotPolygon(
+        int? Sides = null,
+        bool? Regular = null,
+        int? Peripheries = null,
+        double? Rotation = null,
+        double? Skew = null,
+        double? Distortion = null
+    )
     {
-        /// <summary>
-        ///     Creates and initializes a new instance.
-        /// </summary>
-        /// <param name="sides">
-        ///     Number of sides.
-        /// </param>
-        /// <param name="regular">
-        ///     Determines whether the shape is regular.
-        /// </param>
-        /// <param name="peripheries">
-        ///     Number of peripheries.
-        /// </param>
-        /// <param name="rotation">
-        ///     Rotation angle.
-        /// </param>
-        /// <param name="skew">
-        ///     Skew factor.
-        /// </param>
-        /// <param name="distortion">
-        ///     Distortion factor.
-        /// </param>
-        public DotPolygon(
-            int? sides = null,
-            bool? regular = null,
-            int? peripheries = null,
-            double? rotation = null,
-            double? skew = null,
-            double? distortion = null)
-        {
-            Sides = sides;
-            Regular = regular;
-            Peripheries = peripheries;
-            Rotation = rotation;
-            Skew = skew;
-            Distortion = distortion;
-        }
-
         /// <summary>
         ///     Gets or sets the number of sides.
         /// </summary>
-        public virtual int? Sides { get; set; }
+        public virtual int? Sides { get; init; } = Sides;
 
         /// <summary>
         ///     Gets or sets a value indicating whether the shape is regular.
         /// </summary>
-        public virtual bool? Regular { get; set; }
+        public virtual bool? Regular { get; init; } = Regular;
 
         /// <summary>
         ///     Gets or sets the number of peripheries.
         /// </summary>
-        public virtual int? Peripheries { get; set; }
+        public virtual int? Peripheries { get; init; } = Peripheries;
 
         /// <summary>
         ///     Gets or sets rotation angle.
         /// </summary>
-        public virtual double? Rotation { get; set; }
+        public virtual double? Rotation { get; init; } = Rotation;
 
         /// <summary>
         ///     Gets or sets skew factor.
         /// </summary>
-        public virtual double? Skew { get; set; }
+        public virtual double? Skew { get; init; } = Skew;
 
         /// <summary>
         ///     Gets or sets distortion factor.
         /// </summary>
-        public virtual double? Distortion { get; set; }
+        public virtual double? Distortion { get; init; } = Distortion;
     }
 }
