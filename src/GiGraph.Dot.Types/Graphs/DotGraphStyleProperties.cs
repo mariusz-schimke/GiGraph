7282ee1216bec50c506fb28e2ff4337aa07b9294
Mariusz Schimke <mariusz.schimke@gmail.com>
2021-08-22 19:40:13 +0200

    Refactoring

--- src/GiGraph.Dot.Types/Graphs/DotGraphStyleProperties.cs
+++ src/GiGraph.Dot.Types/Graphs/DotGraphStyleProperties.cs
@@ -7,7 +7,7 @@ namespace GiGraph.Dot.Types.Graphs
     /// </summary>
     /// <param name="FillStyle">
     ///     The fill style for graph and clusters. The only option applicable to the root graph is
-    ///     <see cref="DotClusterFillStyle.Radial" />.
+    ///     <see cref="GiGraph.Dot.Types.Clusters.DotClusterFillStyle.Radial" />.
     /// </param>
     public record DotGraphStyleProperties(DotClusterFillStyle FillStyle = DotClusterFillStyle.None);
 }
