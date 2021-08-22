e96babf3e664eb0498f55d8931af4f6672403c81
Mariusz Schimke <mariusz.schimke@gmail.com>
2021-08-22 17:46:51 +0200

    DotEdgeStyleProperties is a record type now

--- src/GiGraph.Dot.Types/Edges/DotEdgeStyleProperties.cs
+++ src/GiGraph.Dot.Types/Edges/DotEdgeStyleProperties.cs
@@ -5,40 +5,30 @@ namespace GiGraph.Dot.Types.Edges
     /// <summary>
     ///     Edge style options.
     /// </summary>
-    public class DotEdgeStyleProperties
+    /// <param name="LineStyle">
+    ///     A line style for the edge.
+    /// </param>
+    /// <param name="LineWeight">
+    ///     A line weight for the edge.
+    /// </param>
+    /// <param name="Invisible">
+    ///     Determines whether the element is invisible.
+    /// </param>
+    public record DotEdgeStyleProperties(DotLineStyle LineStyle = default, DotLineWeight LineWeight = default, bool Invisible = false)
     {
-        /// <summary>
-        ///     Creates and initializes a new instance.
-        /// </summary>
-        /// <param name="lineStyle">
-        ///     A line style for the edge.
-        /// </param>
-        /// <param name="lineWeight">
-        ///     A line weight for the edge.
-        /// </param>
-        /// <param name="invisible">
-        ///     Determines whether the element is invisible.
-        /// </param>
-        public DotEdgeStyleProperties(DotLineStyle lineStyle = default, DotLineWeight lineWeight = default, bool invisible = false)
-        {
-            LineStyle = lineStyle;
-            LineWeight = lineWeight;
-            Invisible = invisible;
-        }
-
         /// <summary>
         ///     Gets or sets a line style for the edge.
         /// </summary>
-        public virtual DotLineStyle LineStyle { get; set; }
+        public virtual DotLineStyle LineStyle { get; init; } = LineStyle;
 
         /// <summary>
         ///     Gets or sets a line weight for the edge.
         /// </summary>
-        public virtual DotLineWeight LineWeight { get; set; }
+        public virtual DotLineWeight LineWeight { get; init; } = LineWeight;
 
         /// <summary>
         ///     Gets or sets a value indicating whether the element is invisible.
         /// </summary>
-        public virtual bool Invisible { get; set; }
+        public virtual bool Invisible { get; init; } = Invisible;
     }
 }
