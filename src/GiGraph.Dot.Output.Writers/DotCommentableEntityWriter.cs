000c30a84cff18d078e8faa3f9fe210e9345a48a
Mariusz Schimke <mariusz.schimke@gmail.com>
2021-09-04 19:50:39 +0200

    Added a helper to handle writing commented entities

---
+++ src/GiGraph.Dot.Output.Writers/DotCommentableEntityWriter.cs
@@ -0,0 +1,51 @@
+﻿using GiGraph.Dot.Output.Writers.TokenWriter;
+
+namespace GiGraph.Dot.Output.Writers
+{
+    public class DotCommentableEntityWriter
+    {
+        protected readonly DotTokenWriter _tokenWriter;
+        protected bool? _isCommented;
+        protected bool _prependIndentation;
+
+        public DotCommentableEntityWriter(DotTokenWriter tokenWriter)
+        {
+            _tokenWriter = tokenWriter;
+        }
+
+        public virtual DotTokenWriter BeginEntity()
+        {
+            return _tokenWriter.CloneWith(
+                tw => tw.OnBeforeAppendToken = (sender, e) =>
+                {
+                    tw.OnBeforeAppendToken = null;
+
+                    if (false == _isCommented && e.IsCommentStartToken)
+                    {
+                        tw.NewLine();
+                    }
+                    else if (_prependIndentation)
+                    {
+                        tw.Indentation();
+                    }
+
+                    _isCommented = e.IsCommentStartToken;
+                });
+        }
+
+        public virtual void EndEntity(bool linger = true, bool enforceLineBreak = true)
+        {
+            // the assumption is that a commented attribute needs to have an empty line above and below
+            if (true == _isCommented)
+            {
+                _tokenWriter.EmptyLine(linger, enforceLineBreak);
+                _prependIndentation = false;
+            }
+            else
+            {
+                _tokenWriter.LineBreak(linger && !enforceLineBreak);
+                _prependIndentation = true;
+            }
+        }
+    }
+}
