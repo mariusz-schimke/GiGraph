51e391ca6faaa1eb1de6b3988fe8ad4358220725
Mariusz Schimke <mariusz.schimke@gmail.com>
2021-09-10 19:51:42 +0200

    Refactoring

---
+++ src/GiGraph.Dot.Entities/Edges/Endpoints/DotEdgeEndpoint.Attributes.cs
@@ -0,0 +1,53 @@
+using GiGraph.Dot.Entities.Edges.Endpoints.Attributes;
+using GiGraph.Dot.Entities.Labels;
+using GiGraph.Dot.Types.Arrowheads;
+using GiGraph.Dot.Types.Clusters;
+using GiGraph.Dot.Types.Edges;
+
+namespace GiGraph.Dot.Entities.Edges.Endpoints
+{
+    public abstract partial class DotEdgeEndpoint : IDotEdgeEndpointAttributes
+    {
+        /// <inheritdoc cref="IDotEdgeEndpointAttributes.Label" />
+        public virtual DotLabel Label
+        {
+            get => _attributes.Label;
+            set => _attributes.Label = value;
+        }
+
+        /// <inheritdoc cref="IDotEdgeEndpointAttributes.Port" />
+        public virtual DotEndpointPort Port
+        {
+            get => _attributes.Port;
+            set => _attributes.Port = value;
+        }
+
+        /// <inheritdoc cref="IDotEdgeEndpointAttributes.ClusterId" />
+        public virtual DotClusterId ClusterId
+        {
+            get => _attributes.ClusterId;
+            set => _attributes.ClusterId = value;
+        }
+
+        /// <inheritdoc cref="IDotEdgeEndpointAttributes.ClipToNodeBoundary" />
+        public virtual bool? ClipToNodeBoundary
+        {
+            get => _attributes.ClipToNodeBoundary;
+            set => _attributes.ClipToNodeBoundary = value;
+        }
+
+        /// <inheritdoc cref="IDotEdgeEndpointAttributes.GroupName" />
+        public virtual string GroupName
+        {
+            get => _attributes.GroupName;
+            set => _attributes.GroupName = value;
+        }
+
+        /// <inheritdoc cref="IDotEdgeEndpointAttributes.Arrowhead" />
+        public virtual DotArrowheadDefinition Arrowhead
+        {
+            get => _attributes.Arrowhead;
+            set => _attributes.Arrowhead = value;
+        }
+    }
+}
--- src/GiGraph.Dot.Entities/Graphs/Attributes/IDotGraphClusterAttributes.cs
+++ src/GiGraph.Dot.Entities/Graphs/Attributes/IDotGraphClusterAttributes.cs
@@ -1,5 +1,4 @@
 ﻿using GiGraph.Dot.Entities.Attributes.Properties.Common.GraphCluster;
-using GiGraph.Dot.Entities.Edges.Attributes;
 using GiGraph.Dot.Entities.Edges.Endpoints.Attributes;
 using GiGraph.Dot.Types.Clusters;
 
