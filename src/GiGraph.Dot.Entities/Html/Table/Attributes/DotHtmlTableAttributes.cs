a41c10ab672d77252418f0e6724450404a0a9010
Mariusz Schimke <mariusz.schimke@gmail.com>
2021-09-14 18:48:47 +0200

    HTML table attributes refactored

--- src/GiGraph.Dot.Entities/Html/Table/Attributes/DotHtmlTableAttributes.cs
+++ src/GiGraph.Dot.Entities/Html/Table/Attributes/DotHtmlTableAttributes.cs
@@ -1,50 +1,49 @@
 ﻿using System;
 using System.Reflection;
-using GiGraph.Dot.Entities.Attributes.Collections;
 using GiGraph.Dot.Entities.Attributes.Properties.KeyLookup;
-using GiGraph.Dot.Entities.Html.Attributes.Factories;
+using GiGraph.Dot.Entities.Html.Attributes.Collections;
 using GiGraph.Dot.Output.Metadata;
 using GiGraph.Dot.Types.Alignment;
 
 namespace GiGraph.Dot.Entities.Html.Table.Attributes
 {
-    public class DotHtmlTableAttributes : DotHtmlTableTableCellCommonAttributes<IDotHtmlTableAttributes>, IDotHtmlTableAttributes
+    public class DotHtmlTableAttributes : DotHtmlTableTableCellCommonAttributes, IDotHtmlTableAttributes
     {
         protected static readonly Lazy<DotMemberAttributeKeyLookup> HtmlTableAttributesKeyLookup = new DotMemberAttributeKeyLookupBuilder<DotHtmlTableAttributes, IDotHtmlTableAttributes>().BuildLazy();
 
-        protected DotHtmlTableAttributes(DotAttributeCollection attributes, Lazy<DotMemberAttributeKeyLookup> attributeKeyLookup)
+        public DotHtmlTableAttributes(DotHtmlAttributeCollection attributes)
+            : this(attributes, HtmlTableAttributesKeyLookup)
+        {
+        }
+
+        private DotHtmlTableAttributes(DotHtmlAttributeCollection attributes, Lazy<DotMemberAttributeKeyLookup> attributeKeyLookup)
             : base(attributes, attributeKeyLookup)
         {
         }
 
-        public DotHtmlTableAttributes()
-            : this(new DotAttributeCollection(DotHtmlAttributeFactory.Instance), HtmlTableAttributesKeyLookup)
-        {
-        }
-
         [DotAttributeKey("align")]
-        DotHorizontalAlignment? IDotHtmlTableAttributes.HorizontalAlignment
+        public virtual DotHorizontalAlignment? HorizontalAlignment
         {
             get => GetValueAs<DotHorizontalAlignment>(MethodBase.GetCurrentMethod(), out var result) ? result : null;
             set => SetOrRemove(MethodBase.GetCurrentMethod(), value.HasValue, () => value!.Value);
         }
 
         [DotAttributeKey("cellborder")]
-        int? IDotHtmlTableAttributes.CellBorderWidth
+        public virtual int? CellBorderWidth
         {
             get => GetValueAsInt(MethodBase.GetCurrentMethod());
             set => SetOrRemove(MethodBase.GetCurrentMethod(), value);
         }
 
         [DotAttributeKey("rows")]
-        string IDotHtmlTableAttributes.RowFormat
+        public virtual string RowFormat
         {
             get => GetValueAsString(MethodBase.GetCurrentMethod());
             set => SetOrRemove(MethodBase.GetCurrentMethod(), value);
         }
 
         [DotAttributeKey("columns")]
-        string IDotHtmlTableAttributes.ColumnFormat
+        public virtual string ColumnFormat
         {
             get => GetValueAsString(MethodBase.GetCurrentMethod());
             set => SetOrRemove(MethodBase.GetCurrentMethod(), value);
