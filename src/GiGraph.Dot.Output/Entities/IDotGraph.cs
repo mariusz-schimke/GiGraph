811a0e472996349366bce2f369e6785f1ecf1906
Mariusz Schimke <mariusz.schimke@gmail.com>
2021-09-17 23:14:46 +0200

    Comments added

--- src/GiGraph.Dot.Output/Entities/IDotGraph.cs
+++ src/GiGraph.Dot.Output/Entities/IDotGraph.cs
@@ -4,7 +4,14 @@ namespace GiGraph.Dot.Output.Entities
 {
     public interface IDotGraph : IDotEntity
     {
+        /// <summary>
+        ///     Gets the identifier of the graph.
+        /// </summary>
         string Id { get; }
+
+        /// <summary>
+        ///     Gets the subsections of the graph.
+        /// </summary>
         IEnumerable<IDotGraphSection> Subsections { get; }
     }
 }
