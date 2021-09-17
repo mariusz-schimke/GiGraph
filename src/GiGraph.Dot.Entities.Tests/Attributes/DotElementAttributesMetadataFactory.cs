4b029d2a91889353cf69d3b1ba621d750b18cddf
Mariusz Schimke <mariusz.schimke@gmail.com>
2021-09-17 21:48:37 +0200

    Unit tests adjusted

--- src/GiGraph.Dot.Entities.Tests/Attributes/DotElementAttributesMetadataFactory.cs
+++ src/GiGraph.Dot.Entities.Tests/Attributes/DotElementAttributesMetadataFactory.cs
@@ -3,7 +3,6 @@ using System.Linq;
 using System.Reflection;
 using GiGraph.Dot.Entities.Attributes.Properties;
 using GiGraph.Dot.Entities.Clusters;
-using GiGraph.Dot.Entities.Clusters.Collections;
 using GiGraph.Dot.Entities.Edges;
 using GiGraph.Dot.Entities.Graphs;
 using GiGraph.Dot.Entities.Nodes;
@@ -17,34 +16,23 @@ namespace GiGraph.Dot.Entities.Tests.Attributes
     {
         public static IEnumerable<(DotCompatibleElements Element, Dictionary<string, DotAttributePropertyMetadata> Attributes)> Create()
         {
-            var graph = new DotGraph();
-            var edge = new DotEdge("");
-
             var result = new List<(DotCompatibleElements, Dictionary<string, DotAttributePropertyMetadata>)>
             {
                 (DotCompatibleElements.Node, new DotNode("").Attributes.GetMetadataDictionary()),
                 (DotCompatibleElements.Subgraph, new DotSubgraph().Attributes.GetMetadataDictionary()),
                 (DotCompatibleElements.Cluster, new DotCluster("").Attributes.GetMetadataDictionary()),
-                (
-                    DotCompatibleElements.Graph,
-                    graph.Attributes.GetMetadataDictionary()
-                       .Concat(GetMetadataDictionary(graph.Clusters.Attributes, rootProperty: graph.GetType().GetProperty(nameof(graph.Clusters), typeof(DotGraphClusterCollection))))
-                       .ToDictionary(key => key.Key, element => element.Value)
-                ),
-                (
-                    DotCompatibleElements.Edge,
-                    edge.Attributes.GetMetadataDictionary()
-                       .Concat(GetMetadataDictionary(edge.Tail.Attributes, rootProperty: edge.GetType().GetProperty(nameof(edge.Tail))))
-                       .Concat(GetMetadataDictionary(edge.Head.Attributes, rootProperty: edge.GetType().GetProperty(nameof(edge.Head))))
-                       .ToDictionary(key => key.Key, element => element.Value)
-                )
+                (DotCompatibleElements.Graph, new DotGraph().Attributes.GetMetadataDictionary()),
+                (DotCompatibleElements.Edge, new DotEdge("").Attributes.GetMetadataDictionary())
             };
 
             result.Reverse();
             return result;
         }
 
-        public static Dictionary<string, DotAttributePropertyMetadata> GetMetadataDictionary<TIEntityAttributeProperties>(DotEntityAttributes<TIEntityAttributeProperties> attributes, PropertyInfo rootProperty)
+        public static Dictionary<string, DotAttributePropertyMetadata> GetMetadataDictionary<TIEntityAttributeProperties, TEntityAttributeProperties>(
+            DotEntityAttributesAccessor<TIEntityAttributeProperties, TEntityAttributeProperties> attributes, PropertyInfo rootProperty
+        )
+            where TEntityAttributeProperties : DotEntityAttributes<TIEntityAttributeProperties, TEntityAttributeProperties>, TIEntityAttributeProperties
         {
             var prefix = rootProperty is not null
                 ? Enumerable.Empty<PropertyInfo>().Append(rootProperty)
