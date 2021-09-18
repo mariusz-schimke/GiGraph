86df74f5788de4e0323b930b88c5018c36602e84
Mariusz Schimke <mariusz.schimke@gmail.com>
2021-09-18 08:33:43 +0200

    Refactoring

--- src/GiGraph.Dot.Entities/Html/Attributes/Properties/DotHtmlElementRootAttributesAccessor.cs
+++ src/GiGraph.Dot.Entities/Html/Attributes/Properties/DotHtmlElementRootAttributesAccessor.cs
@@ -4,13 +4,13 @@ using GiGraph.Dot.Entities.Html.Attributes.Collections;
 namespace GiGraph.Dot.Entities.Html.Attributes.Properties
 {
     public class DotHtmlElementRootAttributesAccessor<TIHtmlElementAttributeProperties, THtmlElementAttributeProperties> : DotEntityAttributesAccessor<TIHtmlElementAttributeProperties, THtmlElementAttributeProperties>
-        where THtmlElementAttributeProperties : DotHtmlEntityAttributes<TIHtmlElementAttributeProperties, THtmlElementAttributeProperties>, TIHtmlElementAttributeProperties
+        where THtmlElementAttributeProperties : DotHtmlElementAttributes<TIHtmlElementAttributeProperties, THtmlElementAttributeProperties>, TIHtmlElementAttributeProperties
     {
         public DotHtmlElementRootAttributesAccessor(THtmlElementAttributeProperties attributes)
             : base(attributes)
         {
         }
 
-        public DotHtmlAttributeCollection Collection => ((DotHtmlEntityAttributes<TIHtmlElementAttributeProperties, THtmlElementAttributeProperties>) _attributes).Collection;
+        public DotHtmlAttributeCollection Collection => ((DotHtmlElementAttributes<TIHtmlElementAttributeProperties, THtmlElementAttributeProperties>) _attributes).Collection;
     }
 }
