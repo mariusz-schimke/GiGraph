b0b432a47a1756f8e68305982054e52425005391
Mariusz Schimke <mariusz.schimke@gmail.com>
2023-08-21 16:57:09 +0200

    Add Dot to DotCompatibleOutputs

--- src/GiGraph.Dot.Output/Metadata/DotCompatibleOutputs.cs
+++ src/GiGraph.Dot.Output/Metadata/DotCompatibleOutputs.cs
@@ -2,6 +2,7 @@ using System;
 
 namespace GiGraph.Dot.Output.Metadata;
 
+// TODO: Rename to DotCompatibleOutputFormats (include properties of this type).
 /// <summary>
 ///     Determines outputs that an attribute is used for.
 /// </summary>
@@ -14,33 +15,41 @@ public enum DotCompatibleOutputs
     Any = -1,
 
     /// <summary>
+    ///     The attribute is used for text output, for example:
+    ///     <c>
+    ///         echo 'digraph G { Node0 }' | dot -Tcanon
+    ///     </c>
+    /// </summary>
+    Dot = 1 << 0,
+
+    /// <summary>
     ///     The attribute is used for the xdot output. It extends the dot format by providing much more detailed information about how
     ///     graph components are drawn. It relies on additional attributes for nodes, edges and graphs.
     /// </summary>
-    Xdot = 1 << 0,
+    Xdot = 1 << 1,
 
     /// <summary>
     ///     The attribute is used for the Windows Bitmap output.
     /// </summary>
-    Bitmap = 1 << 1,
+    Bitmap = 1 << 2,
 
     /// <summary>
     ///     The attribute is used for the Scalable Vector Graphics output.
     /// </summary>
-    Svg = 1 << 2,
+    Svg = 1 << 3,
 
     /// <summary>
     ///     The attribute is used for the Client-side imagemap output (deprecated).
     /// </summary>
-    Cmap = 1 << 3,
+    Cmap = 1 << 4,
 
     /// <summary>
     ///     The attribute is used for the map output.
     /// </summary>
-    Map = 1 << 4,
+    Map = 1 << 5,
 
     /// <summary>
     ///     The attribute is used for the PostScript output.
     /// </summary>
-    PostScript = 1 << 5
+    PostScript = 1 << 6
 }
