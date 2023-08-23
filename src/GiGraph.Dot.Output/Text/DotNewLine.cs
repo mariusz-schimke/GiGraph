4019b7c57f283855774bcb57f44b3c0fc0960cd9
Mariusz Schimke <mariusz.schimke@gmail.com>
2023-08-23 16:14:21 +0200

    Add a LineContinuation support to DotNewLine

--- src/GiGraph.Dot.Output/Text/DotNewLine.cs
+++ src/GiGraph.Dot.Output/Text/DotNewLine.cs
@@ -18,7 +18,33 @@ public static class DotNewLine
     public const string Unix = "\n";
 
     /// <summary>
+    ///     Gets the line continuation character that can be used for string attributes in order to split text in the output DOT script
+    ///     into multiple lines. Has to be followed by a line break. Does not affect the way the text is visualized (it's only for
+    ///     formatting the output script, so the line continuation character and the line break do not appear in the visualization).
+    /// </summary>
+    public const string LineContinuationChar = "\\";
+
+    /// <summary>
     ///     Gets the newline string defined for this environment.
     /// </summary>
     public static string SystemDefault => Environment.NewLine;
+
+    /// <summary>
+    ///     Returns a line continuation character followed by the system-default new line string. To be used for string attributes in
+    ///     order to split text in the output DOT script into multiple lines. Does not affect the way the text is visualized (it's only
+    ///     for formatting the output script, so the line continuation character and the line break do not appear in the visualization).
+    /// </summary>
+    public static string LineContinuation() => LineContinuation(SystemDefault);
+
+    /// <summary>
+    ///     Returns a line continuation character followed by the specified new line string. To be used for string attributes in order to
+    ///     split text in the output DOT script into multiple lines. Does not affect the way the text is visualized (it's only for
+    ///     formatting the output script, so the line continuation character and the line break do not appear in the visualization).
+    /// </summary>
+    /// <param name="newLine">
+    ///     The new line string to use.
+    /// </param>
+    public static string LineContinuation(string newLine) => string.IsNullOrEmpty(newLine)
+        ? throw new ArgumentNullException(nameof(newLine), "A new line string has to be specified for line continuation")
+        : $"{LineContinuationChar}{newLine}";
 }
