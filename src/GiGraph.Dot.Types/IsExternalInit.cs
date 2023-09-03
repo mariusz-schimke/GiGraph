397517f7cca6d4c0f3dee770d77a8bea1c5557ed
Mariusz Schimke <mariusz.schimke@gmail.com>
2023-09-03 20:21:49 +0200

    Convert the package to a single assembly to follow the guidelines for NuGet packages

--- src/GiGraph.Dot.Types/IsExternalInit.cs
+++
@@ -1,8 +0,0 @@
-namespace System.Runtime.CompilerServices;
-
-/// <summary>
-///     Removes the error CS0518: Predefined type 'System.Runtime.CompilerServices.IsExternalInit' is not defined or imported.
-/// </summary>
-internal static class IsExternalInit
-{
-}
