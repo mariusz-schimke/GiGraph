6fc03b3aa291bc3bfe106124ec9e6332048b78db
Mariusz Schimke <mariusz.schimke@gmail.com>
2025-02-15 19:29:17 +0100

    WIP: adjust code to enabled nullability

--- src/GiGraph.Dot/Entities/Edges/Collections/DotEdgeCollection.ManyToOne.cs
+++ src/GiGraph.Dot/Entities/Edges/Collections/DotEdgeCollection.ManyToOne.cs
@@ -17,7 +17,8 @@ public partial class DotEdgeCollection
     /// <param name="tailNodeIds">
     ///     The identifiers of the tail nodes.
     /// </param>
-    public virtual DotEdge<DotSubgraphEndpoint, DotEndpoint> AddManyToOne(string headNodeId, params string[] tailNodeIds) => AddManyToOne(tailNodeIds, headNodeId);
+    public virtual DotEdge<DotSubgraphEndpoint, DotEndpoint> AddManyToOne(string headNodeId, params string[] tailNodeIds) =>
+        AddManyToOne(tailNodeIds, headNodeId);
 
     /// <summary>
     ///     Adds a group of edges where all <paramref name="tailNodeIds" /> as tail nodes are joined to the
@@ -32,7 +33,8 @@ public partial class DotEdgeCollection
     /// <param name="tailNodeIds">
     ///     The identifiers of the tail nodes.
     /// </param>
-    public virtual DotEdge<DotSubgraphEndpoint, DotEndpoint> AddManyToOne(Action<DotEdge<DotSubgraphEndpoint, DotEndpoint>> init, string headNodeId, params string[] tailNodeIds) => AddManyToOne(tailNodeIds, headNodeId, init);
+    public virtual DotEdge<DotSubgraphEndpoint, DotEndpoint> AddManyToOne(Action<DotEdge<DotSubgraphEndpoint, DotEndpoint>> init, string headNodeId, params string[] tailNodeIds) =>
+        AddManyToOne(tailNodeIds, headNodeId, init);
 
     /// <summary>
     ///     Adds a group of edges where all <paramref name="tailNodeIds" /> as tail nodes are joined to the
@@ -49,7 +51,7 @@ public partial class DotEdgeCollection
     /// </param>
     public virtual DotEdge<DotSubgraphEndpoint, DotEndpoint> AddManyToOne(
         IEnumerable<string> tailNodeIds, string headNodeId,
-        Action<DotEdge<DotSubgraphEndpoint, DotEndpoint>> init = null) =>
+        Action<DotEdge<DotSubgraphEndpoint, DotEndpoint>>? init = null) =>
         AddManyToOne(DotSubgraph.FromNodes(tailNodeIds), headNodeId, init);
 
     /// <summary>
@@ -66,7 +68,7 @@ public partial class DotEdgeCollection
     /// </param>
     public virtual DotEdge<DotEndpointGroup, DotEndpoint> AddManyToOne(
         DotEndpointGroup tails, DotEndpoint head,
-        Action<DotEdge<DotEndpointGroup, DotEndpoint>> init = null) =>
+        Action<DotEdge<DotEndpointGroup, DotEndpoint>>? init = null) =>
         Add(tails, head, init);
 
     /// <summary>
@@ -83,7 +85,7 @@ public partial class DotEdgeCollection
     /// </param>
     public virtual DotEdge<DotSubgraphEndpoint, DotEndpoint> AddManyToOne(
         DotSubgraphEndpoint tails, DotEndpoint head,
-        Action<DotEdge<DotSubgraphEndpoint, DotEndpoint>> init = null) =>
+        Action<DotEdge<DotSubgraphEndpoint, DotEndpoint>>? init = null) =>
         Add(tails, head, init);
 
     /// <summary>
@@ -98,5 +100,6 @@ public partial class DotEdgeCollection
     /// <param name="init">
     ///     An optional initializer delegate to call for the created edge group.
     /// </param>
-    public virtual DotEdge<DotSubgraphEndpoint, DotEndpoint> AddManyToOne(DotSubgraph tails, string headNodeId, Action<DotEdge<DotSubgraphEndpoint, DotEndpoint>> init = null) => Add(new(tails), new(headNodeId), init);
+    public virtual DotEdge<DotSubgraphEndpoint, DotEndpoint> AddManyToOne(DotSubgraph tails, string headNodeId, Action<DotEdge<DotSubgraphEndpoint, DotEndpoint>>? init = null) =>
+        Add(new(tails), new(headNodeId), init);
 }
