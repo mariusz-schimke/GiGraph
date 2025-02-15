88af20022247f0587af2def679228ac3f5cad14a
Mariusz Schimke <mariusz.schimke@gmail.com>
2025-02-15 14:51:13 +0100

    WIP: adjust code to enabled nullability

--- src/GiGraph.Dot/Types/Colors/DotColorDefinition.cs
+++ src/GiGraph.Dot/Types/Colors/DotColorDefinition.cs
@@ -14,9 +14,14 @@ public abstract record DotColorDefinition : IDotEncodable
 
     protected internal abstract string GetDotEncodedColor(DotSyntaxOptions options, DotSyntaxRules syntaxRules);
 
-    public static implicit operator DotColorDefinition(Color? color) => color.HasValue ? new DotColor(color.Value) : null;
+    // TODO: consider using generators to generate implicit operators where possible
+    // [AutoGenerateImplicitOperators(typeof(Color), typeof(DotColor))]
+    // maybe only the nullable variants should be auto generated since they don't create a new instance explicitly?
+    
+    public static implicit operator DotColorDefinition(Color color) => new DotColor(color);
+    public static implicit operator DotColorDefinition?(Color? color) => color.HasValue ? color.Value : null;
 
-    public static implicit operator DotColorDefinition(Color[] colors) => colors is not null ? new DotMulticolor(colors) : null;
+    public static implicit operator DotColorDefinition(Color[]? colors) => colors is not null ? new DotMulticolor(colors) : null!;
 
-    public static implicit operator DotColorDefinition(DotColor[] colors) => colors is not null ? new DotMulticolor(colors) : null;
+    public static implicit operator DotColorDefinition(DotColor[]? colors) => colors is not null ? new DotMulticolor(colors) : null!;
 }
