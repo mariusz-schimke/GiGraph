ad56ecdc412e4d9c111ff5d1c6309c058c7f35d1
Mariusz Schimke <mariusz.schimke@gmail.com>
2025-03-30 21:47:52 +0200

    Revert "Cleanup"
    This reverts commit 95d97f383f836d01bfa20522f2554033228b6421.

--- src/GiGraph.Dot/Entities/Attributes/Properties/Common/Hyperlink/DotHyperlinkAttributes.Generic.cs
+++ src/GiGraph.Dot/Entities/Attributes/Properties/Common/Hyperlink/DotHyperlinkAttributes.Generic.cs
@@ -37,14 +37,16 @@ public abstract partial class DotHyperlinkAttributes<TIEntityHyperlinkAttributes
     /// <param name="target">
     ///     The target of the hyperlink. See <see cref="DotHyperlinkTargets"/> for accepted values.
     /// </param>
-    /// <param name="href">
-    ///     Synonym for <paramref name="url"/>.
-    /// </param>
-    public virtual void Set(DotEscapeString? url, DotEscapeString? target = null, DotEscapeString? href = null)
+    public virtual void Set(DotEscapeString? url, DotEscapeString? target = null)
     {
         Url = url;
         Target = target;
+    }
+
+    protected virtual void SetAll(DotEscapeString? url, DotEscapeString? target, DotEscapeString? href)
+    {
         Href = href;
+        Set(url, target);
     }
 
     /// <summary>
@@ -55,6 +57,6 @@ public abstract partial class DotHyperlinkAttributes<TIEntityHyperlinkAttributes
     /// </param>
     public virtual void Set(DotHyperlink attributes)
     {
-        Set(attributes.Url, attributes.Target, attributes.Href);
+        SetAll(attributes.Url, attributes.Target, attributes.Href);
     }
 }
