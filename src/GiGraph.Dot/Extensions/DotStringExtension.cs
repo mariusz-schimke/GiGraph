45bf25d4f77a6bdfee29b852311d8211e5f53181
Mariusz Schimke <mariusz.schimke@gmail.com>
2025-04-21 08:01:26 +0200

    Rename the AsFormattedString extension method on string to AsEscapedString

--- src/GiGraph.Dot/Extensions/DotStringExtension.cs
+++ src/GiGraph.Dot/Extensions/DotStringExtension.cs
@@ -7,19 +7,19 @@ namespace GiGraph.Dot.Extensions;
 public static class DotStringExtension
 {
     /// <summary>
-    ///     Converts the string to <see cref="DotEscapeString" /> without escaping its content. When assigned to a label of an element,
+    ///     Converts the string to <see cref="DotEscapeString"/> without escaping its content. When assigned to a label of an element,
     ///     any markups used in the text can be correctly interpreted by the visualization engine. Make sure the content complies with
     ///     the DOT syntax rules as otherwise the output may become syntactically incorrect. Consider using
-    ///     <see cref="DotFormattedTextBuilder" /> for formulating formatted text instead.
+    ///     <see cref="DotFormattedTextBuilder"/> for formulating formatted text instead.
     /// </summary>
     /// <param name="string">
     ///     The string to convert.
     /// </param>
     [Pure]
-    public static DotEscapeString AsFormattedString(this string @string) => (DotEscapedString) @string;
+    public static DotEscapedString AsEscapedString(this string @string) => @string;
 
     /// <summary>
-    ///     Converts the string to <see cref="DotHtmlString" /> without modifying it in any way. When assigned to a label of an element,
+    ///     Converts the string to <see cref="DotHtmlString"/> without modifying it in any way. When assigned to a label of an element,
     ///     it will be interpreted as HTML, not as plain text.
     /// </summary>
     /// <param name="string">
