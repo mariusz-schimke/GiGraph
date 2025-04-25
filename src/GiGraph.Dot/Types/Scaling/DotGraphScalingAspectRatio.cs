f960bd5b35c4751d9e58e8fac35cb755a21cb6f7
Mariusz Schimke <mariusz.schimke@gmail.com>
2025-04-25 22:05:33 +0200

    DotGraphScalingAspectRatio refactoring

--- src/GiGraph.Dot/Types/Scaling/DotGraphScalingAspectRatio.cs
+++ src/GiGraph.Dot/Types/Scaling/DotGraphScalingAspectRatio.cs
@@ -1,21 +1,21 @@
 using GiGraph.Dot.Output.Options;
 
-namespace GiGraph.Dot.Types.Graphs;
+namespace GiGraph.Dot.Types.Scaling;
 
 /// <summary>
 ///     The aspect ratio of the graph. If ratio = x where x is a floating point number, then the drawing is scaled up in one
 ///     dimension to achieve the requested ratio expressed as drawing height/width. For example, ratio = 2.0 makes the drawing twice
 ///     as high as it is wide.
 /// </summary>
-/// <param name="ratio">
+/// <param name="aspectRatio">
 ///     The aspect ratio.
 /// </param>
-public class DotGraphScalingAspectRatio(double ratio) : DotGraphScalingDefinition
+public class DotGraphScalingAspectRatio(double aspectRatio) : DotGraphScalingDefinition
 {
     /// <summary>
     ///     The aspect ratio.
     /// </summary>
-    public double Ratio { get; } = ratio;
+    public double AspectRatio { get; } = aspectRatio;
 
-    protected override string GetDotEncodedValue(DotSyntaxOptions options, DotSyntaxRules syntaxRules) => Ratio.ToString(syntaxRules.Culture);
+    protected override string GetDotEncodedValue(DotSyntaxOptions options, DotSyntaxRules syntaxRules) => AspectRatio.ToString(syntaxRules.Culture);
 }
