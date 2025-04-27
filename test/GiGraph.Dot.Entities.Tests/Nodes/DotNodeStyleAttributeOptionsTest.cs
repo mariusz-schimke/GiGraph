09e8922520f2a8f36cbb929c2c8274d4dec52c11
Mariusz Schimke <mariusz.schimke@gmail.com>
2025-04-27 21:02:28 +0200

    Add node style unit tests

--- test/GiGraph.Dot.Entities.Tests/Nodes/DotNodeStyleAttributeOptionsTest.cs
+++ test/GiGraph.Dot.Entities.Tests/Nodes/DotNodeStyleAttributeOptionsTest.cs
@@ -17,14 +17,16 @@ public class DotNodeStyleAttributeOptionsTest
         var node = graph.Nodes.Add("n1");
 
         // set by class
-        node.Style.SetStyleOptions(new DotNodeStyleOptions(
-            DotNodeFillStyle.Radial,
-            DotBorderStyle.Dashed,
-            DotBorderWeight.Bold,
-            DotCornerStyle.Rounded,
-            true,
-            true
-        ));
+        node.Style.SetStyleOptions(
+            new DotNodeStyleOptions(
+                DotNodeFillStyle.Radial,
+                DotBorderStyle.Dashed,
+                DotBorderWeight.Bold,
+                DotCornerStyle.Rounded,
+                true,
+                true
+            )
+        );
 
         Snapshot.Match(graph.ToDot(), snapshotName);
 
@@ -43,5 +45,43 @@ public class DotNodeStyleAttributeOptionsTest
         );
 
         Snapshot.Match(graph.ToDot(), snapshotName);
+    }
+
+    [Fact]
+    public void nullifying_last_style_option_nullifies_style()
+    {
+        var graph = new DotGraph();
+        var node = graph.Nodes.Add("n1");
+
+        Assert.False(node.Style.HasStyleOptions());
+
+        node.Style.SetStyleOptions(
+            new DotNodeStyleOptions(
+                DotNodeFillStyle.Radial,
+                DotBorderStyle.Dashed,
+                DotBorderWeight.Bold,
+                DotCornerStyle.Rounded,
+                true,
+                true
+            )
+        );
+
+        Assert.True(node.Style.HasStyleOptions());
+        Assert.False(node.Style.HasDefaultStyleOptions());
+
+        node.Style.FillStyle = null;
+        node.Style.BorderStyle = null;
+        node.Style.BorderWeight = null;
+        node.Style.Diagonals = null;
+        node.Style.Invisible = null;
+
+        Assert.True(node.Style.HasStyleOptions());
+        Assert.False(node.Style.HasDefaultStyleOptions());
+
+        node.Style.CornerStyle = DotCornerStyle.Normal;
+        Assert.True(node.Style.HasDefaultStyleOptions());
+
+        node.Style.CornerStyle = null;
+        Assert.False(node.Style.HasStyleOptions());
     }
 }
