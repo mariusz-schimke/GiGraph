8c793cf7df6611308e7b2476f6a3a715f4332936
Mariusz Schimke <28633575+mariusz-schimke@users.noreply.github.com>
2025-04-29 22:27:17 +0200

    Add cancellation token to SaveAsync (#415)

--- src/GiGraph.Dot/Extensions/DotGraphExtension.Save.cs
+++ src/GiGraph.Dot/Extensions/DotGraphExtension.Save.cs
@@ -1,5 +1,4 @@
-﻿using System.Diagnostics.Contracts;
-using System.Text;
+﻿using System.Text;
 using GiGraph.Dot.Entities.Graphs;
 using GiGraph.Dot.Output.Generators.Graphs;
 using GiGraph.Dot.Output.Generators.Providers;
@@ -10,41 +9,8 @@ using GiGraph.Dot.Output.Writers.TokenWriter;
 
 namespace GiGraph.Dot.Extensions;
 
-/// <summary>
-///     Provides extension methods for <see cref="DotGraph"/>.
-/// </summary>
-public static class DotGraphExtension
+public static partial class DotGraphExtension
 {
-    /// <summary>
-    ///     Converts the graph to its DOT format representation.
-    /// </summary>
-    /// <param name="graph">
-    ///     The graph to convert to the DOT format.
-    /// </param>
-    /// <param name="formattingOptions">
-    ///     The formatting options to use.
-    /// </param>
-    /// <param name="syntaxOptions">
-    ///     The generation options to use.
-    /// </param>
-    /// <param name="syntaxRules">
-    ///     The syntax rules to use.
-    /// </param>
-    [Pure]
-    public static string ToDot(
-        this DotGraph graph,
-        DotFormattingOptions? formattingOptions = null,
-        DotSyntaxOptions? syntaxOptions = null,
-        DotSyntaxRules? syntaxRules = null
-    )
-    {
-        using var stringWriter = new StringWriter();
-        graph.Save(stringWriter, formattingOptions, syntaxOptions, syntaxRules);
-
-        stringWriter.Flush();
-        return stringWriter.ToString();
-    }
-
     /// <summary>
     ///     Writes the DOT format representation of the graph to the provided text writer.
     /// </summary>
@@ -148,7 +114,7 @@ public static class DotGraphExtension
     ///     The syntax rules to use.
     /// </param>
     /// <param name="encoding">
-    ///     The encoding to use for the output text.
+    ///     The encoding to use for the output text. If not specified, UTF-8 without BOM is used.
     /// </param>
     public static void Save(
         this DotGraph graph,
@@ -162,42 +128,6 @@ public static class DotGraphExtension
         using var fileStream = File.Create(filePath);
         graph.Save(fileStream, formattingOptions, syntaxOptions, syntaxRules, encoding);
         fileStream.Flush();
-    }
-
-    /// <summary>
-    ///     Writes the DOT format representation of the graph to the specified file (the file will be overwritten if it already exists).
-    ///     The complete output is generated synchronously in memory and then written asynchronously to the specified file.
-    /// </summary>
-    /// <param name="graph">
-    ///     The graph to convert to the DOT format.
-    /// </param>
-    /// <param name="filePath">
-    ///     The path to the file to save the graph to.
-    /// </param>
-    /// <param name="formattingOptions">
-    ///     The formatting options to use.
-    /// </param>
-    /// <param name="syntaxOptions">
-    ///     The generation options to use.
-    /// </param>
-    /// <param name="syntaxRules">
-    ///     The syntax rules to use.
-    /// </param>
-    /// <param name="encoding">
-    ///     The encoding to use for the output text.
-    /// </param>
-    public static async Task SaveAsync(
-        this DotGraph graph,
-        string filePath,
-        DotFormattingOptions? formattingOptions = null,
-        DotSyntaxOptions? syntaxOptions = null,
-        DotSyntaxRules? syntaxRules = null,
-        Encoding? encoding = null
-    )
-    {
-        using var fileStream = File.Create(filePath);
-        await graph.SaveAsync(fileStream, formattingOptions, syntaxOptions, syntaxRules, encoding);
-        await fileStream.FlushAsync();
     }
 
     /// <summary>
@@ -219,7 +149,7 @@ public static class DotGraphExtension
     ///     The syntax rules to use.
     /// </param>
     /// <param name="encoding">
-    ///     The encoding to use for the output text.
+    ///     The encoding to use for the output text. If not specified, UTF-8 without BOM is used.
     /// </param>
     public static void Save(
         this DotGraph graph,
@@ -233,45 +163,6 @@ public static class DotGraphExtension
         using var streamWriter = CreateStreamWriter(stream, encoding);
         graph.Save(streamWriter, formattingOptions, syntaxOptions, syntaxRules);
         streamWriter.Flush();
-    }
-
-    /// <summary>
-    ///     Converts the specified graph to its DOT format representation and writes it to the provided stream. The complete output is
-    ///     generated synchronously in memory and then written asynchronously to the specified stream.
-    /// </summary>
-    /// <param name="graph">
-    ///     The graph to convert to the DOT format.
-    /// </param>
-    /// <param name="stream">
-    ///     The stream to write the graph to.
-    /// </param>
-    /// <param name="formattingOptions">
-    ///     The formatting options to use.
-    /// </param>
-    /// <param name="syntaxOptions">
-    ///     The generation options to use.
-    /// </param>
-    /// <param name="syntaxRules">
-    ///     The syntax rules to use.
-    /// </param>
-    /// <param name="encoding">
-    ///     The encoding to use for the output text.
-    /// </param>
-    public static async Task SaveAsync(
-        this DotGraph graph,
-        Stream stream,
-        DotFormattingOptions? formattingOptions = null,
-        DotSyntaxOptions? syntaxOptions = null,
-        DotSyntaxRules? syntaxRules = null,
-        Encoding? encoding = null
-    )
-    {
-        using var streamWriter = CreateStreamWriter(stream, encoding);
-
-        // it would be better to build the graph directly to stream, but the solution does not support async building
-        var output = graph.ToDot(formattingOptions, syntaxOptions, syntaxRules);
-        await streamWriter.WriteAsync(output);
-        await streamWriter.FlushAsync();
     }
 
     private static StreamWriter CreateStreamWriter(Stream stream, Encoding? encoding)
