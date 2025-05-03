e0d355293e7451067a26402f5f5319c69642975d
Mariusz Schimke <mariusz.schimke@gmail.com>
2025-05-03 20:58:52 +0200

    Remove redundant method overloads

--- src/GiGraph.Dot/Entities/Subgraphs/DotSubgraph.cs
+++ src/GiGraph.Dot/Entities/Subgraphs/DotSubgraph.cs
@@ -8,16 +8,16 @@ namespace GiGraph.Dot.Entities.Subgraphs;
 
 /// <summary>
 ///     <para>
-///         Represents a subgraph as a collection of nodes that may be constrained within one rank (see <see cref="DotRank" />). Use
-///         a subgraph when you want to have more granular control on the layout or style of the nodes it contains. However, when you
-///         want the nodes to be displayed together in a bounding box, use a cluster subgraph instead (<see cref="DotCluster" /> ).
+///         Represents a subgraph as a collection of nodes that may be constrained within one rank (see <see cref="DotRank"/>). Use a
+///         subgraph when you want to have more granular control on the layout or style of the nodes it contains. However, when you
+///         want the nodes to be displayed together in a bounding box, use a cluster subgraph instead (<see cref="DotCluster"/> ).
 ///     </para>
 ///     <para>
-///         Subgraph does not have any border or fill, as opposed to a cluster subgraph (<see cref="DotCluster" />). However, it
+///         Subgraph does not have any border or fill, as opposed to a cluster subgraph (<see cref="DotCluster"/>). However, it
 ///         supports applying a common style to nodes and edges within it, as well as constraining the nodes within one rank.
 ///     </para>
 ///     <para>
-///         Subgraph (<see cref="DotSubgraph" />) can also be used as a head or tail of an edge, in which case all nodes within them
+///         Subgraph (<see cref="DotSubgraph"/>) can also be used as a head or tail of an edge, in which case all nodes within them
 ///         are connected to the other side of the edge.
 ///     </para>
 /// </summary>
@@ -62,9 +62,9 @@ public class DotSubgraph : DotSubgraphSection, IDotGraph, IDotOrderable
 
     /// <summary>
     ///     <para>
-    ///         The subsections of the graph. They appear consecutively in the DOT output, and inherit the graph attributes, and
-    ///         the global node and/or edge attributes of their predecessors. When overridden in any subsection, the new graph attributes
-    ///         and global node/edge attributes apply to the elements the section itself contains, and also to those that belong to the
+    ///         The subsections of the graph. They appear consecutively in the DOT output, and inherit the graph attributes, and the
+    ///         global node and/or edge attributes of their predecessors. When overridden in any subsection, the new graph attributes and
+    ///         global node/edge attributes apply to the elements the section itself contains, and also to those that belong to the
     ///         sections that follow it (if any).
     ///     </para>
     ///     <para>
@@ -74,17 +74,17 @@ public class DotSubgraph : DotSubgraphSection, IDotGraph, IDotOrderable
     ///     </para>
     ///     <para>
     ///         As far as setting global node and/or edge attributes for a specific group of elements is concerned,
-    ///         <see cref="Subgraphs" /> may be the cleaner and preferable way to achieve the effect.
+    ///         <see cref="Subgraphs"/> may be the cleaner and preferable way to achieve the effect.
     ///     </para>
     /// </summary>
     public DotGraphSectionCollection<DotSubgraphSection> Subsections { get; }
-
-    IEnumerable<IDotGraphSection> IDotGraph.Subsections => Subsections;
 
     /// <summary>
     ///     Gets or sets the identifier of the subgraph (optional).
     /// </summary>
     public virtual string? Id { get; set; }
+
+    IEnumerable<IDotGraphSection> IDotGraph.Subsections => Subsections;
 
     string? IDotOrderable.OrderingKey => Id;
 
@@ -95,31 +95,6 @@ public class DotSubgraph : DotSubgraphSection, IDotGraph, IDotOrderable
     ///     The identifiers of nodes to populate the subgraph with.
     /// </param>
     public static DotSubgraph FromNodes(params string[] nodeIds) => FromNodes(nodeIds, nodeRank: null);
-
-    /// <summary>
-    ///     Creates a new subgraph, and populates it with the specified nodes.
-    /// </summary>
-    /// <param name="id">
-    ///     The identifier to assign to the subgraph.
-    /// </param>
-    /// <param name="nodeRank">
-    ///     The rank constraints to apply to the nodes in the subgraph.
-    /// </param>
-    /// <param name="nodeIds">
-    ///     The identifiers of nodes to populate the subgraph with.
-    /// </param>
-    public static DotSubgraph FromNodes(string id, DotRank? nodeRank, params string[] nodeIds) => FromNodes(nodeIds, nodeRank, id);
-
-    /// <summary>
-    ///     Creates a new subgraph, and populates it with the specified nodes.
-    /// </summary>
-    /// <param name="nodeRank">
-    ///     The rank constraints to apply to the nodes in the subgraph.
-    /// </param>
-    /// <param name="nodeIds">
-    ///     The identifiers of nodes to populate the subgraph with.
-    /// </param>
-    public static DotSubgraph FromNodes(DotRank? nodeRank, params string[] nodeIds) => FromNodes(nodeIds, nodeRank);
 
     /// <summary>
     ///     Creates a new subgraph, and populates it with the specified nodes.
