e1c30137289abb617c07431f0b4a00a17e580296
Mariusz Schimke <mariusz.schimke@gmail.com>
2025-05-06 21:24:29 +0200

    Add HTML table style enums

---
+++ src/GiGraph.Dot/Types/Html/Table/DotHtmlTableFillStyle.cs
@@ -0,0 +1,16 @@
+﻿namespace GiGraph.Dot.Types.Html.Table;
+
+/// <summary>
+///     The fill style of an HTML table or cell.
+/// </summary>
+public enum DotHtmlTableFillStyle
+{
+    /// <summary>
+    ///     Uses regular fill if a fill color is specified. This value is not an actual style option; it indicates that no fill style is
+    ///     specified, allowing the renderer to use its default behavior.
+    /// </summary>
+    Regular = 0,
+
+    /// <inheritdoc cref="DotHtmlTableStyles.Radial"/>
+    Radial = DotHtmlTableStyles.Radial
+}
