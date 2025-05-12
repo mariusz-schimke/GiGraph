157348a4ed42ffbaef5f72f903291a559269afc3
Mariusz Schimke <mariusz.schimke@gmail.com>
2025-05-12 18:51:29 +0200

    Add helper methods to HTML table and cell for setting fill color

---
+++ src/GiGraph.Dot/Entities/Html/Table/DotHtmlTableCell.Qualities.cs
@@ -0,0 +1,12 @@
+﻿using GiGraph.Dot.Entities.Qualities;
+using GiGraph.Dot.Types.Colors;
+using GiGraph.Dot.Types.Styling;
+
+namespace GiGraph.Dot.Entities.Html.Table;
+
+public partial class DotHtmlTableCell : IDotFillable
+{
+    void IDotFillable.SetFillStyle(DotFillStyle style) => Style.RadialFill = style == DotFillStyle.Radial;
+    void IDotFillable.SetFillColor(DotColorDefinition color) => Style.BackgroundColor = color;
+    void IDotFillable.SetGradientFillAngle(int? angle) => Style.GradientFillAngle = angle;
+}
