181afa37288ef9645e44681982e3ba7bafda4dcc
Mariusz Schimke <mariusz.schimke@gmail.com>
2025-05-14 22:07:57 +0200

    Move IDotFillable implementation from HTML table and cell to their nested Style property

--- src/GiGraph.Dot/Entities/Html/Table/Attributes/DotHtmlTableTableCellCommonStyleAttributes.Qualities.cs
+++ src/GiGraph.Dot/Entities/Html/Table/Attributes/DotHtmlTableTableCellCommonStyleAttributes.Qualities.cs
@@ -2,11 +2,11 @@
 using GiGraph.Dot.Types.Colors;
 using GiGraph.Dot.Types.Styling;
 
-namespace GiGraph.Dot.Entities.Html.Table;
+namespace GiGraph.Dot.Entities.Html.Table.Attributes;
 
-public partial class DotHtmlTable : IDotFillable
+public partial class DotHtmlTableTableCellCommonStyleAttributes<TIHtmlTableTableCellStyleAttributeProperties, THtmlTableTableCellStyleAttributeProperties> : IDotFillable
 {
-    void IDotFillable.SetFillStyle(DotFillStyle style) => Style.RadialFill = style == DotFillStyle.Radial;
-    void IDotFillable.SetFillColor(DotColorDefinition color) => Style.BackgroundColor = color;
-    void IDotFillable.SetGradientFillAngle(int? angle) => Style.GradientFillAngle = angle;
+    void IDotFillable.SetFillStyle(DotFillStyle style) => RadialFill = style == DotFillStyle.Radial;
+    void IDotFillable.SetFillColor(DotColorDefinition color) => BackgroundColor = color;
+    void IDotFillable.SetGradientFillAngle(int? angle) => GradientFillAngle = angle;
 }
