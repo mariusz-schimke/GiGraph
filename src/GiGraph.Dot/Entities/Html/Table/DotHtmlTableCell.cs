2ca1b8442fd944653a4b07aecec8e17fcbc0eafb
Mariusz Schimke <mariusz.schimke@gmail.com>
2025-05-16 22:08:08 +0200

    Add a SetSpan helper method

--- src/GiGraph.Dot/Entities/Html/Table/DotHtmlTableCell.cs
+++ src/GiGraph.Dot/Entities/Html/Table/DotHtmlTableCell.cs
@@ -48,4 +48,19 @@ public partial class DotHtmlTableCell : DotHtmlElement
     ///     Provides access to the attributes of the table cell.
     /// </summary>
     public new DotHtmlElementRootAttributesAccessor<IDotHtmlTableCellAttributes, DotHtmlTableCellAttributes> Attributes { get; }
+
+    /// <summary>
+    ///     Sets row and column span.
+    /// </summary>
+    /// <param name="columnSpan">
+    ///     The column span to set.
+    /// </param>
+    /// <param name="rowSpan">
+    ///     The row span to set.
+    /// </param>
+    public virtual void SetSpan(int? columnSpan = null, int? rowSpan = null)
+    {
+        ColumnSpan = columnSpan;
+        RowSpan = rowSpan;
+    }
 }
