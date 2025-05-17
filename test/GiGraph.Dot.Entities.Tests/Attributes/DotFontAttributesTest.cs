3061cbcdbf4cd95b0b78b5fdb3d341b108d8cc05
Mariusz Schimke <mariusz.schimke@gmail.com>
2025-05-17 10:36:57 +0200

    Add DotFontAttributes test

---
+++ test/GiGraph.Dot.Entities.Tests/Attributes/DotFontAttributesTest.cs
@@ -0,0 +1,30 @@
+using System.Drawing;
+using GiGraph.Dot.Entities.Attributes.Collections;
+using GiGraph.Dot.Entities.Attributes.Properties.Common.Font;
+using GiGraph.Dot.Types.Fonts;
+using Xunit;
+
+namespace GiGraph.Dot.Entities.Tests.Attributes;
+
+public class DotFontAttributesTest
+{
+    [Fact]
+    public void setter_with_params_sets_all_properties()
+    {
+        var font = new DotFontAttributes(new DotAttributeCollection());
+        font.Set("arial", 12, Color.Red);
+        Assert.Equal("arial", font.Name);
+        Assert.Equal(12, font.Size);
+        Assert.Equal(Color.Red, font.Color!.Color);
+    }
+    
+    [Fact]
+    public void setter_with_font_sets_all_properties()
+    {
+        var font = new DotFontAttributes(new DotAttributeCollection());
+        font.Set(new DotFont("arial", 12, Color.Red));
+        Assert.Equal("arial", font.Name);
+        Assert.Equal(12, font.Size);
+        Assert.Equal(Color.Red, font.Color!.Color);
+    }
+}
