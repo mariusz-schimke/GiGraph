db87dd84a13aa0484915945e905e5d0e24ac7d57
Mariusz Schimke <mariusz.schimke@gmail.com>
2025-05-18 18:40:55 +0200

    Remove DotGraphStyleOptions

--- test/GiGraph.Dot.Entities.Tests/GraphSections/DotGraphStyleAttributeOptionsTest.cs
+++ test/GiGraph.Dot.Entities.Tests/GraphSections/DotGraphStyleAttributeOptionsTest.cs
@@ -1,7 +1,6 @@
 using GiGraph.Dot.Entities.Graphs;
 using GiGraph.Dot.Extensions;
 using GiGraph.Dot.Types.Clusters.Style;
-using GiGraph.Dot.Types.Graphs.Style;
 using GiGraph.Dot.Types.Styling;
 using Snapshooter.Xunit;
 using Xunit;
@@ -14,49 +13,28 @@ public class DotGraphStyleAttributeOptionsTest
     public void all_style_properties_are_specified()
     {
         const string snapshotName = "styled_graph";
-        var graph = new DotGraph();
-
-        graph.Style.SetStyleOptions(
-            new DotGraphStyleOptions(
-                DotClusterFillStyle.Striped
-            )
-        );
+        var graph = new DotGraph
+        {
+            Style =
+            {
+                FillStyle = DotClusterFillStyle.Striped
+            }
+        };
 
         Assert.False(graph.Style.HasDefaultStyleOptions());
         Assert.False(graph.Clusters.Style.HasDefaultStyleOptions());
 
-        graph.Clusters.Style.SetStyleOptions(
-            new DotClusterStyleOptions(
-                DotClusterFillStyle.Striped,
-                DotBorderStyle.Dotted,
-                DotBorderWeight.Bold,
-                DotCornerStyle.Rounded,
-                true
-            )
-        );
+        graph.Clusters.Style.FillStyle = DotClusterFillStyle.Striped;
+        graph.Clusters.Style.BorderStyle = DotBorderStyle.Dotted;
+        graph.Clusters.Style.BorderWeight = DotBorderWeight.Bold;
+        graph.Clusters.Style.CornerStyle = DotCornerStyle.Rounded;
+        graph.Clusters.Style.Invisible = true;
 
         Snapshot.Match(graph.ToDot(), snapshotName);
 
         graph.Style.SetDefaultStyleOptions();
         Assert.True(graph.Style.HasDefaultStyleOptions());
         Assert.True(graph.Clusters.Style.HasDefaultStyleOptions());
-
-        // set the same another way
-        graph.Clusters.Style.SetStyleOptions(
-            DotClusterFillStyle.Radial, // this one is shared with one directly on the graph (see below)
-            DotBorderStyle.Dotted,
-            DotBorderWeight.Bold,
-            DotCornerStyle.Rounded,
-            true
-        );
-
-        graph.Style.SetStyleOptions(
-            new DotGraphStyleOptions(
-                DotClusterFillStyle.Striped // overwrites the one set on the clusters above
-            )
-        );
-
-        Snapshot.Match(graph.ToDot(), snapshotName);
 
         graph.Clusters.Style.SetDefaultStyleOptions();
         Assert.True(graph.Style.HasDefaultStyleOptions());
