67001f572da2a71128c92dfb1d76b39cebccef7a
Mariusz Schimke <mariusz.schimke@gmail.com>
2025-05-21 21:17:28 +0200

    Add unit tests

--- test/GiGraph.Dot.Entities.Tests/Html/Table/DotHtmlTableHyperlinkTest.cs
+++ test/GiGraph.Dot.Entities.Tests/Html/Table/DotHtmlTableHyperlinkTest.cs
@@ -7,20 +7,6 @@ namespace GiGraph.Dot.Entities.Tests.Html.Table;
 public class DotHtmlTableHyperlinkTest
 {
     [Fact]
-    public void hyperlink_setter_with_params_sets_all_properties()
-    {
-        var table = new DotHtmlTable();
-        var hyperlink = CreateHyperlink();
-
-        table.Hyperlink.Set(hyperlink.Href, hyperlink.Target, hyperlink.Tooltip, hyperlink.Title);
-
-        Assert.Equal(hyperlink.Href, table.Hyperlink.Href);
-        Assert.Equal(hyperlink.Target, table.Hyperlink.Target);
-        Assert.Equal(hyperlink.Tooltip, table.Hyperlink.Tooltip);
-        Assert.Equal(hyperlink.Title, table.Hyperlink.Title);
-    }
-
-    [Fact]
     public void hyperlink_setter_with_record_sets_all_properties()
     {
         var table = new DotHtmlTable();
---
+++ test/GiGraph.Dot.Entities.Tests/Qualities/DotHasHyperlinkExtensionTest.cs
@@ -0,0 +1,99 @@
+using Bogus;
+using GiGraph.Dot.Entities.Edges;
+using GiGraph.Dot.Entities.Html.Table;
+using GiGraph.Dot.Entities.Qualities;
+using GiGraph.Dot.Extensions;
+using GiGraph.Dot.Types.Hyperlinks;
+using Xunit;
+
+namespace GiGraph.Dot.Entities.Tests.Qualities;
+
+public class DotHasHyperlinkExtensionTest
+{
+    [Fact]
+    public void table_hyperlink_setter_with_params_sets_all_properties()
+    {
+        var table = new DotHtmlTable();
+        TestMethodsOfHasHyperlink(table.Hyperlink);
+        TestMethodsOfHasHyperlinkWithTooltip(table.Hyperlink);
+    }
+
+    [Fact]
+    public void table_cell_hyperlink_setter_with_params_sets_all_properties()
+    {
+        var cell = new DotHtmlTableCell();
+        TestMethodsOfHasHyperlink(cell.Hyperlink);
+        TestMethodsOfHasHyperlinkWithTooltip(cell.Hyperlink);
+    }
+
+    [Fact]
+    public void edge_hyperlink_setter_with_params_sets_all_properties()
+    {
+        var edge = new DotEdge("node1", "node2");
+        TestMethodsOfHasHyperlink(edge.Hyperlink);
+    }
+
+    [Fact]
+    public void edge_edge_hyperlink_with_params_setter_sets_all_properties()
+    {
+        var edge = new DotEdge("node1", "node2");
+        TestMethodsOfHasHyperlink(edge.EdgeHyperlink);
+        TestMethodsOfHasHyperlinkWithTooltip(edge.EdgeHyperlink);
+    }
+
+    private void TestMethodsOfHasHyperlink<T>(T entity)
+        where T : IDotHasHyperlink
+    {
+        var hyperlink = CreateRandomHyperlink();
+        entity.Set(hyperlink.Href, hyperlink.Target);
+        Assert.Equal(hyperlink.Href, entity.Href);
+        Assert.Equal(hyperlink.Target, entity.Target);
+
+        hyperlink = CreateRandomHyperlink();
+        entity.SetWithNewWindowTarget(hyperlink.Href);
+        Assert.Equal(hyperlink.Href, entity.Href);
+        Assert.Equal(DotHyperlinkTargets.Blank, entity.Target);
+
+        entity.Target = null;
+        Assert.Null(entity.Target);
+        entity.SetNewWindowTarget();
+        Assert.Equal(hyperlink.Href, entity.Href);
+        Assert.Equal(DotHyperlinkTargets.Blank, entity.Target);
+    }
+
+    private void TestMethodsOfHasHyperlinkWithTooltip<T>(T entity)
+        where T : IDotHasHyperlinkWithTooltip
+    {
+        var hyperlink = CreateRandomHyperlink();
+        entity.Set(hyperlink.Href, hyperlink.Target, hyperlink.Tooltip);
+        Assert.Equal(hyperlink.Href, entity.Href);
+        Assert.Equal(hyperlink.Target, entity.Target);
+        Assert.Equal(hyperlink.Tooltip, entity.Tooltip);
+
+        hyperlink = CreateRandomHyperlink();
+        entity.Target = hyperlink.Target;
+        entity.SetWithTooltip(hyperlink.Href, hyperlink.Tooltip);
+        Assert.Equal(hyperlink.Href, entity.Href);
+        Assert.Equal(hyperlink.Target, entity.Target);
+        Assert.Equal(hyperlink.Tooltip, entity.Tooltip);
+
+        hyperlink = CreateRandomHyperlink();
+        entity.SetWithNewWindowTarget(hyperlink.Href, hyperlink.Tooltip);
+        Assert.Equal(hyperlink.Href, entity.Href);
+        Assert.Equal(DotHyperlinkTargets.Blank, entity.Target);
+        Assert.Equal(hyperlink.Tooltip, entity.Tooltip);
+    }
+
+    private static DotHyperlink CreateRandomHyperlink()
+    {
+        var faker = new Faker();
+        return new DotHyperlink
+        {
+            Href = faker.Random.Hash(),
+            Url = faker.Random.Hash(),
+            Target = faker.Random.Hash(),
+            Tooltip = faker.Random.Hash(),
+            Title = faker.Random.Hash()
+        };
+    }
+}
