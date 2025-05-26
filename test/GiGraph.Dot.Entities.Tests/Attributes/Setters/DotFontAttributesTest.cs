ce13683cae5557dc5f39c0a637d85a80a79fee07
Mariusz Schimke <28633575+mariusz-schimke@users.noreply.github.com>
2025-05-26 21:37:43 +0200

    Ensure extension methods have unit tests (#435)
    Ensure extension methods have unit tests

--- test/GiGraph.Dot.Entities.Tests/Attributes/Setters/DotFontAttributesTest.cs
+++ test/GiGraph.Dot.Entities.Tests/Attributes/Setters/DotFontAttributesTest.cs
@@ -1,7 +1,6 @@
 using System.Drawing;
 using GiGraph.Dot.Entities.Attributes.Collections;
 using GiGraph.Dot.Entities.Attributes.Properties.Common.Font;
-using GiGraph.Dot.Extensions;
 using GiGraph.Dot.Types.Fonts;
 using Xunit;
 
@@ -9,26 +8,6 @@ namespace GiGraph.Dot.Entities.Tests.Attributes.Setters;
 
 public class DotFontAttributesTest
 {
-    [Fact]
-    public void setter_with_params_sets_all_properties()
-    {
-        var font = new DotFontAttributes(new DotAttributeCollection());
-        Assert.Null(font.Name);
-        Assert.Null(font.Size);
-        Assert.Null(font.Color);
-
-        font.Name = "arial";
-        font.Set(12, Color.Red);
-        Assert.Equal("arial", font.Name);
-        Assert.Equal(12, font.Size);
-        Assert.Equal(Color.Red, font.Color!.Color);
-
-        font.Set("helvetica", 13, Color.Blue);
-        Assert.Equal("helvetica", font.Name);
-        Assert.Equal(13, font.Size);
-        Assert.Equal(Color.Blue, font.Color!.Color);
-    }
-
     [Fact]
     public void setter_with_font_sets_all_properties()
     {
---
+++ test/GiGraph.Dot.Entities.Tests/Qualities/DotHasFontAttributesExtensionTest.cs
@@ -0,0 +1,53 @@
+using System.Drawing;
+using GiGraph.Dot.Entities.Attributes.Collections;
+using GiGraph.Dot.Entities.Attributes.Properties.Common.Font;
+using GiGraph.Dot.Entities.Graphs.Attributes;
+using GiGraph.Dot.Entities.Html.Font;
+using GiGraph.Dot.Entities.Qualities;
+using GiGraph.Dot.Extensions;
+using Xunit;
+
+namespace GiGraph.Dot.Entities.Tests.Qualities;
+
+public class DotHasFontAttributesExtensionTest
+{
+    [Fact]
+    public void graph_font_setters_set_all_properties()
+    {
+        var fontAttributes = new DotGraphFontAttributes(new DotAttributeCollection());
+        TestExtensionMethods(fontAttributes);
+    }
+
+    [Fact]
+    public void font_attributes_setters_set_all_properties()
+    {
+        var fontAttributes = new DotFontAttributes(new DotAttributeCollection());
+        TestExtensionMethods(fontAttributes);
+    }
+
+    [Fact]
+    public void html_font_setters_set_all_properties()
+    {
+        var font = new DotHtmlFont();
+        TestExtensionMethods(font);
+    }
+
+    private void TestExtensionMethods<T>(T entity)
+        where T : IDotHasFontAttributes
+    {
+        Assert.Null(entity.Name);
+        Assert.Null(entity.Size);
+        Assert.Null(entity.Color);
+
+        entity.Name = "arial";
+        entity.Set(12, Color.Red);
+        Assert.Equal("arial", entity.Name);
+        Assert.Equal(12, entity.Size);
+        Assert.Equal(Color.Red, entity.Color!.Color);
+
+        entity.Set("helvetica", 13, Color.Blue);
+        Assert.Equal("helvetica", entity.Name);
+        Assert.Equal(13, entity.Size);
+        Assert.Equal(Color.Blue, entity.Color!.Color);
+    }
+}
