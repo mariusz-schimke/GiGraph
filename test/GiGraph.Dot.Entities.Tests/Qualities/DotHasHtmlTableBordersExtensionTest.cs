ca1551f82cba6047299dd5f16b655e0c151e91d0
Mariusz Schimke <mariusz.schimke@gmail.com>
2025-05-26 21:22:26 +0200

    Cleanup

--- test/GiGraph.Dot.Entities.Tests/Qualities/DotHasHtmlTableBordersExtensionTest.cs
+++ test/GiGraph.Dot.Entities.Tests/Qualities/DotHasHtmlTableBordersExtensionTest.cs
@@ -1,4 +1,5 @@
 using GiGraph.Dot.Entities.Html.Table;
+using GiGraph.Dot.Entities.Qualities;
 using GiGraph.Dot.Extensions;
 using GiGraph.Dot.Types.Html.Table;
 using Xunit;
@@ -11,31 +12,34 @@ public class DotHasHtmlTableBordersExtensionTest
     public void table_borders_setter_sets_correct_borders()
     {
         var table = new DotHtmlTable();
-        Assert.Null(table.Borders);
-
-        table.SetBorders(true);
-        Assert.Equal(DotHtmlTableBorders.Top, table.Borders);
-
-        table.SetBorders(true, true);
-        Assert.Equal(DotHtmlTableBorders.Top | DotHtmlTableBorders.Right, table.Borders);
-
-        table.SetBorders(true, true, true);
-        Assert.Equal(DotHtmlTableBorders.Top | DotHtmlTableBorders.Right | DotHtmlTableBorders.Bottom, table.Borders);
-
-        table.SetBorders(true, true, true, true);
-        Assert.Equal(DotHtmlTableBorders.Top | DotHtmlTableBorders.Right | DotHtmlTableBorders.Bottom | DotHtmlTableBorders.Left, table.Borders);
-
-        table.SetBorders();
-        Assert.Null(table.Borders);
+        TestSettingBorders(table);
     }
 
     [Fact]
-    public void borders_setter_is_available_on_cell()
+    public void table_cell_borders_setter_sets_correct_borders()
     {
-        // This test just makes sure the table cell implements the interface for the extension method to be available.
-        // The rest is tested by the other tests here on the table.
-        var table = new DotHtmlTableCell();
-        table.SetBorders();
-        Assert.Null(table.Borders);
+        var cell = new DotHtmlTableCell();
+        TestSettingBorders(cell);
+    }
+
+    private void TestSettingBorders<T>(T entity)
+        where T : IDotHasHtmlTableBorders
+    {
+        Assert.Null(entity.Borders);
+
+        entity.SetBorders(true);
+        Assert.Equal(DotHtmlTableBorders.Top, entity.Borders);
+
+        entity.SetBorders(true, true);
+        Assert.Equal(DotHtmlTableBorders.Top | DotHtmlTableBorders.Right, entity.Borders);
+
+        entity.SetBorders(true, true, true);
+        Assert.Equal(DotHtmlTableBorders.Top | DotHtmlTableBorders.Right | DotHtmlTableBorders.Bottom, entity.Borders);
+
+        entity.SetBorders(true, true, true, true);
+        Assert.Equal(DotHtmlTableBorders.Top | DotHtmlTableBorders.Right | DotHtmlTableBorders.Bottom | DotHtmlTableBorders.Left, entity.Borders);
+
+        entity.SetBorders();
+        Assert.Null(entity.Borders);
     }
 }
